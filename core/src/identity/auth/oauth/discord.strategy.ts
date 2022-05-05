79ce5916a7537dee1642eb122509bd992ac461d6
Jake Bailey <asaxplayinghorse@gmail.com>
2022-05-05 10:01:01 -0700

    fix: field names in discord strategy

--- core/src/identity/auth/oauth/discord.strategy.ts
+++ core/src/identity/auth/oauth/discord.strategy.ts
@@ -1,14 +1,11 @@
 import {Injectable} from "@nestjs/common";
 import {PassportStrategy} from "@nestjs/passport";
-//import {readFileSync} from "fs";
-//import type {VerifyCallback} from "passport-discord-oauth20";
 import {Profile, Strategy} from "passport-discord";
 import {UserAuthenticationAccountType} from "src/database";
 import {User} from "src/database/identity/user/user.model";
 import {UserService} from "src/identity/user/user.service";
 
 import {config} from "../../../util/config";
-import type {DiscordProfileType} from "./types/profile.type";
 
 export type Done = (err: Error, user: User) => void;
 
@@ -39,14 +36,14 @@ export class DiscordStrategy extends PassportStrategy(Strategy, "discord") {
         // If no users returned from query, create a new one
         if (queryResult.length === 0) {
             const userProfile = {
-                description: "",
-                email: profile.emails[0].value,
+                description: "Discord user",
+                email: email,
                 firstName: username,
                 lastName: "",
             };
 
             const authAcct = {
-                accountType: UserAuthenticationAccountType.discord,
+                accountType: UserAuthenticationAccountType.DISCORD,
                 accountId: discordId,
                 oauthToken: accessToken,
             };
@@ -55,7 +52,7 @@ export class DiscordStrategy extends PassportStrategy(Strategy, "discord") {
         // Else, return the one we found
             user = queryResult[0];
         }
-        done(null, user);
+
         return user;
     }
 }
