1f4a7f5b97aec6f0e927e88f503fa99f60b33eb6
Brian D <contact@noimbrian.com>
2022-05-08 16:20:50 -0500

    chore: push account imports to analytics

--- core/src/identity/auth/oauth/strategies/discord.strategy.ts
+++ core/src/identity/auth/oauth/strategies/discord.strategy.ts
@@ -1,24 +1,25 @@
-import {Injectable} from "@nestjs/common";
+import {Injectable, Logger} from "@nestjs/common";
 import {PassportStrategy} from "@nestjs/passport";
 import {readFileSync} from "fs";
 import type {GuildInfo, Profile} from "passport-discord";
 import {Strategy} from "passport-discord";
-import type {
-    IrrelevantFields,
-    UserAuthenticationAccount,
-    UserProfile,
-} from "src/database";
+import type {IrrelevantFields, UserAuthenticationAccount, UserProfile,} from "src/database";
 import {UserAuthenticationAccountType} from "src/database";
 import {User} from "src/database/identity/user/user.model";
 import {UserService} from "src/identity/user/user.service";
 import {config} from "src/util/config";
+import {AnalyticsEndpoint, AnalyticsService} from "@sprocketbot/common";
 
 export type Done = (err: string, user: User) => void;
 const mleGuildId = "172404472637685760";
+
 @Injectable()
 export class DiscordStrategy extends PassportStrategy(Strategy, "discord") {
 
-    constructor(private readonly userService: UserService) {
+    private readonly logger = new Logger(DiscordStrategy.name)
+
+    constructor(private readonly userService: UserService,
+                private readonly analyticsService: AnalyticsService) {
         super({
             clientID: config.auth.discordClientId,
             clientSecret: readFileSync("./secret/discord-secret.txt").toString(),
@@ -45,7 +46,7 @@ export class DiscordStrategy extends PassportStrategy(Strategy, "discord") {
         }
 
         // First, check if the user already exists
-        const queryResult = await this.userService.getUsers({where: {email: profile.email as string} });
+        const queryResult = await this.userService.getUsers({where: {email: profile.email as string}});
 
         // If no users returned from query, create a new one
         if (queryResult.length === 0) {
@@ -62,6 +63,18 @@ export class DiscordStrategy extends PassportStrategy(Strategy, "discord") {
                 oauthToken: accessToken,
             };
             user = await this.userService.createUser(userProfile, [authAcct]);
+            this.analyticsService.send(AnalyticsEndpoint.Analytics, {
+                name: "SprocketAccountImported",
+                tags: [
+                    ['source', "MLEDB"],
+                ],
+                strings: [
+                    ['account_id', profile.id],
+                    ['displayname', profile.username]
+                ]
+            })
+                .then(() => this.logger.log("Account Import Recorded via Analytics"))
+                .catch(this.logger.error.bind(this.logger))
         } else {
             // Else, return the one we found
             user = queryResult[0];
