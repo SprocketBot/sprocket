11439f2665e66c5b5eeb9657c9dfcb89edaf9ef5
Brian D <contact@noimbrian.com>
2022-05-08 17:42:51 -0500

    chore: fix user service

--- core/src/identity/user/user.service.ts
+++ core/src/identity/user/user.service.ts
@@ -4,9 +4,7 @@ import {Repository} from "typeorm";
 import type {FindManyOptions} from "typeorm/find-options/FindManyOptions";
 
 import type {IrrelevantFields} from "../../database";
-import {
-    User, UserAuthenticationAccount, UserProfile,
-} from "../../database";
+import {User, UserAuthenticationAccount, UserProfile,} from "../../database";
 
 @Injectable()
 export class UserService {
@@ -14,7 +12,8 @@ export class UserService {
         @InjectRepository(User) private userRepository: Repository<User>,
         @InjectRepository(UserProfile) private userProfileRepository: Repository<UserProfile>,
         @InjectRepository(UserAuthenticationAccount) private userAuthAcctRepository: Repository<UserAuthenticationAccount>,
-    ) {}
+    ) {
+    }
 
     /**
      * Creates an User with a given profile.
@@ -29,9 +28,11 @@ export class UserService {
         const authAccts = authenticationAccounts.map(acct => this.userAuthAcctRepository.create(acct));
         const user = this.userRepository.create({userProfile: profile});
         user.authenticationAccounts = authAccts;
-        
+        authAccts.forEach(aa => aa.user = user)
+
         await this.userProfileRepository.save(user.userProfile);
         await this.userRepository.save(user);
+        await this.userAuthAcctRepository.save(authAccts)
         return user;
     }
 
@@ -62,7 +63,7 @@ export class UserService {
      * for the user.
      */
     async getUserAuthenticationAccountsForUser(userId: number): Promise<UserAuthenticationAccount[]> {
-        const authAccounts = await this.userAuthAcctRepository.find({where: {id: userId} });
+        const authAccounts = await this.userAuthAcctRepository.find({where: {user: {id: userId}}});
         return authAccounts;
     }
 
@@ -74,14 +75,17 @@ export class UserService {
     async getUserById(id: number): Promise<User> {
         return this.userRepository.findOneOrFail(id);
     }
-    
+
     /**
      * Finds Users that match a given query.
      * @param query A query to search for matching Organzations.
      * @returns An array containg Users matching the given query.
      */
     async getUsers(query: FindManyOptions<UserProfile>): Promise<User[]> {
-        const userProfiles = await this.userProfileRepository.find({...query, relations: ["user", ...query.relations ?? []] });
+        const userProfiles = await this.userProfileRepository.find({
+            ...query,
+            relations: ["user", ...query.relations ?? []]
+        });
         return userProfiles.map(op => op.user);
     }
 
@@ -94,7 +98,7 @@ export class UserService {
     async updateUserProfile(userId: number, data: Omit<Partial<UserProfile>, "user">): Promise<UserProfile> {
         let {userProfile} = await this.userRepository.findOneOrFail(
             userId,
-            {relations: ["userProfile"] },
+            {relations: ["userProfile"]},
         );
         userProfile = this.userProfileRepository.merge(userProfile, data);
         await this.userProfileRepository.save(userProfile);
@@ -121,7 +125,7 @@ export class UserService {
      * @returns The found UserProfile
      */
     async getUserProfileForUser(userId: number): Promise<UserProfile> {
-        const org = await this.userRepository.findOneOrFail(userId, {relations: ["userProfile"] });
+        const org = await this.userRepository.findOneOrFail(userId, {relations: ["userProfile"]});
         return org.userProfile;
     }
 }
