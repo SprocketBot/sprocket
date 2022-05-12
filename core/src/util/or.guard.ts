eab45c283e0fc52620454526f5d836fb093b1ef3
Bryan Pikaard <bryan@pikaard.com>
2022-05-12 23:11:10 +0000

    feat: allow guards that use injected services

--- core/src/util/or.guard.ts
+++ core/src/util/or.guard.ts
@@ -2,18 +2,35 @@ import type {
     CanActivate, ExecutionContext, Type,
 } from "@nestjs/common";
 import {Injectable} from "@nestjs/common";
+import {ModuleRef} from "@nestjs/core";
 
-export function OrGuard(...guards: CanActivate[]): Type<CanActivate> {
+/**
+ * Function to create an "or" guard. Only one must pass true for the method to be accessible.
+ *
+ * ```js
+ * \@UseGuards(OrGuard(QueueBanGuard, JoinScrimPlayerGuard))
+ * async function joinScrim(): Promise<void> {}
+ * ```
+ *
+ * @param {Array<Type<CanActivate>>} guards The guards to use.
+ * @returns {Type<CanActivate>} The combined guard.
+ */
+export function OrGuard(...guards: Array<Type<CanActivate>>): Type<CanActivate> {
     @Injectable()
     class _OrGuard implements CanActivate {
+        constructor(private readonly moduleRef: ModuleRef) {}
+
         async canActivate(context: ExecutionContext): Promise<boolean> {
             const errors: string[] = [];
-            const guardResponses = await Promise.all(guards.map(async (g): Promise<boolean> => {
+            const guardResponses = await Promise.all(guards.map(async (_guard): Promise<boolean> => {
+                const guard = await this.moduleRef.create<CanActivate>(_guard);
+
                 try {
-                    const canActivate = g.canActivate(context) as Promise<boolean>;
+                    const canActivate = guard.canActivate(context) as Promise<boolean>;
                     return await canActivate;
                 } catch (e) {
-                    errors.push((e as Error).message);
+                    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+                    errors.push((e as Error)?.message ?? "Unknown Error");
                     return false;
                 }
             }));
