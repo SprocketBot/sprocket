5c3c1672a5fa05ece540d199e8f3351b110ef690
Zachary Stence <zachstence@gmail.com>
2022-05-14 19:31:58 -0500

    fix: specify server_hostname for broker ssl

--- microservices/replay-parse-service/src/celeryconfig.py
+++ microservices/replay-parse-service/src/celeryconfig.py
@@ -7,3 +7,11 @@ broker_transport_options = {
 task_default_queue = config["celery"]["queue"]
 broker_url = config["celery"]["broker"]
 result_backend = config["celery"]["backend"]
+
+# This option isn't documented anywhere in Celery's docs, and I don't fully understand why it works
+# Something to do with rabbitmq.dev.spr.ocket.cloud requiring SNI support
+# https://www.ssllabs.com/ssltest/analyze.html?d=rabbitmq.dev.spr.ocket.cloud > "This site works only in browsers with SNI support."
+# https://github.com/celery/py-amqp/blob/98f6d364188215c2973693a79e461c7e9b54daef/amqp/transport.py#L488
+broker_use_ssl = {
+    'server_hostname': 'rabbitmq.dev.spr.ocket.cloud'
+}
