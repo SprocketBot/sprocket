109e1164e90a622693d22eaf40acb2a2c678e56f
Zachary Stence <zachstence@gmail.com>
2022-05-15 19:32:02 -0500

    docs: redis tls ???

--- microservices/replay-parse-service/src/celeryconfig.py
+++ microservices/replay-parse-service/src/celeryconfig.py
@@ -1,3 +1,4 @@
+import ssl
 from config import config
 from urllib.parse import urlparse
 
@@ -12,8 +13,8 @@ result_backend = config["celery"]["backend"]
 
 # If we're using SSL, specify the server_hostname
 _BROKER_URL = urlparse(config["celery"]["broker"])
-_SECURE = _BROKER_URL.scheme.endswith("s")
-_SERVER_HOSTNAME = _BROKER_URL.hostname
+_BROKER_SECURE = _BROKER_URL.scheme.endswith("s")
+_BROKER_HOSTNAME = _BROKER_URL.hostname
 
 # This option isn't documented anywhere in Celery's docs, and I don't fully understand why it works
 # Something to do with rabbitmq.dev.spr.ocket.cloud requiring SNI support
@@ -21,7 +22,23 @@ _SERVER_HOSTNAME = _BROKER_URL.hostname
 # https://github.com/celery/py-amqp/blob/98f6d364188215c2973693a79e461c7e9b54daef/amqp/transport.py#L488
 broker_use_ssl = None
 
-if _SECURE: 
+if _BROKER_SECURE: 
     broker_use_ssl = {
-        'server_hostname': _SERVER_HOSTNAME
+        'server_hostname': _BROKER_HOSTNAME
     }
+
+# TODO not sure if we need this
+
+# # If we're using SSL, specify the server_hostname
+# _BACKEND_URL = urlparse(config["celery"]["backend"])
+# _BACKEND_SECURE = _BACKEND_URL.scheme.endswith("s")
+# _BACKEND_HOSTNAME = _BACKEND_URL.hostname
+# print(_BACKEND_HOSTNAME)
+
+# redis_use_ssl = None
+
+# if _BACKEND_SECURE: 
+#     redis_use_ssl = {
+#         'server_hostname': _BACKEND_HOSTNAME,
+#         'ssl_cert_reqs': ssl.CERT_NONE
+#     }
