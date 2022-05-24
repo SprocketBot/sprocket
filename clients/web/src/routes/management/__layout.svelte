e125cae988b1a7c228a8b4f598b8b9e2da1b65de
Jake Bailey <asaxplayinghorse@gmail.com>
2022-05-24 11:57:18 -0700

    feat: create management route

---
+++ clients/web/src/routes/management/__layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts" context="module">
+    import type {LoadInput, LoadOutput} from "@sveltejs/kit";
+
+    export function load({session}: LoadInput): LoadOutput {
+        if (!session.user) {
+            return {redirect: "/auth/login", status: 302};
+        }
+        return {};
+    }
+</script>
+
+<slot/>
---
+++ clients/web/src/routes/management/index.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import {currentScrim} from "$lib/api";
+    import type {MetricsResult} from "$lib/api/queries/ScrimMetrics.store";
+    import {scrimMetrics} from "$lib/api/queries/ScrimMetrics.store";
+
+    import {
+        AvailableScrimsView,
+        DashboardLayout,
+        DashboardCard,
+        DashboardNumberCard,
+        QueuedView,
+    } from "$lib/components";
+
+    let metrics: MetricsResult["metrics"];
+    $: metrics = $scrimMetrics.data?.metrics;
+
+    let activityChange: number = 0;
+
+    function calculateActivityChange() {
+        const prev = metrics.previousCompletedScrims;
+        const cur = metrics?.completedScrims;
+        if (metrics?.completedScrims && metrics.previousCompletedScrims) {
+            const change = cur - prev;
+            const ratio = change / Math.max(prev, 1);
+            console.log({
+                change, cur, prev, ratio,
+            });
+
+            activityChange = Math.round(ratio * 100);
+        }
+    }
+
+    $: if (typeof metrics?.completedScrims === "number" || typeof metrics?.previousCompletedScrims === "number") {
+        calculateActivityChange();
+    }
+</script>
+
+<DashboardLayout>
+    <DashboardCard class="col-span-6 xl:col-span-5 row-span-3">
+        {#if $currentScrim.fetching}
+            Loading...
+        {:else if $currentScrim.data?.currentScrim}
+            <QueuedView/>
+        {:else}
+            <AvailableScrimsView/>
+        {/if}
+    </DashboardCard>
+    <DashboardNumberCard title="Scrims in the last hour"
+                         value={metrics?.completedScrims ?? 0}
+                         description="{Math.abs(activityChange)}% {activityChange > 0 ? 'up' : 'down'}"
+    />
+    <DashboardNumberCard title="Pending Scrims"
+                         value={metrics?.pendingScrims ?? 0}
+    />
+    <DashboardNumberCard title="Active Players"
+                         value={metrics?.totalPlayers ?? 0}
+    />
+
+</DashboardLayout>
+
+<style lang="postcss">
+
+    h2 {
+        @apply text-4xl mb-8;
+    }
+</style>
