44dd64afcf7e4bf94adb1666944961af11f41dda
Jake Bailey <asaxplayinghorse@gmail.com>
2022-06-02 16:22:04 -0700

    fix: whitespace

--- clients/web/src/lib/components/organisms/scrims/modals/ScrimManagementModal.svelte
+++ clients/web/src/lib/components/organisms/scrims/modals/ScrimManagementModal.svelte
@@ -33,58 +33,57 @@
         {#if uploading}
             <UploadReplaysModal bind:visible={uploading} submissionId={targetScrim?.submissionId} />
         {:else}
-        {#if targetScrim && targetScrim?.playerCount > 0}
-            {#if targetScrim.players === null}
-                This Scrim is still in the 'PENDING' state.
-            {:else}
-                No longer in pending, we've popped! Here are the players:
-                <table class="table text-center w-full" >
-                <thead>
-                <tr>
-                    <th>Player Name</th>
-                    <th>Player ID</th>
-                    <th>Checked In?</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                {#each targetScrim.players as player (player.id)}
+            {#if targetScrim && targetScrim?.playerCount > 0}
+                {#if targetScrim.players === null}
+                    This Scrim is still in the 'PENDING' state.
+                {:else}
+                    No longer in pending, we've popped! Here are the players:
+                    <table class="table text-center w-full" >
+                    <thead>
                     <tr>
-                        <td>{player.name}</td>
-                        <td>{player.id}</td>
-                        <td>{player.checkedIn}</td>
-                        <td>
-                            <button class="btn btn-outline float-right lg:btn-sm">
-                                Manage
-                            </button>
-                        </td>
+                        <th>Player Name</th>
+                        <th>Player ID</th>
+                        <th>Checked In?</th>
+                        <th></th>
                     </tr>
-                {/each}
-                </tbody>
-            </table>
-            {/if}
-            {#if targetScrim.status === "IN_PROGRESS"}
+                    </thead>
+                    <tbody>
+                    {#each targetScrim.players as player (player.id)}
+                        <tr>
+                            <td>{player.name}</td>
+                            <td>{player.id}</td>
+                            <td>{player.checkedIn}</td>
+                            <td>
+                                <button class="btn btn-outline float-right lg:btn-sm">
+                                    Manage
+                                </button>
+                            </td>
+                        </tr>
+                    {/each}
+                    </tbody>
+                </table>
+                {/if}
+                {#if targetScrim.status === "IN_PROGRESS"}
+                    <div class="divider" />
+                    <div class="flex items-center">
+                        <h3 class="flex-1 text-info-content">Now you can also
+                        upload replays.</h3>
+                        <div>
+                            <button on:click={uploadReplays} class="btn btn-primary
+                             btn-sm">Upload</button>
+                        </div>
+                    </div>
+                {/if}
                 <div class="divider" />
                 <div class="flex items-center">
-                    <h3 class="flex-1 text-info-content">Now you can also
-                    upload replays.</h3>
+                    <h3 class="flex-1 text-error-content">Cancel this scrim</h3>
                     <div>
-                        <button on:click={uploadReplays} class="btn btn-primary
-                         btn-sm">Upload</button>
+                        <button on:click={cancelScrim} class="btn btn-error btn-outline btn-sm">Cancel</button>
                     </div>
                 </div>
+            {:else}
+                Target Scrim does not exist, sorry. 
             {/if}
-            <div class="divider" />
-            <div class="flex items-center">
-                <h3 class="flex-1 text-error-content">Cancel this scrim</h3>
-                <div>
-                    <button on:click={cancelScrim} class="btn btn-error btn-outline btn-sm">Cancel</button>
-                </div>
-            </div>
-
-        {:else}
-            Target Scrim does not exist, sorry. 
-        {/if}
         {/if}
     </section>
 
