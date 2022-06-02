081b45c00ba260410660f43ffe5080c733b16396
Jake Bailey <asaxplayinghorse@gmail.com>
2022-06-01 23:08:02 -0700

    feat: add player list back to scrim table

--- clients/web/src/lib/components/molecules/scrims/AdminScrimTable.svelte
+++ clients/web/src/lib/components/molecules/scrims/AdminScrimTable.svelte
@@ -1,37 +1,17 @@
 <script lang="ts">
     import {ScrimManagementModal} from "$lib/components";
     import {screamingSnakeToHuman} from "$lib/utils";
-    import {AllCurrentScrimsQuery} from "$lib/api";
-    import {client} from "$lib/api/client";
     import { activeScrims, type ActiveScrims } from "../../../api/queries/ActiveScrims.store";
-    import type {CurrentScrim} from "../../../api/queries/CurrentScrim.store";
 
     /*
     This will be the table that shows in ScrimManagementModal (Currently Named Modal
     but I have since realized that it not the greatest)
 
-    TODO: Populate all scrims list
     TODO: Create/Implement Search
     TODO: Figure out Visibility status pattern from CreateScrimModal and Scrim Table
 
      */
 
-    //export let adminScrimsInTable = [
-    //   {
-    //       "id":1,
-    //       "status": 'Pending',
-    //       "gameMode": {
-    //           "description": "Doubles",
-    //       },
-    //       "playerCount": 4,
-    //       "maxPlayers": 4,
-    //       "settings": {
-    //           "competitive": true,
-    //           "mode": "DOUBLES"
-    //       }
-    //   }
-    //];
-    //console.log(adminScrimsInTable);
     export let visible = false;
     let scrimManagementModalVisible = false;
     let targetId;
@@ -75,15 +55,16 @@
     <tbody>
     {#if activeScrimsData?.length}
         {#each activeScrimsData as scrim (scrim.id)}
+            {console.log(scrim)}
             <tr>
                 <td>{scrim.id}</td>
                 <td>{screamingSnakeToHuman(scrim.settings.mode)}</td>
                 <td>{scrim.status}</td>
-                <!--{#if scrim.players.length >= 1}-->
-                <!--    <td>{scrim.players.join(", ")}</td>-->
-                <!--    {:else}-->
-                <!--    <td>Empty Scrim</td>-->
-                <!--{/if}-->
+                {#if scrim.players?.length >= 1}
+                    <td>{scrim.players.join(", ")}</td>
+                {:else}
+                    <td>Empty Scrim</td>
+                {/if}
                 <td>
                     <button on:click={() => { openScrimManagementModal(scrim.id) }} class="btn btn-outline float-right lg:btn-sm">
                         Manage
