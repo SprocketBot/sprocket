6428aa1eca64e42f5d2323a0c0da8b63921023af
Zachary Stence <zachstence@gmail.com>
2022-06-04 14:35:08 -0500

    ci: where is requirements? pt2

--- microservices/replay-parse-service/src/main.py
+++ microservices/replay-parse-service/src/main.py
@@ -14,7 +14,7 @@ from progress import Progress
 import celeryconfig
 from kombu import Producer, Connection
 
-print("deploy attempt 4")
+print("deploy attempt 5")
 
 # Celery pipeline for starting jobs (broker) and returning results (backend)
 app = celery.Celery(config_source=celeryconfig)
