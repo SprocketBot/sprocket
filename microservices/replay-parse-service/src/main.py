29fc49812cf74d80e997af4d97b411e1eda4467b
Zachary Stence <zachstence@gmail.com>
2022-06-04 14:41:15 -0500

    ci: workdir

--- microservices/replay-parse-service/src/main.py
+++ microservices/replay-parse-service/src/main.py
@@ -14,7 +14,7 @@ from progress import Progress
 import celeryconfig
 from kombu import Producer, Connection
 
-print("deploy attempt 5")
+print("deploy attempt 6")
 
 # Celery pipeline for starting jobs (broker) and returning results (backend)
 app = celery.Celery(config_source=celeryconfig)
