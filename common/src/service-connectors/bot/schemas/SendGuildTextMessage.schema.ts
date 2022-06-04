a3eafb1724ad780cb96954968a59cfbc2bff4fec
Bryan Pikaard <bryan@pikaard.com>
2022-06-04 01:45:51 +0000

    feat: guild text message stuff

--- common/src/service-connectors/bot/schemas/SendGuildTextMessage.schema.ts
+++ common/src/service-connectors/bot/schemas/SendGuildTextMessage.schema.ts
@@ -3,8 +3,9 @@ import {z} from "zod";
 import {MessageContentSchema} from "../types";
 
 export const SendGuildTextMessage_Request = z.object({
+    organizationId: z.number(),
     channelId: z.string(),
-    content: z.union([z.string(), MessageContentSchema]),
+    content: MessageContentSchema,
 });
 
 export const SendGuildTextMessage_Response = z.boolean();
