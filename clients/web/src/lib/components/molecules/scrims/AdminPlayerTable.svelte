56edbde58e40406d93316cc09ddb71064a74cfc9
Jake Bailey <asaxplayinghorse@gmail.com>
2022-06-04 19:42:06 -0700

    fix: lint player table

--- clients/web/src/lib/components/molecules/scrims/AdminPlayerTable.svelte
+++ clients/web/src/lib/components/molecules/scrims/AdminPlayerTable.svelte
@@ -1,45 +1,51 @@
 <script lang="ts">
-    import {activePlayers, type Player} from "$lib/api";
-    import {PlayerManagementModal} from "../../organisms/index.js";
+    import { activePlayers, type Player } from "$lib/api";
+    import { PlayerManagementModal } from "../../organisms/index.js";
     import FaHammer from "svelte-icons/fa/FaHammer.svelte";
 
     // export let visible = false;
     let playerManagementModalVisible = false;
     let activePlayersData: Player[] | undefined;
     $: activePlayersData = $activePlayers;
-    let targetPlayer;
+    let targetPlayer: number;
     // export let selectedPlayer;
 
     const openPlayerManagementModal = (playerId: number) => {
         playerManagementModalVisible = true;
         targetPlayer = playerId;
     };
-
 </script>
 
 <table>
     <thead>
-    <tr>
-
-        <th>Player Name</th>
-        <th>Player ID</th>
-        <th><textarea class="input-bordered bg-base-300/20 w-3" rows="1" id:> Player Search: </textarea></th>
-    </tr>
+        <tr>
+            <th>Player Name</th>
+            <th>Player ID</th>
+            <th
+                ><textarea class="input-bordered bg-base-300/20 w-3" rows="1">
+                    Player Search:
+                </textarea></th
+            >
+        </tr>
     </thead>
     <tbody>
         {#if activePlayersData}
             {#each activePlayersData as player (player.id)}
                 <tr>
-
-                <td>{player.name}</td>
-                <td>{player.id}</td>
-                <td>
-                    <button class="btn btn-outline float-right lg:btn-sm" on:click={() => { openPlayerManagementModal(player.id) }}>
-                        <span class = "h-6">
-                            <FaHammer/>
-                        </span>
-                    </button>
-                </td>
+                    <td>{player.name}</td>
+                    <td>{player.id}</td>
+                    <td>
+                        <button
+                            class="btn btn-outline float-right lg:btn-sm"
+                            on:click={() => {
+                                openPlayerManagementModal(player.id);
+                            }}
+                        >
+                            <span class="h-6">
+                                <FaHammer />
+                            </span>
+                        </button>
+                    </td>
                 </tr>
             {/each}
         {/if}
@@ -47,7 +53,10 @@
 </table>
 
 {#if playerManagementModalVisible}
-    <PlayerManagementModal playerId={targetPlayer} bind:visible={playerManagementModalVisible} />
+    <PlayerManagementModal
+        playerId={targetPlayer}
+        bind:visible={playerManagementModalVisible}
+    />
 {/if}
 
 <style lang="postcss">
