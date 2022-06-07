04d100865cca163fb12526ee221dc4767b11a185
Brian D <contact@noimbrian.com>
2022-06-06 19:06:38 -0500

    feat: make navigation items dynamic

--- clients/web/src/routes/__layout.svelte
+++ clients/web/src/routes/__layout.svelte
@@ -1,32 +1,49 @@
-<script lang="ts">
-    import "../app.postcss";
-    import {setContext} from "svelte";
-    import {NavigationContextKey} from "$lib/types";
-    import type {NavigationItem} from "$lib/types/Navigation.js";
-    import {initializeClient} from "$lib/api/client";
-    import {session} from "$app/stores";
-    import {
-        AuthGuard, Chatwoot, ToastContainer,
-    } from "$lib/components";
+<script lang='ts' context='module'>
+	import type {NavigationItem} from "$lib/types/Navigation";
+	import type {Load} from "@sveltejs/kit";
 
-    setContext<NavigationItem[]>(NavigationContextKey, [
-        {
-            target: "/scrims",
-            label: "Play",
-        },
-        {
-            target: "/management",
-            label: "Manage",
-        },
-    ]);
+	export const load: Load = ({session}) => {
+	    const items: NavigationItem[] = [ {
+	        target: "/scrims",
+	        label: "Play",
+	    } ];
 
-    initializeClient($session);
+	    // 0 === MLEDB ADMIN
+	    if (session.user?.orgTeams.some(s => s === 0)) {
+	        items.push({
+	            target: "/management",
+	            label: "Manage",
+	        });
+	    }
+	    return {
+	        props: {
+	            navigationItems: items,
+	        },
+	    };
+	};
+
+</script>
+<script lang='ts'>
+	import "../app.postcss";
+	import {setContext} from "svelte";
+	import {NavigationContextKey} from "$lib/types";
+	import {initializeClient} from "$lib/api/client";
+	import {session} from "$app/stores";
+	import {
+	    AuthGuard, Chatwoot, ToastContainer,
+	} from "$lib/components";
+
+	export let navigationItems: NavigationItem[];
+
+	setContext<NavigationItem[]>(NavigationContextKey, navigationItems);
+
+	initializeClient($session);
 </script>
 
-<slot/>
+<slot />
 
-<ToastContainer showTestButton={false}/>
+<ToastContainer showTestButton={false} />
 
 <AuthGuard>
-    <Chatwoot/>
+	<Chatwoot />
 </AuthGuard>
