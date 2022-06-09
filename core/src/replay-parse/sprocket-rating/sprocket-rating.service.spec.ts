80d4890282913d976e48f9e682ca8a6fdb0d40ca
Jake Bailey <asaxplayinghorse@gmail.com>
2022-06-09 11:45:43 -0700

    feat: add unit tests for sprocket rating

--- core/src/replay-parse/sprocket-rating/sprocket-rating.service.spec.ts
+++ core/src/replay-parse/sprocket-rating/sprocket-rating.service.spec.ts
@@ -17,4 +17,41 @@ describe("SprocketRatingService", () => {
     it("should be defined", () => {
         expect(service).toBeDefined();
     });
+    
+    it("Should return all zeros", () => {
+        const input = {
+            goals: 0.0,
+            assists: 0.0,
+            shots: 0.0,
+            saves: 0.0,
+            goals_against: 0.0,
+            shots_against: 0.0,
+        };
+        const result = {
+            opi: 0.0, dpi: 0.0, gpi: 0.0,
+        };
+
+        expect(service.calcSprocketRating(input)).toStrictEqual(result);
+
+    });
+
+    it("Should give valid results", () => {
+        const input = {
+            goals: 1.0,
+            assists: 2.0,
+            shots: 7.0,
+            saves: 1.0,
+            goals_against: 4.0,
+            shots_against: 5.0,
+        };
+        const result = {
+            opi: 92.41236614475706,
+            dpi: 27.36595668648241,
+            gpi: 59.88916141561973,
+        };
+
+        expect(service.calcSprocketRating(input)).toStrictEqual(result);
+
+    });
+
 });
