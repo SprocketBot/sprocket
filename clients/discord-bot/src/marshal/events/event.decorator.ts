97fc9cfa896b81cdbe9bbfadf5b8569a4f1c1dd4
Bryan Pikaard <bryan@pikaard.com>
2022-06-09 00:43:25 +0000

    fix: remove useless stuff

--- clients/discord-bot/src/marshal/events/event.decorator.ts
+++ clients/discord-bot/src/marshal/events/event.decorator.ts
@@ -1,23 +1,8 @@
-import {Logger} from "@nestjs/common";
-
-import type {Marshal} from "../marshal";
 import {MarshalMetadataKey} from "../types";
-import type {
-    EventFunction, EventMeta, EventSpec,
-} from "./events.types";
+import type {EventMeta, EventSpec} from "./events.types";
 
-const logger = new Logger("EventDecorator");
 export const Event = (eventSpec: EventSpec): MethodDecorator => <T>(target: Object, key: string | symbol, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> => {
     if (!descriptor.value) throw new Error("Descriptor is undefined??");
-
-    // <  Decorator>
-    const originalMethod: EventFunction = descriptor.value as unknown as EventFunction;
-
-    // @t22s-expect-error If it was not a CommandFunction before, then it is using this decorator incorrectly
-    // descriptor.value = async function(this: Marshal, ...params: Parameters<EventFunction>): Promise<unknown> {
-        
-    // };
-    // </ Decorator>
 
     // <Metadata>
     const eventMeta: EventMeta = {
