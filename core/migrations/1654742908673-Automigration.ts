c22f9a33b8f20ca535513fe721fe214beb8a202e
Bryan Pikaard <bryan@pikaard.com>
2022-06-09 02:50:07 +0000

    chore: migration

---
+++ core/migrations/1654742908673-Automigration.ts
@@ -0,0 +1,24 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Automigration1654742908673 implements MigrationInterface {
+    name = 'Automigration1654742908673'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "sprocket"."organization_configuration_key_type_enum" AS ENUM('STRING', 'INTEGER', 'FLOAT', 'ARRAY_STRING')`);
+        await queryRunner.query(`ALTER TABLE "sprocket"."organization_configuration_key" ADD "type" "sprocket"."organization_configuration_key_type_enum" NOT NULL`);
+        await queryRunner.query(`ALTER TYPE "sprocket"."organization_configuration_key_code_enum" RENAME TO "organization_configuration_key_code_enum_old"`);
+        await queryRunner.query(`CREATE TYPE "sprocket"."organization_configuration_key_code_enum" AS ENUM('SCRIM_QUEUE_BAN_CHECKIN_TIMEOUT_MINUTES', 'SCRIM_QUEUE_BAN_INITIAL_DURATION_MINUTES', 'SCRIM_QUEUE_BAN_DURATION_MODIFIER', 'SCRIM_QUEUE_BAN_MODIFIER_FALL_OFF_DAYS', 'PRIMARY_DISCORD_GUILD_SNOWFLAKE', 'ALTERNATE_DISCORD_GUILD_SNOWFLAKES')`);
+        await queryRunner.query(`ALTER TABLE "sprocket"."organization_configuration_key" ALTER COLUMN "code" TYPE "sprocket"."organization_configuration_key_code_enum" USING "code"::"text"::"sprocket"."organization_configuration_key_code_enum"`);
+        await queryRunner.query(`DROP TYPE "sprocket"."organization_configuration_key_code_enum_old"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "sprocket"."organization_configuration_key_code_enum_old" AS ENUM('SCRIM_QUEUE_BAN_CHECKIN_TIMEOUT_MINUTES', 'SCRIM_QUEUE_BAN_INITIAL_DURATION_MINUTES', 'SCRIM_QUEUE_BAN_DURATION_MODIFIER', 'SCRIM_QUEUE_BAN_MODIFIER_FALL_OFF_DAYS')`);
+        await queryRunner.query(`ALTER TABLE "sprocket"."organization_configuration_key" ALTER COLUMN "code" TYPE "sprocket"."organization_configuration_key_code_enum_old" USING "code"::"text"::"sprocket"."organization_configuration_key_code_enum_old"`);
+        await queryRunner.query(`DROP TYPE "sprocket"."organization_configuration_key_code_enum"`);
+        await queryRunner.query(`ALTER TYPE "sprocket"."organization_configuration_key_code_enum_old" RENAME TO "organization_configuration_key_code_enum"`);
+        await queryRunner.query(`ALTER TABLE "sprocket"."organization_configuration_key" DROP COLUMN "type"`);
+        await queryRunner.query(`DROP TYPE "sprocket"."organization_configuration_key_type_enum"`);
+    }
+
+}
