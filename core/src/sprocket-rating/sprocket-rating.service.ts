c4e972887573205894d4c4123ec54baee695a479
Jake Bailey <asaxplayinghorse@gmail.com>
2022-06-14 15:43:38 -0700

    feat: squash bug in OPI formula

--- core/src/sprocket-rating/sprocket-rating.service.ts
+++ core/src/sprocket-rating/sprocket-rating.service.ts
@@ -10,18 +10,22 @@ export class SprocketRatingService {
         const OPI_goal_w = 1.0;
         const OPI_assist_w = 0.8;
         const OPI_shot_w = 0.2;
-
         const DPI_goal_w = 1.1;
         const DPI_saves_w = 0.5;
         const DPI_shot_w = 0.4;
 
-        const OPI_beta = -1 * Math.log(9);
-        const OPI_mu =  2.03;
-        const OPI_sigma = 1.38;
+        const G_2s = 1.5;
+        const A_2s = 0.75;
+        const Sv_2s = 1.75;
+        const Sh_2s = 3.75;
 
+        const xOPI_2s = 2.03;
+        const yOPI_2s = 1.38;
+        const xDPI_2s = 1.93;
+        const yDPI_2s = 0.85;
+
+        const OPI_beta = -1 * Math.log(9);
         const DPI_beta = OPI_beta;
-        const DPI_mu = 1.93;
-        const DPI_sigma = 0.85;
 
         const goals = core.goals ?? 0;
         const assists = core.assists ?? 0;
@@ -31,11 +35,11 @@ export class SprocketRatingService {
         const shots_against = core.shots_against ?? 0;
 
         if ((goals + assists + shots + saves + goals_against + shots_against) > 0) {
-            const opi_raw = (OPI_goal_w * (goals / 1.5)) + (OPI_assist_w * (assists / 0.75)) + (OPI_shot_w * (shots / 1.75));
-            const opi = 100.0 / (1 + Math.exp(OPI_beta * ((opi_raw - OPI_mu) / OPI_sigma)));
+            const opi_raw = (OPI_goal_w * (goals / G_2s)) + (OPI_assist_w * (assists / A_2s)) + (OPI_shot_w * (shots / Sh_2s));
+            const opi = 100.0 / (1 + Math.exp(OPI_beta * ((opi_raw - xOPI_2s) / yOPI_2s)));
 
-            const dpi_raw = (DPI_goal_w * (2.0 - (goals_against / 2.0 / 1.5))) + (DPI_saves_w * (saves / 1.75)) + (DPI_shot_w * (2.0 - (shots_against / 2.0 / 3.75)));
-            const dpi = 100.0 / (1 + Math.exp(DPI_beta * ((dpi_raw - DPI_mu) / DPI_sigma)));
+            const dpi_raw = (DPI_goal_w * (2.0 - (goals_against / 2.0 / G_2s))) + (DPI_saves_w * (saves / Sv_2s)) + (DPI_shot_w * (2.0 - (shots_against / 2.0 / Sh_2s)));
+            const dpi = 100.0 / (1 + Math.exp(DPI_beta * ((dpi_raw - xDPI_2s) / yDPI_2s)));
 
             const gpi = (opi + dpi) / 2.0;
 
