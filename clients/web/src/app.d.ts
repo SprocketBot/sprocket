91573d06c3983b71bc80b191a381183fda9f865d
Zachary Stence <zachstence@gmail.com>
2022-07-24 18:32:01 -0500

    fix: chatwoot integration

--- clients/web/src/app.d.ts
+++ clients/web/src/app.d.ts
@@ -5,21 +5,26 @@
 // for information about these interfaces
 import type {SessionUser} from "$lib/types/SessionUser";
 
+import type {ChatwootSDK} from "$lib/components/abstract/Chatwoot";
+import type {Config} from "./lib/utils/types";
+
 declare global {
     declare namespace App {
         interface Locals {
             user: SessionUser;
             token: string;
         }
-        // interface Platform {}
+
         interface Session {
             config: Config;
     
             token?: string;
             user?: SessionUser;
         }
-    
-        // interface Stuff {}
+    }
+
+    declare interface Window {
+        chatwootSDK?: ChatwootSDK;
     }
 }
 
--- clients/web/src/lib/components/abstract/Chatwoot.svelte
+++ clients/web/src/lib/components/abstract/Chatwoot.svelte
@@ -1,25 +1,27 @@
-<script>
+<script lang="ts">
     import {browser} from "$app/env";
     import {onMount} from "svelte";
+    import {session} from "$app/stores";
+
+    const {
+        enabled, url, websiteToken,
+    } = $session.config.chatwoot;
 
     onMount(() => {
-        if (browser && !window.location.origin.includes("localhost")) {
-            (function(d, t) {
-                const BASE_URL = "https://chatwoot.spr.ocket.cloud";
-                const g = d.createElement(t),
-                      s = d.getElementsByTagName(t)[0];
-                g.src = `${BASE_URL}/packs/js/sdk.js`;
+        if (browser && enabled) {
+            (function() {
+                const g = document.createElement("script");
+                g.src = `${url}/packs/js/sdk.js`;
                 g.defer = true;
                 g.async = true;
-                s.parentNode.insertBefore(g, s);
+                document.body.append(g);
                 g.onload = function() {
-                    window.chatwootSDK.run({
-                        websiteToken: "2A4URw1JquVYpjKGkpzWEYQH",
-                        baseUrl: BASE_URL,
-                        darkMode: "auto",
+                    window.chatwootSDK?.run({
+                        websiteToken: websiteToken,
+                        baseUrl: url,
                     });
                 };
-            })(document, "script");
+            })();
         }
     });
 </script>
