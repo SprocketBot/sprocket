bb91ee79c9fa3861fdf7324e9c280b6c5636c1b1
Zachary Stence <zachstence@gmail.com>
2022-07-28 20:07:51 -0500

    ci: vite exclude crypto

--- clients/web/vite.config.js
+++ clients/web/vite.config.js
@@ -4,13 +4,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	optimizeDeps: {
-		exclude: ["src/lib/config.json"],
+		exclude: ["src/lib/config.json", "crypto"],
 		allowNodeBuiltins: ['fs']
 	},
 	server: {
 		port: 3000
 	}
-
 };
 
 export default config;
