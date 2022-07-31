82497b3890fa3c4a18f022d77143540d4bbb15e9
Bryan Pikaard <bryan@pikaard.com>
2022-07-31 00:48:07 +0000

    feat: scrim ratifying notification

---
+++ microservices/notification-service/src/submission/submission.event-subscriber.ts
@@ -0,0 +1,31 @@
+import {Injectable, Logger} from "@nestjs/common";
+import type {EventResponse} from "@sprocketbot/common";
+import {
+    EventsService, EventTopic,
+} from "@sprocketbot/common";
+
+import {SubmissionService} from "./submission.service";
+
+@Injectable()
+export class SubmissionEventSubscriber {
+    private readonly logger = new Logger(SubmissionEventSubscriber.name);
+
+    constructor(
+        private readonly eventsService: EventsService,
+        private readonly submissionService: SubmissionService,
+    ) {}
+
+    async onApplicationBootstrap(): Promise<void> {
+        await this.eventsService.subscribe(EventTopic.SubmissionRatifying, false).then(obs => {
+            obs.subscribe(this.onSubmissionRatifying);
+        });
+    }
+
+    onSubmissionRatifying = (d: EventResponse<EventTopic.SubmissionRatifying>): void => {
+        this.logger.error("SUBMISSION RATIFYING");
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        if (d.topic !== EventTopic.SubmissionRatifying) return;
+
+        this.submissionService.sendSubmissionRatifyingNotifications(d.payload).catch(e => { this.logger.error(e) });
+    };
+}
---
+++ microservices/notification-service/src/submission/submission.service.ts
@@ -0,0 +1,106 @@
+import {Injectable, Logger} from "@nestjs/common";
+import type {
+    EventPayload,
+    EventTopic,
+    MatchReplaySubmission,
+    ReplaySubmission,
+    ScrimReplaySubmission,
+} from "@sprocketbot/common";
+import {
+    BotEndpoint,
+    BotService,
+    ButtonComponentStyle,
+    ComponentType,
+    config,
+    CoreEndpoint,
+    CoreService,
+    MatchmakingEndpoint,
+    MatchmakingService,
+    RedisService,
+    ResponseStatus,
+} from "@sprocketbot/common";
+
+@Injectable()
+export class SubmissionService {
+    private readonly logger = new Logger(SubmissionService.name);
+
+    constructor(
+        private readonly botService: BotService,
+        private readonly coreService: CoreService,
+        private readonly redisService: RedisService,
+        private readonly matchmakingService: MatchmakingService,
+    ) {}
+
+    async sendSubmissionRatifyingNotifications(payload: EventPayload<EventTopic.SubmissionRatifying>): Promise<void> {
+        const submission = await this.redisService.getJson<ReplaySubmission>(payload.redisKey);
+        const [,, submissionId] = payload.redisKey.split(":");
+
+        this.logger.log(submissionId);
+
+        if (submissionId.startsWith("scrim")) {
+            await this.sendScrimSubmissionRatifyingNotifications(submission as ScrimReplaySubmission);
+        } else if (submissionId.startsWith("match")) {
+            await this.sendMatchSubmissionRatifyingNotifications(submission as MatchReplaySubmission);
+        }
+    }
+
+    async sendScrimSubmissionRatifyingNotifications(submission: ScrimReplaySubmission): Promise<void> {
+        const scrimResult = await this.matchmakingService.send(MatchmakingEndpoint.GetScrim, submission.scrimId);
+        if (scrimResult.status === ResponseStatus.ERROR) throw scrimResult.error;
+        const scrim = scrimResult.data;
+        
+        const organizationBrandingResult = await this.coreService.send(CoreEndpoint.GetOrganizationProfile, {id: scrim.organizationId});
+        if (organizationBrandingResult.status === ResponseStatus.ERROR) throw organizationBrandingResult.error;
+
+        await Promise.all(scrim.players.map(async p => {
+            const userResult = await this.coreService.send(CoreEndpoint.GetDiscordIdByUser, p.id);
+            if (userResult.status === ResponseStatus.ERROR) throw userResult.error;
+            if (!userResult.data) return;
+
+            await this.botService.send(BotEndpoint.SendDirectMessage, {
+                userId: userResult.data,
+                payload: {
+                    embeds: [ {
+                        title: "Your scrim is ready for ratification!",
+                        description: `Hey, ${p.name}! The replays uploaded for your ${organizationBrandingResult.data.name} scrim have finished processing and are ready to be ratified. Verify they are correct and ratify them [here](${config.web.url}/scrims).`,
+                        author: {
+                            name: `${organizationBrandingResult.data.name} Scrims`,
+                        },
+                        footer: {
+                            text: organizationBrandingResult.data.name,
+                        },
+                        timestamp: Date.now(),
+                    } ],
+                    components: [ {
+                        type: ComponentType.ACTION_ROW,
+                        components: [
+                            {
+                                type: ComponentType.BUTTON,
+                                style: ButtonComponentStyle.LINK,
+                                label: "Ratify here!",
+                                url: `${config.web.url}/scrims`,
+                            },
+                        ],
+                    } ],
+                },
+                brandingOptions: {
+                    organizationId: scrim.organizationId,
+                    options: {
+                        author: {
+                            icon: true,
+                        },
+                        color: true,
+                        thumbnail: true,
+                        footer: {
+                            icon: true,
+                        },
+                    },
+                },
+            });
+        }));
+    }
+
+    async sendMatchSubmissionRatifyingNotifications(submission: MatchReplaySubmission): Promise<void> {
+        throw new Error(`Not implementation for match notifications - matchId=${submission.matchId}`);
+    }
+}
