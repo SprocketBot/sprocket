1fa25b70b99b416dd71c46d34c0344d89161d0f3
Bryan Pikaard <bryan@pikaard.com>
2022-08-12 00:37:08 +0000

    fix: remove old files

--- common/src/service-connectors/core/schemas/GetMleMatchStakeholdersBySprocketMatchId.schema.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod";
-
-export const GetMleMatchStakeholdersBySprocketMatchId_Request = z.object({
-    matchId: z.number(),
-});
-
-export const GetMleMatchStakeholdersBySprocketMatchId_Response = z.object({
-    discordIds: z.array(z.string()),
-});
--- common/src/service-connectors/core/schemas/GetOrganizationIdByMatchId.schema.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod";
-
-export const GetOrganizationIdByMatchId_Request = z.object({
-    matchId: z.number(),
-});
-
-export const GetOrganizationIdByMatchId_Response = z.object({
-    organizationId: z.number(),
-});
