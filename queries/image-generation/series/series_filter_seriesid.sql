0824eea4849709798fb8b87644ac304982625b40
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2022-08-20 21:01:00 -0700

    chore: only relevant series
    Some series don't have series replays yet, we can't proccess those

--- queries/image-generation/series/series_filter_seriesid.sql
+++ queries/image-generation/series/series_filter_seriesid.sql
@@ -1,15 +1,42 @@
+WITH
+match_info as (
+	SELECT 
+		s.id,
+		s.league,
+		s.mode,
+		f.home_name,
+		f.away_name,
+		f.match_id,
+		m.season,
+		m.match_number
+	FROM mledb.series s
+	INNER JOIN mledb.fixture f
+	ON s.fixture_id = f.id
+	INNER JOIN mledb.match m
+	ON f.match_id = m.id
+	WHERE s.fixture_id IS NOT null
+)
+
+
 SELECT
   CONCAT(
-    DATE_TRUNC('minute', se.created_at),
+    'S', mi.season, '-', mi.match_number,
     ' ',
-    LEFT(se.league, 1),
+    LEFT(mi.league, 1),
     'L ',
-    REPLACE(INITCAP(se.mode), '_', ' '),
-    ' '
+    CASE WHEN mi.mode = 'DOUBLES'
+	  THEN '2s'
+	  ELSE '3s'
+	END,
+    ' ',
+	mi.home_name, ' vs ', mi.away_name
   ) AS Description,
-  se.ID AS Value
-FROM
-	mledb.series se 
-	WHERE se.fixture_id IS NOT NULL
+  mi.ID AS Value
+FROM match_info mi 
+	INNER JOIN (
+		SELECT DISTINCT(sr.series_id)
+		FROM mledb.series_replay sr
+	) as se_sid
+	on mi.id = se_sid.series_id
 ORDER BY
-  se.ID DESC
+  mi.ID DESC
