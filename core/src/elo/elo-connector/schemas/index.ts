40ce4d42269469d7ced70af70faffca630ce8488
Jake Bailey <jbailey@castorengineering.com>
2022-09-04 22:30:27 -0700

    Update core/src/elo/elo-connector/schemas/index.ts
    Co-authored-by: Bryan Pikaard <bryan@pikaard.com>

--- core/src/elo/elo-connector/schemas/index.ts
+++ core/src/elo/elo-connector/schemas/index.ts
@@ -1,4 +1,4 @@
-export * from "./AddPlayer.schema";
+export * from "./AddPlayers.schema";
 export * from "./CalculateEloForMatch.schema";
 export * from "./CalculateEloForNcp.schema";
 export * from "./CalculateSalaries.schema";
