4554bfd67eae1e4914b8749ecd81b434d9afd4b8
Zachary Stence <zachstence@gmail.com>
2022-09-09 22:35:19 -0500

    feat: admin settings game feature toggles

---
+++ clients/web/src/lib/components/molecules/AdminSettings/AdminSettings.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import GameFeatureToggle from "./GameFeatureToggle.svelte";
+</script>
+
+
+<div>
+    <GameFeatureToggle label="Scrims" value={false} loading on:change={() => { console.log("scrims") }} />
+    <GameFeatureToggle label="Rankouts" value={true} loading on:change={() => { console.log("rankouts") }} />
+</div>
---
+++ clients/web/src/lib/components/molecules/AdminSettings/GameFeatureToggle.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    export let label: string;
+    export let value: boolean;
+    export let loading: boolean;
+
+    const id = `game-feature-toggle-${label}`;
+</script>
+
+
+<div>
+    <label for={id}>{label}:</label>
+    <span class:enabled={value} class:disabled={!value}>{value ? "Enabled" : "Disabled"}</span>
+    <button type="button" id={id} class:loading>{value ? "Disable" : "Enable"}</button>
+</div>
+
+
+<style lang="postcss">
+    div {
+        @apply flex items-center gap-4 p-1;
+
+        * {
+            @apply w-32;
+        }
+    }
+
+    label {
+        @apply text-right;
+    }
+
+    span {
+        @apply px-3 rounded-lg text-center;
+
+        &.enabled {
+            @apply text-green-500 font-bold;
+        }
+
+        &.disabled {
+            @apply text-red-500 font-bold;
+        }
+    }
+
+    button {
+        @apply btn btn-sm;
+    }
+</style>
