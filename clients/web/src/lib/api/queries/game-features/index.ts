1913cb84ae41eb1f1b4dda34ed64c3994a13059d
Zachary Stence <zachstence@gmail.com>
2022-09-09 23:33:48 -0500

    refactor: move scrim toggle to adminsettings

--- clients/web/src/lib/api/queries/game-features/index.ts
+++ clients/web/src/lib/api/queries/game-features/index.ts
@@ -1,1 +1,3 @@
 export * from "./GameFeature.store";
+export * from "./feature.types";
+export * from "./SetGameFeature.mutation";
--- clients/web/src/lib/components/molecules/AdminSettings/AdminSettings.svelte
+++ clients/web/src/lib/components/molecules/AdminSettings/AdminSettings.svelte
@@ -1,30 +1,55 @@
 <script lang="ts">
     import GameFeatureToggle from "./GameFeatureToggle.svelte";
-    import { GameFeatureStore } from "$lib/api";
-    import { FeatureCode } from "$lib/api/queries/game-features/feature.types";
-    import { setGameFeatureMutation } from "$lib/api/queries/game-features/SetGameFeature.mutation";
+    import {
+        GameFeatureStore, FeatureCode, setGameFeatureMutation, scrimsDisabled, setScrimsDisabledMutation,
+    } from "$lib/api";
 
     const ROCKET_LEAGUE_GAME_ID = 2;
 
+    // =================================
+    // State
+    // =================================
+    let scrimsLoading = false;
+    let scrimsEnabled: boolean | undefined;
+    $: scrimsEnabled = $scrimsDisabled.data ? !$scrimsDisabled.data.getScrimsDisabled : undefined;
+    
     let rankoutsLoading = false;
-    const rankoutsEnabledStore = new GameFeatureStore({code: FeatureCode.AUTO_RANKOUTS, gameId: ROCKET_LEAGUE_GAME_ID})
+    const rankoutsEnabledStore = new GameFeatureStore({code: FeatureCode.AUTO_RANKOUTS, gameId: ROCKET_LEAGUE_GAME_ID});
     let rankoutsEnabled: boolean | undefined;
     $: rankoutsEnabled = $rankoutsEnabledStore.data?.getFeatureEnabled;
+    
+    // =================================
+    // Handlers
+    // =================================
+    const toggleScrims = async () => {
+        scrimsLoading = true;
+        try {
+            await setScrimsDisabledMutation({disabled: Boolean(scrimsEnabled)});
+        } catch (e) {
+            console.log("Failed to toggle scrims");
+        } finally {
+            scrimsLoading = false;
+        }
+    };
 
     const toggleRankouts = async () => {
         rankoutsLoading = true;
-        await setGameFeatureMutation({
-            code: FeatureCode.AUTO_RANKOUTS,
-            gameId: ROCKET_LEAGUE_GAME_ID,
-            value: !rankoutsEnabled,
-        })
+        try {
+            await setGameFeatureMutation({
+                code: FeatureCode.AUTO_RANKOUTS,
+                gameId: ROCKET_LEAGUE_GAME_ID,
+                value: !rankoutsEnabled,
+            });
+        } catch (e) {
+            console.log("Failed to toggle rankouts", e);
+        }
         rankoutsEnabledStore.invalidate();
         rankoutsLoading = false;
-    }
+    };
 </script>
 
 
 <div>
-    <!-- <GameFeatureToggle label="Scrims" value={undefined} on:change={() => { console.log("scrims") }} /> -->
+    <GameFeatureToggle label="Scrims" value={scrimsEnabled} loading={scrimsLoading} on:toggle={toggleScrims} />
     <GameFeatureToggle label="Rankouts" value={rankoutsEnabled} loading={rankoutsLoading} on:toggle={toggleRankouts} />
 </div>
