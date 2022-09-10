5498c3bd828610c25b58e050cf640e09dc0906f4
Jake Bailey <asaxplayinghorse@gmail.com>
2022-09-10 14:07:46 -0700

    feat: add admin rankout command to player resolver

--- core/src/franchise/player/player.resolver.ts
+++ core/src/franchise/player/player.resolver.ts
@@ -1,21 +1,44 @@
 /* eslint-disable require-atomic-updates */
 import {
+    forwardRef, Inject,
+} from "@nestjs/common";
+import {
+    Mutation,
     ResolveField, Resolver, Root,
 } from "@nestjs/graphql";
+import {InjectRepository} from "@nestjs/typeorm";
+import {
+    EventsService, EventTopic, NotificationEndpoint, NotificationMessageType,
+    NotificationService, NotificationType,
+} from "@sprocketbot/common";
+import {Repository} from "typeorm";
 
 import type {GameSkillGroup} from "../../database";
-import {Member, Player} from "../../database";
-import {MledbPlayerService} from "../../mledb";
+import {
+    Member, Player, UserAuthenticationAccount, UserAuthenticationAccountType,
+} from "../../database";
+import type {ManualSkillGroupChange} from "../../elo/elo-connector";
+import {EloConnectorService, EloEndpoint} from "../../elo/elo-connector";
+import {OrganizationService} from "../../organization";
 import {PopulateService} from "../../util/populate/populate.service";
 import {FranchiseService} from "../franchise";
+import {GameSkillGroupService} from "../game-skill-group";
+import {PlayerService} from "./player.service";
 
 @Resolver(() => Player)
 export class PlayerResolver {
 
     constructor(
         private readonly popService: PopulateService,
-        private readonly mlePlayerService: MledbPlayerService,
+        private readonly playerService: PlayerService,
         private readonly franchiseService: FranchiseService,
+        private readonly skillGroupService: GameSkillGroupService,
+        private readonly eventsService: EventsService,
+        private readonly notificationService: NotificationService,
+        private readonly eloConnectorService: EloConnectorService,
+        @InjectRepository(UserAuthenticationAccount) private userAuthRepository: Repository<UserAuthenticationAccount>,
+        @Inject(forwardRef(() => OrganizationService)) private readonly organizationService: OrganizationService,
+
     ) {}
 
     @ResolveField()
@@ -54,4 +77,123 @@ export class PlayerResolver {
 
         return this.popService.populateOneOrFail(Player, player, "member");
     }
+
+    @Mutation()
+    async changePlayerSkillGroup(playerId: number, salary: number, skillGroupId: number): Promise<string> {
+        const player = await this.playerService.getPlayer({
+            where: {id: playerId},
+            relations: {
+                member: {
+                    user: {
+                        authenticationAccounts: true,
+                    },
+                    organization: true,
+                    profile: true,
+                },
+                skillGroup: {
+                    organization: true,
+                    game: true,
+                    profile: true,
+                },
+            },
+        });
+
+        const skillGroup = await this.skillGroupService.getGameSkillGroup({
+            where: {
+                id: skillGroupId,
+            },
+            relations: {
+                profile: true,
+            },
+        });
+
+        const discordAccount = await this.userAuthRepository.findOneOrFail({
+            where: {
+                user: {
+                    id: player.member.user.id,
+                },
+                accountType: UserAuthenticationAccountType.DISCORD,
+            },
+        });
+        const orgProfile = await this.organizationService.getOrganizationProfileForOrganization(player.member.organization.id);
+
+        if (player.skillGroup.id === skillGroupId) return "ERROR: You are already in this skill group";
+
+        const inputData: ManualSkillGroupChange = {
+            id: playerId,
+            salary: salary,
+            skillGroup: skillGroup.ordinal,
+        };
+        
+        await this.playerService.updatePlayerStanding(playerId, salary, skillGroupId);
+        await this.playerService.mle_rankDownPlayer(playerId, salary);
+        await this.eloConnectorService.createJob(EloEndpoint.SGChange, inputData);
+
+        await this.eventsService.publish(EventTopic.PlayerSkillGroupChanged, {
+            playerId: player.id,
+            name: player.member.profile.name,
+            organizationId: player.skillGroup.organizationId,
+            discordId: discordAccount.accountId,
+            old: {
+                id: player.skillGroup.id,
+                name: player.skillGroup.profile.description,
+                salary: Number(player.salary),
+                discordEmojiId: player.skillGroup.profile.discordEmojiId,
+            },
+            new: {
+                id: skillGroup.id,
+                name: skillGroup.profile.description,
+                salary: Number(salary),
+                discordEmojiId: skillGroup.profile.discordEmojiId,
+            },
+        });
+
+        await this.notificationService.send(NotificationEndpoint.SendNotification, {
+            type: NotificationType.BASIC,
+            userId: player.member.user.id,
+            notification: {
+                type: NotificationMessageType.DirectMessage,
+                userId: discordAccount.accountId,
+                payload: {
+                    embeds: [ {
+                        title: "You Have Ranked Out",
+                        description: `You have been ranked out from ${player.skillGroup.profile.description} to ${skillGroup.profile.description}.`,
+                        author: {
+                            name: `${orgProfile.name}`,
+                        },
+                        fields: [
+                            {
+                                name: "New League",
+                                value: `${skillGroup.profile.description}`,
+                            },
+                            {
+                                name: "New Salary",
+                                value: `${salary}`,
+                            },
+                        ],
+                        footer: {
+                            text: orgProfile.name,
+                        },
+                        timestamp: Date.now(),
+                    } ],
+                },
+                brandingOptions: {
+                    organizationId: player.member.organization.id,
+                    options: {
+                        author: {
+                            icon: true,
+                        },
+                        color: true,
+                        thumbnail: true,
+                        footer: {
+                            icon: true,
+                        },
+                    },
+                },
+            },
+        });
+
+        return "SUCCESS";
+    }
+
 }
