2c5a056908aae402d6a4380363f4ca0a06cfc55b
Zachary Stence <zachstence@gmail.com>
2022-09-13 10:33:48 -0500

    fix: dev.brick returns

--- clients/discord-bot/src/commands/administrator-commands/administrator-commands.module.ts
+++ clients/discord-bot/src/commands/administrator-commands/administrator-commands.module.ts
@@ -4,12 +4,12 @@ import {CoreModule, EventsModule} from "@sprocketbot/common";
 import {DiscordModule} from "../../discord/discord.module";
 import {EmbedModule} from "../../embed/embed.module";
 import {CommandsModule} from "../../marshal";
-import {CommandDecoratorTestMarshal} from "./command-decorator-test.marshal";
-import {DebugCommandsMarshal} from "./debug-commands.marshal";
+// import {CommandDecoratorTestMarshal} from "./command-decorator-test.marshal";
+// import {DebugCommandsMarshal} from "./debug-commands.marshal";
 import {DeveloperCommandsMarshal} from "./developer-commands.marshal";
-import {MiscCommandsMarshal} from "./misc-commands.marshal";
-import {SprocketConfigurationMarshal} from "./sprocket-configuration.marshal";
-import {SprocketStatusMarshal} from "./sprocket-status.marshal";
+// import {MiscCommandsMarshal} from "./misc-commands.marshal";
+// import {SprocketConfigurationMarshal} from "./sprocket-configuration.marshal";
+// import {SprocketStatusMarshal} from "./sprocket-status.marshal";
 
 @Module({
     imports: [
@@ -20,11 +20,12 @@ import {SprocketStatusMarshal} from "./sprocket-status.marshal";
         EventsModule,
     ],
     providers: [
-        DebugCommandsMarshal,
-        SprocketStatusMarshal,
-        MiscCommandsMarshal,
-        SprocketConfigurationMarshal,
-        CommandDecoratorTestMarshal,
+        // TODO scope by role before re-adding
+        // DebugCommandsMarshal,
+        // SprocketStatusMarshal,
+        // MiscCommandsMarshal,
+        // SprocketConfigurationMarshal,
+        // CommandDecoratorTestMarshal,
         DeveloperCommandsMarshal,
     ],
 })
--- clients/discord-bot/src/commands/commands.module.ts
+++ clients/discord-bot/src/commands/commands.module.ts
@@ -1,12 +1,11 @@
 import {Module} from "@nestjs/common";
 
-// import {AdministratorCommandsModule} from "./administrator-commands/administrator-commands.module";
+import {AdministratorCommandsModule} from "./administrator-commands/administrator-commands.module";
 import {MemberCommandsModule} from "./member-commands/member-commands.module";
 
 @Module({
     imports: [
-        // TODO scope by role before re-adding
-        // AdministratorCommandsModule,
+        AdministratorCommandsModule,
         MemberCommandsModule,
     ],
 })
