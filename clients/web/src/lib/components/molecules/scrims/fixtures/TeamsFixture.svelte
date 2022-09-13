77fb2348be3d2feac2eff80148f162b94133215d
Ace Starno <8878824+CodeRedJack@users.noreply.github.com>
2022-09-12 17:39:34 -0700

    Fix: updated Teams fixture to show play all {number} games regardless of Game Mode.

--- clients/web/src/lib/components/molecules/scrims/fixtures/TeamsFixture.svelte
+++ clients/web/src/lib/components/molecules/scrims/fixtures/TeamsFixture.svelte
@@ -3,10 +3,11 @@
     import GameCard from "./GameCard.svelte";
 
     export let scrim: CurrentScrim;
+    const numberOfGames: number = scrim.games.length;
 </script>
 
 
-
-    <GameCard game={scrim.games[0]} title="Teams Series - Play All 5 Games"/>
+    
+    <GameCard game={scrim.games[0]} title="Teams Series - Play All {numberOfGames} Games"/>
 
 
