eba15aa1b129bdf1012840d31635b49d23a07d0b
Jake Bailey <asaxplayinghorse@gmail.com>
2022-09-17 15:58:23 -0700

    feat: add token refresh logic to front and backend

--- core/src/identity/auth/oauth/oauth.service.ts
+++ core/src/identity/auth/oauth/oauth.service.ts
@@ -1,22 +1,32 @@
-import {Injectable} from "@nestjs/common";
+import {Injectable, Logger} from "@nestjs/common";
 import {JwtService} from "@nestjs/jwt";
 
 import type {AccessToken, AuthPayload} from "./types";
 
 @Injectable()
 export class OauthService {
+    private readonly logger = new Logger(OauthService.name);
+
     constructor(private jwtService: JwtService) {}
 
     async login(user: AuthPayload): Promise<AccessToken> {
         const payload = {username: user.username, sub: user.userId};
         return {
-            access_token: this.jwtService.sign(payload),
+            access_token: this.jwtService.sign(payload, {expiresIn: "15m"}),
+            refresh_token: this.jwtService.sign(payload, {expiresIn: "7d"}),
         };
     }
 
     async loginDiscord(user: AuthPayload): Promise<AccessToken> {
         return {
-            access_token: this.jwtService.sign(user),
+            access_token: this.jwtService.sign(user, {expiresIn: "1m"}),
+            refresh_token: this.jwtService.sign(user, {expiresIn: "7d"}),
         };
+    }
+
+    async refreshTokens(user: AuthPayload, refreshToken: string): Promise<AccessToken> {
+        this.logger.verbose(refreshToken);
+        const tokens = await this.loginDiscord(user);
+        return tokens;
     }
 }
