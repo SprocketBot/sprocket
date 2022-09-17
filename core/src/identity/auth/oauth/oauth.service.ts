8e4e0b64eef092250931117743b4a758a653358f
Jake Bailey <asaxplayinghorse@gmail.com>
2022-09-17 16:00:17 -0700

    fix: adjust expiry time for access_token JWT

--- core/src/identity/auth/oauth/oauth.service.ts
+++ core/src/identity/auth/oauth/oauth.service.ts
@@ -12,14 +12,14 @@ export class OauthService {
     async login(user: AuthPayload): Promise<AccessToken> {
         const payload = {username: user.username, sub: user.userId};
         return {
-            access_token: this.jwtService.sign(payload, {expiresIn: "15m"}),
+            access_token: this.jwtService.sign(payload, {expiresIn: "30m"}),
             refresh_token: this.jwtService.sign(payload, {expiresIn: "7d"}),
         };
     }
 
     async loginDiscord(user: AuthPayload): Promise<AccessToken> {
         return {
-            access_token: this.jwtService.sign(user, {expiresIn: "1m"}),
+            access_token: this.jwtService.sign(user, {expiresIn: "30m"}),
             refresh_token: this.jwtService.sign(user, {expiresIn: "7d"}),
         };
     }
