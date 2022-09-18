051ef4cdc6214473ff1e40c1f5eb86f73295f1c5
Brian D <contact@noimbrian.com>
2022-09-18 11:24:19 -0500

    fix: all the things

--- core/src/mledb/mledb-scrim/mledb-finalization.service.ts
+++ core/src/mledb/mledb-scrim/mledb-finalization.service.ts
@@ -3,7 +3,11 @@ import {
 } from "@nestjs/common";
 import {InjectRepository} from "@nestjs/typeorm";
 import type {
-    BallchasingPlayer, BallchasingResponse, BallchasingTeam, ReplaySubmission, Scrim,
+    BallchasingPlayer,
+    BallchasingResponse,
+    BallchasingTeam,
+    ReplaySubmission,
+    Scrim,
 } from "@sprocketbot/common";
 import type {EntityManager} from "typeorm";
 import {Repository} from "typeorm";
@@ -101,10 +105,22 @@ export class MledbFinalizationService {
     }
 
     async saveScrim(submission: ScrimReplaySubmission, submissionId: string, em: EntityManager, scrimObject: Scrim): Promise<MLE_Scrim> {
+        // const mode = scrimObject.settings.teamSize === 2 ? LegacyGameMode.DOUBLES : LegacyGameMode.STANDARD;
+        const {mode, group} = await this.getLeagueAndMode(scrimObject);
         const scrim = em.create(MLE_Scrim);
         const series = em.create(MLE_Series);
 
-        const {mode, group} = await this.getLeagueAndMode(scrimObject);
+        scrim.series = series;
+        series.scrim = scrim;
+
+        series.league = group.profile.description.split(" ")[0].toUpperCase();
+        series.mode = {
+            1: LegacyGameMode.SOLO, 2: LegacyGameMode.DOUBLES, 3: LegacyGameMode.STANDARD,
+        }[mode.teamSize]!;
+
+        series.submissionTimestamp = new Date();
+        series.fullNcp = false;
+
         const author = await this.mlePlayerRepository.findOneOrFail({where: {id: -1} });
 
         scrim.mode = series.mode;
@@ -113,18 +129,10 @@ export class MledbFinalizationService {
         scrim.author = author;
         scrim.host = author;
 
-        await em.save(scrim);
+        await em.insert(MLE_Scrim, scrim);
+        await em.insert(MLE_Series, series);
 
-        series.league = group.profile.description.split(" ")[0].toUpperCase();
-        series.mode = {
-            1: LegacyGameMode.SOLO, 2: LegacyGameMode.DOUBLES, 3: LegacyGameMode.STANDARD,
-        }[mode.teamSize]!;
-        series.scrim = scrim;
-
-        series.submissionTimestamp = new Date();
-        series.fullNcp = false;
-
-        await em.save(series);
+        await this.saveSeries(submission as ReplaySubmission, submissionId, em, series);
 
         if (scrimObject.settings.competitive) {
             const playerEligibilities = await Promise.all(scrimObject.players.map(async p => {
@@ -143,10 +151,8 @@ export class MledbFinalizationService {
                 return playerEligibility;
             }));
 
-            await em.save(playerEligibilities);
+            await em.insert(MLE_EligibilityData, playerEligibilities);
         }
-
-        await this.saveSeries(submission as ReplaySubmission, submissionId, em, series);
 
         return scrim;
     }
@@ -276,11 +282,10 @@ export class MledbFinalizationService {
             sr.series_id = series.id;
         });
         series.seriesReplays = mleSeriesReplays;
-
-        await em.save(mleSeriesReplays);
-        await em.save(coreStats);
-        await em.save(playerStats);
-        await em.save(teamStats);
+        await em.insert(MLE_SeriesReplay, mleSeriesReplays);
+        await em.insert(MLE_PlayerStatsCore, coreStats);
+        await em.insert(MLE_PlayerStats, playerStats);
+        await em.insert(MLE_TeamCoreStats, teamStats);
 
         return series.id;
     }
