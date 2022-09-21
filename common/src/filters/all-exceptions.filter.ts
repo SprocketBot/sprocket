dc50bfba101df3470f268fbaeca10ee5d5ac693e
Zachary Stence <zachstence@gmail.com>
2022-09-20 19:49:09 -0500

    fix: global exception handlers

---
+++ common/src/filters/all-exceptions.filter.ts
@@ -0,0 +1,25 @@
+import {
+    type ExceptionFilter, Catch, Logger,
+} from "@nestjs/common";
+
+/**
+ * Catches any unhandled error in a NestJS microservice
+ *
+ * Should be used when in `main.ts` `bootstrap()` creating a new microservice
+ * @example
+ * async function bootstrap(): Promise<void> {
+ *     const app = await NestFactory.createMicroservice(AppModule, {
+ *         // ...
+ *     });
+ *     app.useGlobalFilters(new AllExceptionsFilter());
+ *     await app.listen();
+ * }
+ */
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+    private readonly logger = new Logger(AllExceptionsFilter.name);
+
+    catch(exception: unknown): void {
+        this.logger.error("Uncaught exception", exception);
+    }
+}
