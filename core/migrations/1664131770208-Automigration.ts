53aaac20c4ac6ddfb5a5325f62074a4774a9d39b
Bryan Pikaard <bryan@pikaard.com>
2022-09-25 18:57:31 +0000

    fix: add schema

--- core/migrations/1664131770208-Automigration.ts
+++ core/migrations/1664131770208-Automigration.ts
@@ -4,11 +4,11 @@ export class Automigration1664131770208 implements MigrationInterface {
     name = 'Automigration1664131770208'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "player" ALTER COLUMN "salary" TYPE double precision`);
+        await queryRunner.query(`ALTER TABLE "sprocket"."player" ALTER COLUMN "salary" TYPE double precision`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "player" ALTER COLUMN "salary" TYPE numeric`);
+        await queryRunner.query(`ALTER TABLE "sprocket"."player" ALTER COLUMN "salary" TYPE numeric`);
     }
     
 }
