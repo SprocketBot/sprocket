892d84bfba253720e1ae6e888cc4e2c272f59dc4
Bryan Pikaard <bryan@pikaard.com>
2022-09-25 18:57:07 +0000

    chore: migration

---
+++ core/migrations/1664131770208-Automigration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Automigration1664131770208 implements MigrationInterface {
+    name = 'Automigration1664131770208'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "player" ALTER COLUMN "salary" TYPE double precision`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "player" ALTER COLUMN "salary" TYPE numeric`);
+    }
+    
+}
