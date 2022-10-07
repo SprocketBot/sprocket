37420216d1a3a8f8ee317b46d9afc092df148bbd
Zachary Stence <zachstence@gmail.com>
2022-10-07 17:23:03 -0500

    chore: tailwind/postcss config paths to prevent strange svelte warnings

--- clients/web/svelte.config.js
+++ clients/web/svelte.config.js
@@ -1,5 +1,11 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const postcssConfigPath = join(__dirname, 'postcss.config.cjs')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +13,11 @@ const config = {
   // for more information about preprocessors
   preprocess: [
     preprocess({
-      postcss: true,
+      postcss: {
+        // The config path must be specified here since we are using a monorepo
+        // https://github.com/sveltejs/language-tools/blob/master/docs/preprocessors/other-css-preprocessors.md#postcss
+        configFilePath: postcssConfigPath,
+      },
     }),
   ],
 
