30cf1dc795fc66de41589ca407267df898b22cdf
Zachary Stence <zachstence@gmail.com>
2022-10-07 17:23:03 -0500

    chore: tailwind/postcss config paths to prevent strange svelte warnings

--- clients/web/postcss.config.cjs
+++ clients/web/postcss.config.cjs
@@ -1,13 +1,22 @@
+const path = require('node:path');
+
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const nesting = require('tailwindcss/nesting');
 
+const tailwindConfigPath = path.resolve(__dirname, './tailwind.config.cjs')
+
 const config = {
+  /**
+   * Plugin order matters!
+   * Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+   * But others, like autoprefixer, need to run after,
+   */
   plugins: [
     nesting,
-    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-    tailwindcss(),
-    //But others, like autoprefixer, need to run after,
+    // The config path must be specified here since we are using a monorepo
+    // https://github.com/sveltejs/language-tools/blob/master/docs/preprocessors/other-css-preprocessors.md#tailwindcss
+    tailwindcss(tailwindConfigPath),
     autoprefixer,
   ],
 };
