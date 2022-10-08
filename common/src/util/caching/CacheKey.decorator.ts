7cf99c1c15a22dde6cbddc79751e69ef19c49cf1
Zachary Stence <zachstence@gmail.com>
2022-10-07 22:22:17 -0500

    chore: lint common

--- common/src/util/caching/CacheKey.decorator.ts
+++ common/src/util/caching/CacheKey.decorator.ts
@@ -2,14 +2,15 @@ import {Logger} from "@nestjs/common";
 
 const logger = new Logger("CacheKey");
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 export function CacheKey(target: Object, name: string | symbol, parameterIndex: number): void {
     let existingKeys: number[] = [];
     try {
-        if (Reflect.getMetadataKeys(target).includes(`sprocketcommon-cachekey-${name}`)) {
-            existingKeys = Reflect.getMetadata(`sprocketcommon-cachekey-${name}`, target) as number[];
+        if (Reflect.getMetadataKeys(target).includes(`sprocketcommon-cachekey-${String(name)}`)) {
+            existingKeys = Reflect.getMetadata(`sprocketcommon-cachekey-${String(name)}`, target) as number[];
         }
         existingKeys.push(parameterIndex);
-        Reflect.defineMetadata(`sprocketcommon-cachekey-${name}`, existingKeys, target);
+        Reflect.defineMetadata(`sprocketcommon-cachekey-${String(name)}`, existingKeys, target);
     } catch (e) {
         logger.error("Failed to create CacheKey", e);
         throw e;
