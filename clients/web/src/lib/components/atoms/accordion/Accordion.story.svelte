38e412449653eefbd590671aa2e15bb402b89839
Zachary Stence <zachstence@gmail.com>
2022-10-09 13:20:21 -0500

    chore: format

--- clients/web/src/lib/components/atoms/accordion/Accordion.story.svelte
+++ clients/web/src/lib/components/atoms/accordion/Accordion.story.svelte
@@ -1,45 +1,49 @@
 <script lang="ts">
-  import type { Hst as _Hst } from '@histoire/plugin-svelte'
-  import Accordion from './Accordion.svelte';
+    import type {Hst as _Hst} from "@histoire/plugin-svelte";
+    import Accordion from "./Accordion.svelte";
 
-  export let Hst: _Hst
+    export let Hst: _Hst;
 </script>
 
 <Hst.Story title="Atoms/Accordion">
-  <Hst.Variant title="Default">
-    <Accordion>
-      <span slot="title">Accordion 1</span>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime nulla dolorem non accusamus
-        aperiam, distinctio earum placeat debitis vel repellat ex quia aut nostrum facere error
-        veritatis. Iusto, dolorum quaerat?
-      </p>
-    </Accordion>
-    <Accordion>
-      <span slot="title">Accordion 2</span>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime nulla dolorem non accusamus
-        aperiam, distinctio earum placeat debitis vel repellat ex quia aut nostrum facere error
-        veritatis. Iusto, dolorum quaerat?
-      </p>
-    </Accordion>
-  </Hst.Variant>
-  <Hst.Variant title="Flush">
-    <Accordion flush>
-      <span slot="title">Accordion 1</span>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime nulla dolorem non accusamus
-        aperiam, distinctio earum placeat debitis vel repellat ex quia aut nostrum facere error
-        veritatis. Iusto, dolorum quaerat?
-      </p>
-    </Accordion>
-    <Accordion flush>
-      <span slot="title">Accordion 2</span>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime nulla dolorem non accusamus
-        aperiam, distinctio earum placeat debitis vel repellat ex quia aut nostrum facere error
-        veritatis. Iusto, dolorum quaerat?
-      </p>
-    </Accordion>
-  </Hst.Variant>
+    <Hst.Variant title="Default">
+        <Accordion>
+            <span slot="title">Accordion 1</span>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+                nulla dolorem non accusamus aperiam, distinctio earum placeat
+                debitis vel repellat ex quia aut nostrum facere error veritatis.
+                Iusto, dolorum quaerat?
+            </p>
+        </Accordion>
+        <Accordion>
+            <span slot="title">Accordion 2</span>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+                nulla dolorem non accusamus aperiam, distinctio earum placeat
+                debitis vel repellat ex quia aut nostrum facere error veritatis.
+                Iusto, dolorum quaerat?
+            </p>
+        </Accordion>
+    </Hst.Variant>
+    <Hst.Variant title="Flush">
+        <Accordion flush>
+            <span slot="title">Accordion 1</span>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+                nulla dolorem non accusamus aperiam, distinctio earum placeat
+                debitis vel repellat ex quia aut nostrum facere error veritatis.
+                Iusto, dolorum quaerat?
+            </p>
+        </Accordion>
+        <Accordion flush>
+            <span slot="title">Accordion 2</span>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+                nulla dolorem non accusamus aperiam, distinctio earum placeat
+                debitis vel repellat ex quia aut nostrum facere error veritatis.
+                Iusto, dolorum quaerat?
+            </p>
+        </Accordion>
+    </Hst.Variant>
 </Hst.Story>
