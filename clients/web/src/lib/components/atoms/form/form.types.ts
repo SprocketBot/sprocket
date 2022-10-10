4cd387be05ddc1eac2e42f021f5fcf47a4248d99
Zachary Stence <zachstence@gmail.com>
2022-10-10 18:21:33 -0500

    feat: input state styling

---
+++ clients/web/src/lib/components/atoms/form/form.types.ts
@@ -0,0 +1,2 @@
+/** Indicates whether a form control contains a value that is valid, invalid, or not yet validated (or contains no data) */
+export type FormControlState = "valid" | "invalid" | "none";
