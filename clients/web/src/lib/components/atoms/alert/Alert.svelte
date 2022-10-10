931d56613a9e5999e401a701f227da3cbd52c7e9
Zachary Stence <zachstence@gmail.com>
2022-10-10 12:25:22 -0500

    fix: clean up alert

--- clients/web/src/lib/components/atoms/alert/Alert.svelte
+++ clients/web/src/lib/components/atoms/alert/Alert.svelte
@@ -36,15 +36,7 @@
 
 <!-- https://flowbite.com/docs/components/alerts/ -->
 {#if !dismissed}
-    <div
-        role="alert"
-        class="p-4 mb-4 border rounded-lg"
-        class:info={variant === "info"}
-        class:success={variant === "success"}
-        class:warning={variant === "warning"}
-        class:error={variant === "error"}
-        out:fade
-    >
+    <div role="alert" class="v-{variant} p-4 mb-4 border rounded-lg" out:fade>
         <div class="flex items-center">
             {#if withIcon && iconSrc}
                 <Icon class="w-5 h-5 mr-2 flex-shrink-0" src={iconSrc} />
@@ -58,13 +50,7 @@
             </h3>
 
             {#if dismissible}
-                <button
-                    on:click={() => dismissed = true}
-                    type="button"
-                    class="ml-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 p-1.5 inline-flex h-8 w-8"
-                    data-dismiss-target="#alert-1"
-                    aria-label="Close"
-                >
+                <button on:click={() => dismissed = true} type="button" class="ml-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 p-1.5 inline-flex h-8 w-8">
                     <span class="sr-only">Close</span>
                     <Icon class="w-5 h-5" src={XMark} />
                 </button>
@@ -83,22 +69,22 @@
 
 
 <style lang="postcss">
-    .info {
+    .v-info {
         @apply border-blue-300 bg-blue-300 text-blue-900;
         button { @apply text-blue-800 focus:ring-blue-400 hover:bg-blue-400/50; }
     }
     
-    .success {
+    .v-success {
         @apply border-green-300 bg-green-300 text-green-900;
         button { @apply text-green-800 focus:ring-green-400 hover:bg-green-400/50; }
     }
 
-    .warning {
+    .v-warning {
         @apply border-orange-300 bg-orange-300 text-orange-900;
         button { @apply text-orange-800 focus:ring-orange-400 hover:bg-orange-400/50; }
     }
 
-    .error {
+    .v-error {
         @apply border-red-300 bg-red-300 text-red-900;
         button { @apply text-red-800 focus:ring-red-400 hover:bg-red-400/50; }
     }
