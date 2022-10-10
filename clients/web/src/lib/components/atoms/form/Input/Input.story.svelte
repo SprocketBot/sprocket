342ad5634dca3b02d513446daa7d031ba53a30bb
Zachary Stence <zachstence@gmail.com>
2022-10-10 15:27:40 -0500

    feat: basic input with size option

---
+++ clients/web/src/lib/components/atoms/form/Input/Input.story.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type {Hst as _Hst} from "@histoire/plugin-svelte";
+    import Input, {type InputSize} from "./Input.svelte";
+
+    export let Hst: _Hst;
+
+    const sizes: InputSize[] = ["sm", "md", "lg"];
+
+    let size: InputSize = "md";
+</script>
+
+<Hst.Story title="Atoms/Input" layout={{type: "grid", width: 500}}>
+    <svelte:fragment slot="controls">
+        <Hst.Select title="Size" bind:value={size} options={sizes} />
+    </svelte:fragment>
+
+    <Hst.Variant title="Default" {size}>
+        <Input label="This is an input" {size} />
+    </Hst.Variant>
+</Hst.Story>
---
+++ clients/web/src/lib/components/atoms/form/Input/Input.svelte
@@ -0,0 +1,39 @@
+<script lang="ts" context="module">
+    export type InputSize = "sm" | "md" | "lg";
+</script>
+
+<script lang="ts">
+    import shortid from "shortid";
+
+    export let label: string;
+    export let size: InputSize = "md";
+
+    const id = shortid.generate();
+</script>
+
+<div class="size-{size}">
+    <label for="input-{id}">{label}</label>
+    <input type="text" id="input-{id}" />
+</div>
+
+<style lang="postcss">
+    label {
+        @apply block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300;
+    }
+
+    input {
+        @apply block w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-info-500 focus:border-info-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-info-500 dark:focus:border-info-500;
+    }
+
+    .size-sm input {
+        @apply p-2 sm:text-xs;
+    }
+
+    .size-md input {
+        @apply p-2.5 text-sm;
+    }
+
+    .size-lg input {
+        @apply p-4 sm:text-base;
+    }
+</style>
