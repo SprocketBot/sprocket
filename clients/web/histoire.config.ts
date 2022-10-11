1ba01118d9fd778ed8841c50808b59c7edf5eeaa
Zachary Stence <zachstence@gmail.com>
2022-10-11 09:20:31 -0500

    feat: customize histoire colors

--- clients/web/histoire.config.ts
+++ clients/web/histoire.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from 'histoire'
-import { HstSvelte } from '@histoire/plugin-svelte'
+import {defineConfig} from "histoire";
+import {HstSvelte} from "@histoire/plugin-svelte";
+
+import tailwind from "./tailwind.config.cjs";
+const {primary, gray} = tailwind.theme.colors;
 
 export default defineConfig({
-  plugins: [
-    HstSvelte(),
-  ],
-  // https://histoire.dev/guide/config.html#global-js-and-css
-  setupFile: './src/histoire.setup.ts',
-})
+    plugins: [HstSvelte()],
+
+    // https://histoire.dev/guide/config.html#global-js-and-css
+    setupFile: "./src/histoire.setup.ts",
+
+    theme: {
+        title: "Histoire | Sprocket",
+        colors: {
+            primary: primary,
+            gray: {
+                ...gray,
+                750: "#242424",
+                850: "#151515",
+                950: "#070707",
+            },
+        },
+    },
+});
