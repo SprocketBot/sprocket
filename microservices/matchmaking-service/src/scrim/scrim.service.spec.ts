946589fdb1664abb233abb73a4dcf235de0fd7f8
Bryan Pikaard <bryan@pikaard.com>
2022-10-14 23:24:12 +0000

    feat: tests!

--- microservices/matchmaking-service/src/scrim/scrim.service.spec.ts
+++ microservices/matchmaking-service/src/scrim/scrim.service.spec.ts
@@ -33,15 +33,21 @@ describe("ScrimService", () => {
     let analyticsService: AnalyticsService;
     let scrimGroupService: ScrimGroupService;
 
+    let publishScrimUpdate: jest.SpyInstance;
+
     let playerInAnyScrim: jest.SpyInstance;
     let createScrim: jest.SpyInstance;
     let updatePlayer: jest.SpyInstance;
     let getScrim: jest.SpyInstance;
     let addPlayerToScrim: jest.SpyInstance;
     let removePlayerFromScrim: jest.SpyInstance;
+    let removeScrim: jest.SpyInstance;
+    let updateScrimStatus: jest.SpyInstance;
+    let updateScrimUnlockedStatus: jest.SpyInstance;
 
     let popScrim: jest.SpyInstance;
     let deleteScrim: jest.SpyInstance;
+    let startScrim: jest.SpyInstance;
 
     let resolveGroupKey: jest.SpyInstance;
 
@@ -67,7 +73,9 @@ describe("ScrimService", () => {
         
         jest.spyOn(eventProxyService, "publish").mockImplementationOnce(async () => true);
         jest.spyOn(analyticsService, "send").mockImplementationOnce(async () => ({} as AnalyticsResponse<AnalyticsEndpoint.Analytics>));
-        jest.spyOn(service, "publishScrimUpdate").mockImplementationOnce(async () => ({} as Scrim));
+
+        publishScrimUpdate = jest.spyOn(service, "publishScrimUpdate");
+        publishScrimUpdate.mockImplementationOnce(async () => ({} as Scrim));
 
         playerInAnyScrim = jest.spyOn(scrimCrudService, "playerInAnyScrim");
         createScrim = jest.spyOn(scrimCrudService, "createScrim");
@@ -75,9 +83,13 @@ describe("ScrimService", () => {
         getScrim = jest.spyOn(scrimCrudService, "getScrim");
         addPlayerToScrim = jest.spyOn(scrimCrudService, "addPlayerToScrim");
         removePlayerFromScrim = jest.spyOn(scrimCrudService, "removePlayerFromScrim");
+        removeScrim = jest.spyOn(scrimCrudService, "removeScrim");
+        updateScrimStatus = jest.spyOn(scrimCrudService, "updateScrimStatus");
+        updateScrimUnlockedStatus = jest.spyOn(scrimCrudService, "updateScrimUnlockedStatus");
 
         popScrim = jest.spyOn(scrimLogicService, "popScrim");
         deleteScrim = jest.spyOn(scrimLogicService, "deleteScrim");
+        startScrim = jest.spyOn(scrimLogicService, "startScrim");
 
         resolveGroupKey = jest.spyOn(scrimGroupService, "resolveGroupKey");
     });
@@ -132,7 +144,7 @@ describe("ScrimService", () => {
                     leaveAfter: 1000,
                 },
             };
-            const scrim: Scrim = {
+            const expected: Scrim = {
                 id: "hello world!",
                 createdAt: someDate,
                 updatedAt: someDate,
@@ -146,11 +158,10 @@ describe("ScrimService", () => {
             };
             
             playerInAnyScrim.mockImplementationOnce(async () => false);
-            createScrim.mockImplementationOnce(async () => scrim);
+            createScrim.mockImplementationOnce(async () => expected);
             updatePlayer.mockImplementationOnce(async () => {});
 
             const actual = await service.createScrim(createScrimData);
-            const expected: Scrim = scrim;
 
             expect(actual).toEqual(expected);
         });
@@ -450,7 +461,10 @@ describe("ScrimService", () => {
         it("Should throw with ScrimNotFound when a player tires to leave a scrim that doesn't exist", async () => {
             getScrim.mockImplementationOnce(async () => null);
 
-            await expect(async () => service.leaveScrim("hi Nigel", 2)).rejects.toThrow(MatchmakingError.ScrimNotFound);
+            const actual = async (): Promise<Scrim> => service.leaveScrim("hi Nigel", 1);
+            const expected = MatchmakingError.ScrimNotFound;
+
+            await expect(actual).rejects.toThrow(expected);
         });
 
         it("Should throw with ScrimAlreadyInProgress when a player tires to leave a scrim that is no longer pending", async () => {
@@ -559,67 +573,300 @@ describe("ScrimService", () => {
 
     describe("Checking Into a Scrim", () => {
         it("Should throw with ScrimNotFound when a player tries to check into a scrim that doesn't exist", async () => {
+            getScrim.mockImplementationOnce(async () => null);
 
+            const actual = async (): Promise<Scrim> => service.checkIn("hi zach", 1);
+            const expected = MatchmakingError.ScrimNotFound;
+
+            await expect(actual).rejects.toThrow(expected);
         });
 
         it("Should throw with ScrimStatusNotPopped when a player tries to check into a scrim that isn't popped", async () => {
+            const scrim: Scrim = {
+                id: "scrim-hihellohowdy",
+                status: ScrimStatus.PENDING,
+            } as Scrim;
 
+            getScrim.mockImplementationOnce(async () => scrim);
+
+            const actual = async (): Promise<Scrim> => service.checkIn(scrim.id, 1);
+            const expected = MatchmakingError.ScrimStatusNotPopped;
+
+            await expect(actual).rejects.toThrow(expected);
         });
 
         it("Should throw with PlayerNotInScrim when a player tries to check into a scrim they are not in", async () => {
+            const scrim: Scrim = {
+                id: "scrim-hihellohowdy",
+                status: ScrimStatus.POPPED,
+                players: [
+                    players.hyper(someDate),
+                    players.tekssx(someDate),
+                ],
+            } as Scrim;
 
+            getScrim.mockImplementationOnce(async () => scrim);
+
+            const actual = async (): Promise<Scrim> => service.checkIn(scrim.id, players.shuckle(someDate).id);
+            const expected = MatchmakingError.PlayerNotInScrim;
+
+            await expect(actual).rejects.toThrow(expected);
         });
 
         it("Should throw with PlayerAlreadyCheckedIn when a player tries to check into a scrim they already checked into", async () => {
+            const scrim: Scrim = {
+                id: "scrim-hihellohowdy",
+                status: ScrimStatus.POPPED,
+                players: [
+                    players.hyper(someDate),
+                    {
+                        ...players.tekssx(someDate),
+                        checkedIn: true,
+                    },
+                ],
+            } as Scrim;
 
+            getScrim.mockImplementationOnce(async () => scrim);
+
+            const actual = async (): Promise<Scrim> => service.checkIn(scrim.id, players.tekssx(someDate).id);
+            const expected = MatchmakingError.PlayerAlreadyCheckedIn;
+
+            await expect(actual).rejects.toThrow(expected);
         });
 
         it("Player should be checked into the scrim", async () => {
+            const scrim: Scrim = {
+                id: "scrim-hihellohowdy",
+                status: ScrimStatus.POPPED,
+                players: [
+                    players.hyper(someDate),
+                    {
+                        ...players.tekssx(someDate),
+                        checkedIn: true,
+                    },
+                    players.shuckle(someDate),
+                    players.nigel(someDate),
+                ],
+            } as Scrim;
 
+            getScrim.mockImplementationOnce(async () => scrim);
+            updatePlayer.mockImplementationOnce(async () => {});
+
+            const expected = {
+                ...scrim,
+                players: [
+                    {
+                        ...players.hyper(someDate),
+                        checkedIn: true,
+                    },
+                    {
+                        ...players.tekssx(someDate),
+                        checkedIn: true,
+                    },
+                    players.shuckle(someDate),
+                    players.nigel(someDate),
+                ],
+            };
+
+            publishScrimUpdate.mockReset();
+            publishScrimUpdate.mockImplementationOnce(async () => expected);
+
+            const actual = await service.checkIn(scrim.id, players.hyper(someDate).id);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it("Scrim should be started", async () => {
+            const scrim: Scrim = {
+                id: "scrim-hihellohowdy",
+                status: ScrimStatus.POPPED,
+                players: [
+                    players.hyper(someDate),
+                    {
+                        ...players.tekssx(someDate),
+                        checkedIn: true,
+                    },
+                ],
+            } as Scrim;
+
+            getScrim.mockImplementationOnce(async () => scrim);
+            updatePlayer.mockImplementationOnce(async () => {});
+            startScrim.mockImplementationOnce(async () => {});
+
+            const expected = {
+                ...scrim,
+                players: [
+                    {
+                        ...players.hyper(someDate),
+                        checkedIn: true,
+                    },
+                    {
+                        ...players.tekssx(someDate),
+                        checkedIn: true,
+                    },
+                ],
+            };
+
+            publishScrimUpdate.mockReset();
+            publishScrimUpdate.mockImplementationOnce(async () => expected);
+
+            const actual = await service.checkIn(scrim.id, players.hyper(someDate).id);
+
+            expect(actual).toEqual(expected);
+            expect(startScrim).toHaveBeenCalledWith(expected);
         });
     });
 
     describe("Cancelling a Scrim", () => {
         it("Should throw with ScrimNotFound when someone attempts to cancel a scrim that doesn't exist", async () => {
-
+            getScrim.mockImplementationOnce(async () => null);
+    
+            const actual = async (): Promise<Scrim> => service.cancelScrim("hi shuckle");
+            const expected = MatchmakingError.ScrimNotFound;
+    
+            await expect(actual).rejects.toThrow(expected);
         });
 
         it("Scrim should be cancelled", async () => {
+            const scrim: Scrim = {
+                id: "scrim-hihellohowdy",
+            } as Scrim;
 
+            const expected = {
+                ...scrim,
+                status: ScrimStatus.CANCELLED,
+            };
+
+            getScrim.mockImplementationOnce(async () => expected);
+            removeScrim.mockImplementationOnce(async () => {});
+
+            const actual = await service.cancelScrim(expected.id);
+    
+            expect(actual).toEqual(expected);
+            expect(removeScrim).toHaveBeenCalledWith(scrim.id);
         });
     });
 
     describe("Completing a Scrim", () => {
         it("Should throw with ScrimNotFound when someone tries to complete a scrim that doesn't exist", async () => {
-
+            getScrim.mockImplementationOnce(async () => null);
+    
+            const actual = async (): Promise<Scrim> => service.completeScrim("hi shuckle");
+            const expected = MatchmakingError.ScrimNotFound;
+    
+            await expect(actual).rejects.toThrow(expected);
         });
 
         it("Should throw with ScrimSubmissionNotFound when someone tries to complete a scrim that has no submission", async () => {
+            const scrim: Scrim = {
+                id: "hi shuckle",
+            } as Scrim;
 
+            getScrim.mockImplementationOnce(async () => scrim);
+    
+            const actual = async (): Promise<Scrim> => service.completeScrim(scrim.id);
+            const expected = MatchmakingError.ScrimSubmissionNotFound;
+    
+            await expect(actual).rejects.toThrow(expected);
         });
 
         it("Should throw with PlayerNotInScrim when a player tries to complete a scrim that they are not in", async () => {
+            const scrim: Scrim = {
+                id: "hi shuckle",
+                players: [
+                    players.hyper(someDate),
+                    players.tekssx(someDate),
+                ],
+                submissionId: "howdy, shuckle!",
+            } as Scrim;
 
+            getScrim.mockImplementationOnce(async () => scrim);
+    
+            const actual = async (): Promise<Scrim> => service.completeScrim(scrim.id, players.shuckle(someDate).id);
+            const expected = MatchmakingError.PlayerNotInScrim;
+    
+            await expect(actual).rejects.toThrow(expected);
         });
 
         it("Scrim should be completed", async () => {
+            const scrim: Scrim = {
+                id: "hi shuckle",
+                players: [
+                    players.hyper(someDate),
+                    players.tekssx(someDate),
+                ],
+                submissionId: "howdy, shuckle!",
+            } as Scrim;
 
+            getScrim.mockImplementationOnce(async () => scrim);
+            removeScrim.mockImplementationOnce(async () => {});
+    
+            const actual = await service.completeScrim(scrim.id, players.hyper(someDate).id);
+            const expected = {
+                ...scrim,
+                status: ScrimStatus.COMPLETE,
+            };
+    
+            expect(actual).toEqual(expected);
         });
     });
 
     describe("Locking and Unlocking a Scrim", () => {
         it("Should throw with ScrimNotFound when someone tries to (un)lock a scrim that doesn't exist", async () => {
-
+            getScrim.mockImplementationOnce(async () => null);
+    
+            const actual = async (): Promise<Scrim> => service.setScrimLocked("bonjour!", false);
+            const expected = MatchmakingError.ScrimNotFound;
+    
+            await expect(actual).rejects.toThrow(expected);
         });
 
-        it("Scrim should be (un)locked", async () => {
+        it("Scrim should be unlocked", async () => {
+            const scrim: Scrim = {
+                id: "hi shuckle",
+                status: ScrimStatus.LOCKED,
+                unlockedStatus: ScrimStatus.IN_PROGRESS,
+            } as Scrim;
 
+            getScrim.mockImplementationOnce(async () => scrim);
+            updateScrimStatus.mockImplementationOnce(async () => {});
+    
+            const actual = await service.setScrimLocked(scrim.id, false);
+            const expected = {
+                ...scrim,
+                status: ScrimStatus.IN_PROGRESS,
+            };
+    
+            expect(actual).toEqual(expected);
+            expect(updateScrimStatus).toHaveBeenCalledWith(scrim.id, ScrimStatus.IN_PROGRESS);
+        });
+
+        it("Scrim should be locked", async () => {
+            const scrim: Scrim = {
+                id: "hi shuckle",
+                status: ScrimStatus.IN_PROGRESS,
+            } as Scrim;
+
+            getScrim.mockImplementationOnce(async () => scrim);
+            updateScrimUnlockedStatus.mockImplementationOnce(async () => {});
+            updateScrimStatus.mockImplementationOnce(async () => {});
+    
+            const actual = await service.setScrimLocked(scrim.id, true);
+            const expected = {
+                ...scrim,
+                status: ScrimStatus.LOCKED,
+            };
+    
+            expect(actual).toEqual(expected);
+            expect(updateScrimStatus).toHaveBeenCalledWith(scrim.id, ScrimStatus.LOCKED);
+            expect(updateScrimUnlockedStatus).toHaveBeenCalledWith(scrim.id, ScrimStatus.IN_PROGRESS);
         });
     });
 
     describe("Developers", () => {
-        it("Zach should be happy", () => {
-            expect(true).toEqual(true);
-        });
+        it("Zach should be happy", () => {});
+        it("Nigel should be happy", () => {});
+        it("Shuckle should be...shuckle", () => {});
+        it("Hyper do be writing a lot of tests.....", () => {});
     });
 });
