9125836bdf60a76c767c7c686dd9e4241a48060b
Zachary Stence <zachstence@gmail.com>
2022-10-17 11:26:34 -0500

    fix: build

--- clients/discord-bot/src/marshal/commands/command-not-found.decorator.ts
+++ clients/discord-bot/src/marshal/commands/command-not-found.decorator.ts
@@ -1,4 +1,5 @@
 import {MarshalMetadataKey} from "../types";
+import {CommandFunction} from "./commands.types";
 
 export const CommandNotFound =
     (): MethodDecorator =>
@@ -15,7 +16,7 @@ export const CommandNotFound =
         descriptor.value = async function (
             // eslint-disable-next-line @typescript-eslint/ban-types
             this: Object,
-            ...params: Parameters<HookFunction>
+            ...params: Parameters<CommandFunction>
         ): Promise<unknown> {
             /*
              * TODO: Will nest guards work, or do we need our own system?
