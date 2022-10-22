5a5dcc052e3c4b30d5898d2e94e74ed76fe1e006
Bryan Pikaard <bryan@pikaard.com>
2022-10-22 23:51:54 +0000

    feat: remove resolver

--- core/src/authorization/authorization.module.ts
+++ core/src/authorization/authorization.module.ts
@@ -1,11 +1,10 @@
 import {Module} from "@nestjs/common";
 
 import {DatabaseModule} from "../database";
-import {AuthorizationResolver} from "./authorization.resolver";
 import {AuthorizationService} from "./authorization.service";
 
 @Module({
     imports: [DatabaseModule],
-    providers: [AuthorizationService, AuthorizationResolver],
+    providers: [AuthorizationService],
 })
 export class AuthorizationModule {}
