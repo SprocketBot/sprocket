a22ade8ce20523be68d55a89be60c34668d60aa5
Bryan Pikaard <bryan@pikaard.com>
2022-10-22 22:26:10 +0000

    fix: unused var

--- core/src/authentication/authentication.resolver.ts
+++ core/src/authentication/authentication.resolver.ts
@@ -2,9 +2,7 @@ import {UseGuards} from "@nestjs/common";
 import {Mutation, Resolver} from "@nestjs/graphql";
 
 import {AuthenticationService} from "./authentication.service";
-import {AuthenticatedUser} from "./decorators/authenticated-user.decorator";
 import {GraphQLJwtRefreshGuard} from "./strategies/jwt/guards";
-import {JwtRefreshPayload} from "./strategies/jwt/jwt.types";
 
 @Resolver()
 export class AuthenticationResolver {
@@ -12,7 +10,7 @@ export class AuthenticationResolver {
 
     @Mutation(() => String)
     @UseGuards(GraphQLJwtRefreshGuard)
-    async refreshLogin(@AuthenticatedUser() user: JwtRefreshPayload): Promise<string> {
+    async refreshLogin(): Promise<string> {
         return "not implemented";
     }
 }
