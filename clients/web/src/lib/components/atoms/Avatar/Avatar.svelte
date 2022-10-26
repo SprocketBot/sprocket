142f3abc973bff43bb9a5a93ff3f71a792c8ca01
Matt McCall <mattbear888@gmail.com>
2022-10-26 19:24:56 -0400

    feat: shape and size controls

--- clients/web/src/lib/components/atoms/Avatar/Avatar.svelte
+++ clients/web/src/lib/components/atoms/Avatar/Avatar.svelte
@@ -10,14 +10,41 @@
 </script>
 
 {#if url===undefined}
-    <div class="overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600">
-       <svg class="absolute -left-1 w-12 h-12 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+    <div class= "shape-{shape} size-{size} overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600">
+       <svg class="absolute w-full h-full text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
     </div>
 {:else}
-    <img class="w-10 h-10 rounded-full" src={url}/>
+    <img class="shape-{shape} size-{size} w-10 h-10 rounded-full" src={url}/>
 {/if}
 
+<style lang="postcss">
+    .shape-rounded {
+        @apply rounded;
+    }
 
+    .shape-square {
+        @apply rounded-none;
+    }
+
+    .shape-circle {
+        @apply rounded-full;
+    }
+    .size-sm{
+        @apply w-6 h-6;
+    }
+    .size-rg{
+        @apply w-8 h-8;
+    }
+    .size-md{
+        @apply w-12 h-12;
+    }
+    .size-lg{
+        @apply w-20 h-20;
+    }
+    .size-xl{
+        @apply w-36 h-36;
+    }
+</style>
 
 
 
