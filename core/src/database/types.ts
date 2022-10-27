499a896beb862221f47016b64da149801ad9a637
Bryan Pikaard <bryan@pikaard.com>
2022-10-27 00:04:02 +0000

    feat: similarity

--- core/src/database/types.ts
+++ core/src/database/types.ts
@@ -1,4 +1,5 @@
 export * from "./configuration/types";
+export * from "./extended-repositories/similarity.input-type";
 export * from "./game/types";
 export * from "./identity/types";
 export * from "./organization/types";
