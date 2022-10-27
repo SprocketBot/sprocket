d1db6032d6e9328260a07186ea95c4aa6c6015a4
Bryan Pikaard <bryan@pikaard.com>
2022-10-27 05:24:06 +0000

    fix: try/catch

--- common/src/celery/celery.service.ts
+++ common/src/celery/celery.service.ts
@@ -82,24 +82,23 @@ export class CeleryService {
         this.logger.debug(`Running celery task asynchronously name=${name} taskId=${taskId}`);
 
         if (opts?.cb) {
-            asyncResult.get()
-                .then((r: unknown) => {
-                    const result = this.parseResult(task, r);
-                    const p = opts.cb!(taskId, result, null);
-                    if (p instanceof Promise) {
-                        p.catch(err => {
-                            this.logger.error(`Celery task callback failed name=${name} taskId=${taskId}`, err);
-                        });
-                    }
-                })
-                .catch((error: Error) => {
-                    const p = opts.cb!(taskId, null, error);
-                    if (p instanceof Promise) {
-                        p.catch(err => {
-                            this.logger.error(`Celery task callback failed name=${name} taskId=${taskId}`, err);
-                        });
-                    }
-                });
+            try {
+                const r = asyncResult.get();
+                const result = this.parseResult(task, r);
+                const p = opts.cb(taskId, result, null);
+                if (p instanceof Promise) {
+                    p.catch(err => {
+                        this.logger.error(`Celery task callback failed name=${name} taskId=${taskId}`, err);
+                    });
+                }
+            } catch (error) {
+                const p = opts.cb(taskId, null, error as Error);
+                if (p instanceof Promise) {
+                    p.catch(err => {
+                        this.logger.error(`Celery task callback failed name=${name} taskId=${taskId}`, err);
+                    });
+                }
+            }
         } else {
             await asyncResult.get() as TaskResult<T>;
             this.logger.debug(`Celery task completed asynchronously name=${name} taskId=${taskId}`);
