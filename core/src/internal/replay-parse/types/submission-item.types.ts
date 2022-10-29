4af736c8c50e592f1ae5a1c79cf405d57bab4858
Bryan Pikaard <bryan@pikaard.com>
2022-10-29 16:03:11 +0000

    refactor: move all to internal

---
+++ core/src/internal/replay-parse/types/submission-item.types.ts
@@ -0,0 +1,40 @@
+import {Field, ObjectType, registerEnumType} from "@nestjs/graphql";
+import type {ProgressMessage, Task} from "@sprocketbot/common";
+import {Parser, ProgressStatus} from "@sprocketbot/common";
+
+import {GqlProgress} from "../../../util/types/celery-progress";
+
+registerEnumType(Parser, {name: "Parser"});
+
+@ObjectType()
+export class SubmissionProgressMessage implements ProgressMessage<Task.ParseReplay> {
+    @Field(() => String, {nullable: true})
+    error: string | null;
+
+    @Field(() => GqlProgress)
+    progress: GqlProgress;
+
+    @Field(() => ProgressStatus)
+    status: ProgressStatus;
+
+    @Field(() => String)
+    taskId: string;
+
+    result: ProgressMessage<Task.ParseReplay>["result"];
+}
+
+@ObjectType()
+export class ReplaySubmissionItem {
+    @Field(() => String)
+    taskId: string;
+
+    @Field(() => String)
+    originalFilename: string;
+
+    @Field(() => SubmissionProgressMessage)
+    progress?: SubmissionProgressMessage;
+
+    inputPath: string;
+
+    outputPath?: string;
+}
---
+++ core/src/internal/scrim/metrics/scrim-metrics.resolver.ts
@@ -0,0 +1,39 @@
+import {Inject} from "@nestjs/common";
+import {Args, Query, ResolveField, Resolver, Subscription} from "@nestjs/graphql";
+import {PubSub} from "apollo-server-express";
+
+import {Period} from "../../../util/types/period.enum";
+import {ScrimPubSub} from "../constants";
+import {ScrimService} from "../scrim.service";
+import {ScrimMetaCrudService} from "../scrim-crud/scrim-crud.service";
+import {ScrimMetrics} from "../types/ScrimMetrics";
+
+@Resolver(() => ScrimMetrics)
+export class ScrimMetricsResolver {
+    constructor(
+        private readonly scrimService: ScrimService,
+        private readonly scrimCrudService: ScrimMetaCrudService,
+        @Inject(ScrimPubSub) private readonly pubSub: PubSub,
+    ) {}
+
+    @Query(() => ScrimMetrics)
+    async getScrimMetrics(): Promise<ScrimMetrics> {
+        return this.scrimService.getScrimMetrics();
+    }
+
+    @Subscription(() => ScrimMetrics)
+    async followScrimMetrics(): Promise<AsyncIterator<ScrimMetrics>> {
+        await this.scrimService.enableSubscription();
+        return this.pubSub.asyncIterator(this.scrimService.metricsSubTopic);
+    }
+
+    @ResolveField()
+    async completedScrims(@Args("period", {type: () => Period}) period: Period): Promise<number> {
+        return this.scrimCrudService.getScrimCountInPreviousPeriod(period);
+    }
+
+    @ResolveField()
+    async previousCompletedScrims(@Args("period", {type: () => Period}) period: Period): Promise<number> {
+        return this.scrimCrudService.getScrimCountInPreviousPeriod(period, true);
+    }
+}
