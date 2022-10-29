be831b409e5a072f9e1e0020a2c6f5b93e500ca4
Bryan Pikaard <bryan@pikaard.com>
2022-10-29 15:08:38 +0000

    feat: log parsing error

--- core/src/authorization/guards/action.guard.ts
+++ core/src/authorization/guards/action.guard.ts
@@ -1,5 +1,5 @@
 import type {CanActivate, ExecutionContext} from "@nestjs/common";
-import {Injectable} from "@nestjs/common";
+import {Injectable, Logger} from "@nestjs/common";
 import {Reflector} from "@nestjs/core";
 import {GqlExecutionContext} from "@nestjs/graphql";
 
@@ -12,6 +12,8 @@ import type {ActionsOperator} from "../decorators/actions.decorator";
 
 @Injectable()
 export class ActionGuard implements CanActivate {
+    private readonly logger = new Logger(ActionGuard.name);
+
     constructor(
         private readonly reflector: Reflector,
         private readonly authorizationService: AuthorizationService,
@@ -30,18 +32,30 @@ export class ActionGuard implements CanActivate {
         if (!ctx?.req.user) return false;
 
         const data = JwtAuthPayloadSchema.safeParse(ctx.req.user);
-        if (!data.success || !data.data.currentOrganizationId) return false;
+        if (!data.success) {
+            this.logger.debug({error: "Failed to parse JwtAuthPayload", data: data});
+            return false;
+        }
 
-        const member = await this.memberRepository.getOrNull({
-            where: {userId: data.data.userId, organizationId: data.data.currentOrganizationId},
-        });
-        if (!member) return false;
+        const actionsForOrganization: string[] = [];
 
-        const memberActions = await this.authorizationService.getMemberActions(member.id);
+        // TODO: Add Sprocket/global permissions and add fetching here
+
+        if (data.data.currentOrganizationId) {
+            const member = await this.memberRepository.getOrNull({
+                where: {userId: data.data.userId, organizationId: data.data.currentOrganizationId},
+            });
+            if (!member) return false;
+
+            const memberActions = await this.authorizationService.getMemberActions(member.id);
+            memberActions.forEach(ma => actionsForOrganization.push(ma));
+        }
+
+        if (!actionsForOrganization.length) return false;
         if (
             !requiredActions.some(requiredAction => {
-                if (requiredAction instanceof Function) return requiredAction(memberActions);
-                return memberActions.includes(requiredAction);
+                if (requiredAction instanceof Function) return requiredAction(actionsForOrganization);
+                return actionsForOrganization.includes(requiredAction);
             })
         )
             return false;
