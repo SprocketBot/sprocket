49cdeeb4741a65ef5231f5b8aed4d338c9076218
Bryan Pikaard <bryan@pikaard.com>
2022-10-29 14:58:15 +0000

    feat: and/or logic for actions

--- core/src/authorization/decorators/actions.decorator.ts
+++ core/src/authorization/decorators/actions.decorator.ts
@@ -4,7 +4,47 @@ import {GraphQLJwtAuthGuard} from "../../authentication/guards";
 import {MetadataKeys} from "../authorization.types";
 import {ActionGuard} from "../guards/action.guard";
 
+export type ActionsOperator = (memberActions: string[]) => boolean;
+
+export function ActionsOr(...actions: Array<string | ActionsOperator>): ActionsOperator {
+    return (memberActions: string[]) =>
+        actions.some(action => {
+            if (action instanceof Function) return action(memberActions);
+            return memberActions.includes(action);
+        });
+}
+
+export function ActionsAnd(...actions: Array<string | ActionsOperator>): ActionsOperator {
+    return (memberActions: string[]) =>
+        actions.every(action => {
+            if (action instanceof Function) return action(memberActions);
+            return memberActions.includes(action);
+        });
+}
+
+/**
+ * Requires a member to have the permission to do provided actions. Acts as an OR for root paramteres.
+ * @param actions List of strings representing actions or action operators. Acts as an OR.
+ * @example
+ * // Requires "DoSomething" OR "DoSomethingElse"
+ * Actions("DoSomething", "DoSomethingElse")
+ * @example
+ * // Requires "DoSomething" OR "DoSomethingElse"
+ * Actions(ActionsOr("DoSomething", "DoSomethingElse"))
+ * @example
+ * // Requires "DoSomething" AND "DoSomethingElse"
+ * Actions(ActionsAnd("DoSomething", "DoSomethingElse"))
+ * @example
+ * // Requires "DoSomething" AND ("DoSomethingElse" OR "OrElseDoThing")
+ * Actions(ActionsAnd("DoSomething", ActionsOr("DoSomethingElse", "OrElseDoThing")))
+ * @example
+ * // Requires "DoSomething" OR ("DoSomethingElse" AND "OrElseDoThing")
+ * Actions("DoSomething", ActionsAnd("DoSomethingElse", "OrElseDoThing"))
+ * @example
+ * // Requires "DoSomething" OR ("DoSomethingElse" AND "OrElseDoThing")
+ * Actions(ActionsOr("DoSomething", ActionsAnd("DoSomethingElse", "OrElseDoThing")))
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function Actions(...actions: string[]): (...decorators: any[]) => void {
+export function Actions(...actions: Array<string | ActionsOperator>): (...decorators: any[]) => void {
     return applyDecorators(SetMetadata(MetadataKeys.Actions, actions), UseGuards(GraphQLJwtAuthGuard, ActionGuard));
 }
