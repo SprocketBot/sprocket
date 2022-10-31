5a3b48170dac9dd7901f03eb005b39ce5558d548
Bryan Pikaard <bryan@pikaard.com>
2022-10-31 22:12:01 +0000

    fix: shuckle comments

--- common/src/service-connectors/matchmaking/schemas/scrim/GetAllScrims.schema.ts
+++ common/src/service-connectors/matchmaking/schemas/scrim/GetAllScrims.schema.ts
@@ -4,7 +4,7 @@ import {ScrimSchema} from "../../types";
 
 export const GetAllScrims_Request = z.object({
     organizationId: z.number().optional(),
-    skillGroupId: z.number().optional(),
+    skillGroupIds: z.array(z.number()).optional(),
 });
 
 export const GetAllScrims_Response = z.array(ScrimSchema);
