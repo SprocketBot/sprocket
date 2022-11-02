749552622518d437d745aaf673106726847e9f02
Bryan Pikaard <bryan@pikaard.com>
2022-11-01 19:08:58 -0500

    Update core/src/database/franchise/player/player.repository.ts
    Co-authored-by: Brian D  <contact@noimbrian.com>

--- core/src/database/franchise/player/player.repository.ts
+++ core/src/database/franchise/player/player.repository.ts
@@ -15,7 +15,7 @@ export class PlayerRepository extends ExtendedRepository<Player> {
         userId: number,
         organizationId: number,
         gameId: number,
-        options?: FindOneOptions<Player>,
+        options?: Omit<FindOneOptions<Player>, "where">,
     ): Promise<Player> {
         return this.get(
             Object.assign(
