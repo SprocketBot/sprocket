7cb35692bcb190610912e0aff0efa2b0404ad31e
Brian D <contact@noimbrian.com>
2022-11-08 15:44:16 -0600

    feat: add breadcrumb component

---
+++ clients/web/src/lib/components/atoms/Breadcrumb/Breadcrumb.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Crumb from "./Crumb.svelte";
+    import {getContext, hasContext} from "svelte";
+    import {type BreadcrumbContext, BREADCRUMB_CONTEXT_KEY} from "./types";
+
+    const crumbs = hasContext(BREADCRUMB_CONTEXT_KEY) ? getContext<BreadcrumbContext>(BREADCRUMB_CONTEXT_KEY) : [];
+</script>
+
+<nav class="flex" aria-label="Breadcrumb">
+    <ol class="inline-flex items-center space-x-1">
+        {#each crumbs as crumb, i}
+            <Crumb {crumb} active={i === crumbs.length - 1} />
+        {/each}
+    </ol>
+</nav>
---
+++ clients/web/src/lib/components/atoms/Breadcrumb/Crumb.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import {Icon} from "@steeze-ui/svelte-icon";
+    import {ChevronRight} from "@steeze-ui/heroicons";
+    import type { ICrumb } from "./types";
+
+    export let active: boolean = false;
+    export let crumb: ICrumb
+    const {icon, label, target} = crumb;
+</script>
+
+<li class="inline-flex items-center">
+    <a href={target} class:active>
+        {#if icon}
+            <div class="w-4 mr-1">
+                <Icon src={icon} />
+            </div>
+        {/if}
+        {label}
+    </a>
+</li>
+<li>
+    {#if !active}
+        <div class="w-4">
+            <Icon src={ChevronRight} />
+        </div>
+    {/if}
+</li>
+
+<style lang="postcss">
+    a {
+        @apply inline-flex items-center text-sm font-medium text-gray-400 hover:text-white;
+        &.active {
+            @apply font-medium text-gray-100;
+        }
+    }
+</style>
