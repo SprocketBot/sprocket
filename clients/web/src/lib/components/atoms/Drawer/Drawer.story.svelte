12b825a5e83a75754e875292031a0f6521ee8cf3
Brian D <contact@noimbrian.com>
2022-11-08 17:04:39 -0600

    fix: padding

--- clients/web/src/lib/components/atoms/Drawer/Drawer.story.svelte
+++ clients/web/src/lib/components/atoms/Drawer/Drawer.story.svelte
@@ -13,7 +13,14 @@
     <Hst.Variant title="Default">
         <button on:click={() => (open = true)}>Open the Drawer</button>
 
-        <Drawer bind:open {position}>Hello World</Drawer>
+        <Drawer bind:open {position}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad quas aperiam exercitationem est veritatis nobis, esse vitae aliquam error ratione delectus voluptatum possimus, doloribus accusamus? Blanditiis sit itaque quo odio?
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt quis quidem laudantium, quisquam officiis vitae, ullam iusto nostrum dolor possimus iste veniam maiores accusantium a repellat non sint exercitationem pariatur.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro natus, dolores voluptate vel laborum, fugiat architecto aliquam officiis ea odit velit! Repellendus porro consequatur facere laborum hic animi ullam beatae?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias ratione deleniti officiis amet sint praesentium sapiente! Quidem excepturi laborum exercitationem quos fuga numquam similique facilis, qui quo tempora itaque repudiandae.
+        
+        
+        </Drawer>
 
         <svelte:fragment slot="controls">
             Position: <select bind:value={position} class="px-2 py-1 rounded bg-gray-800 text-gray-200 outline-none">
