e0196a509a9198f8a034867645bff917a0b43481
Jake Bailey <asaxplayinghorse@gmail.com>
2022-11-09 19:29:18 -0800

    feat: add all variants, parameterize and even use our proper classes

--- clients/web/src/lib/components/atoms/ProgressBar/ProgressBar.svelte
+++ clients/web/src/lib/components/atoms/ProgressBar/ProgressBar.svelte
@@ -1,25 +1,63 @@
 <script lang="ts">
-    export let size = 2;
+    // Obviously, this needs to be its own parameter for the 'filled-ness' of
+    // the bar
+    export let progress = 45;
+
+    // Props to customize which bar you want
+    export let size = "default";
+    export let label = false;
+    export let labelOutside = true;
+    export let progressLabel = "Default Progress Bar Text";
+
+    // Styling choices
+    export let primaryBarColor = "bg-primary-900";
+    export let primaryTextClass = "text-primary-200";
 </script>
 
+{#if label}
+    {#if labelOutside}
+        <div class="flex justify-between mb-1">
+            <span
+                class="text-base font-medium {primaryTextClass}
+            dark:{primaryTextClass}">{progressLabel}</span
+            >
+            <span class="text-sm font-medium {primaryTextClass} dark:{primaryTextClass}">{progress}%</span>
+        </div>
+    {/if}
+{/if}
+
 <div class="w-full rounded-full dark:bg-gray-800">
-    <div class="bg-info-600 rounded-full size-{size}" style="width: 45%" />
+    {#if label}
+        {#if labelOutside}
+            <div class="{primaryBarColor} rounded-full size-{size}" style="width: {progress}%" />
+        {:else}
+            <div
+                class="{primaryBarColor} font-medium {primaryTextClass} text-center 
+                        leading-none rounded-full size-large"
+                style="width: {progress}%"
+            >
+                {progress}%
+            </div>
+        {/if}
+    {:else}
+        <div class="{primaryBarColor} rounded-full size-{size}" style="width: {progress}%" />
+    {/if}
 </div>
 
 <style lang="postcss">
-    .size-1 {
+    .size-small {
         @apply h-1.5;
     }
 
-    .size-2 {
+    .size-default {
         @apply h-2.5;
     }
 
-    .size-3 {
+    .size-large {
         @apply h-4;
     }
 
-    .size-4 {
+    .size-extraLarge {
         @apply h-6;
     }
 </style>
