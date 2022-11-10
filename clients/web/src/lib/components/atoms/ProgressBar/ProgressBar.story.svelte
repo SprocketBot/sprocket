fb6f2682082969c5f258c8a4f1c40b6b1c14cf29
Jake Bailey <asaxplayinghorse@gmail.com>
2022-11-09 19:02:27 -0800

    feat: add ProgressBar atom, some parameters set

---
+++ clients/web/src/lib/components/atoms/ProgressBar/ProgressBar.story.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type {Hst as _Hst} from "@histoire/plugin-svelte";
+    import ProgressBar from "./ProgressBar.svelte";
+
+    export let Hst: _Hst;
+</script>
+
+<Hst.Story title="Atoms/ProgressBar">
+    <Hst.Variant title="Default">
+        <ProgressBar />
+    </Hst.Variant>
+    <Hst.Variant title="Show All Sizes">
+        <ProgressBar />
+        &nbsp;
+        <ProgressBar size={2} />
+        &nbsp;
+        <ProgressBar size={3} />
+    </Hst.Variant>
+</Hst.Story>
