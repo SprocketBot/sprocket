e639cd7d6e049943fdcbd68447349b8ee036e0b0
Jake Bailey <asaxplayinghorse@gmail.com>
2022-11-09 19:29:18 -0800

    feat: add all variants, parameterize and even use our proper classes

--- clients/web/src/lib/components/atoms/ProgressBar/ProgressBar.story.svelte
+++ clients/web/src/lib/components/atoms/ProgressBar/ProgressBar.story.svelte
@@ -7,13 +7,21 @@
 
 <Hst.Story title="Atoms/ProgressBar">
     <Hst.Variant title="Default">
-        <ProgressBar />
+        <ProgressBar progress={75.6767676} />
+    </Hst.Variant>
+    <Hst.Variant title="With Label">
+        <ProgressBar label={true} size="large" />
+    </Hst.Variant>
+    <Hst.Variant title="With Label Inside">
+        <ProgressBar label={true} labelOutside={false} />
     </Hst.Variant>
     <Hst.Variant title="Show All Sizes">
+        <ProgressBar size="small" />
+        &nbsp;
         <ProgressBar />
         &nbsp;
-        <ProgressBar size={2} />
+        <ProgressBar size="large" />
         &nbsp;
-        <ProgressBar size={3} />
+        <ProgressBar size="extraLarge" />
     </Hst.Variant>
 </Hst.Story>
