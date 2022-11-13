6d279f4a6d7922a67e565a07fa0af5ae663913cf
Jake Bailey <asaxplayinghorse@gmail.com>
2022-11-13 11:17:59 -0800

    feat: add card atom, w/ button variant

---
+++ clients/web/src/lib/components/atoms/Card/Card.story.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type {Hst as _Hst} from "@histoire/plugin-svelte";
+    import Card from "./Card.svelte";
+
+    type sizes = "small" | "default" | "large" | "extraLarge";
+
+    const sizeOptions = ["small", "default", "large", "extraLarge"];
+
+    export let Hst: _Hst;
+    let header: string;
+    let text: string;
+    let buttonLink: string;
+    let buttonText: string;
+</script>
+
+<Hst.Story title="Atoms/Card">
+    <svelte:fragment slot="controls">
+        <Hst.Text title="Card Header" bind:value={header} />
+        <Hst.Text title="Card Text" bind:value={text} />
+        <Hst.Text title="Button Link" bind:value={buttonLink} />
+        <Hst.Text title="Button Text" bind:value={buttonText} />
+    </svelte:fragment>
+    <Hst.Variant title="Default Card">
+        <Card {header} {text} {buttonLink} {buttonText} />
+    </Hst.Variant>
+    <Hst.Variant title="With Nav Button">
+        <Card {header} {text} buttonLink="https://google.com" />
+    </Hst.Variant>
+</Hst.Story>
---
+++ clients/web/src/lib/components/atoms/Card/Card.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    export let header = "This is a Sprocket Card 2.0";
+    export let text = "Here we'll have text to make it look like we know what we're talking about. ";
+    export let buttonLink: string;
+    export let buttonText: string = "Read more";
+</script>
+
+<a
+    href="nolink"
+    class="block p-6 max-w-sms 
+    bg-primary-800 
+    rounded-lg border 
+    border-primary-400 
+    shadow-md 
+    hover:bg-primary-400"
+>
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary-50">
+        {header}
+    </h5>
+    <p class="font-normal text-primary-100">
+        {text}
+    </p>
+    {#if buttonLink}
+        <a
+            href={buttonLink}
+            class="inline-flex items-center py-2 px-3 text-sm
+    font-medium text-center text-white bg-secondary-700 rounded-lg hover:bg-secondary-800
+    focus:ring-4 focus:outline-none focus:ring-blue-300"
+        >
+            {buttonText}
+            <svg
+                aria-hidden="true"
+                class="ml-2 -mr-1 w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                /></svg
+            >
+        </a>
+    {/if}
+</a>
