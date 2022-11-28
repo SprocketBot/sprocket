7582834ac161fdaf2508b82234f0a2aadacb03fe
Brian D <contact@noimbrian.com>
2022-11-27 23:25:27 -0600

    chore: remove button link prop; clean up styles

--- clients/web/src/lib/components/atoms/Card/Card.story.svelte
+++ clients/web/src/lib/components/atoms/Card/Card.story.svelte
@@ -3,35 +3,23 @@
     import Card from "./Card.svelte";
 
     export let Hst: _Hst;
-    let header: string;
-    let text: string;
-    let buttonLink: string;
-    let buttonText: string;
+    let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi justo ante, luctus eget scelerisque ac, pulvinar at leo. Phasellus eu odio nec sem aliquam vestibulum. Donec ultrices dapibus velit, in lobortis urna molestie eget.";
 </script>
 
 <Hst.Story title="Atoms/Card">
     <svelte:fragment slot="controls">
-        <Hst.Text title="Card Header" bind:value={header} />
         <Hst.Text title="Card Text" bind:value={text} />
-        <Hst.Text title="Button Link" bind:value={buttonLink} />
-        <Hst.Text title="Button Text" bind:value={buttonText} />
     </svelte:fragment>
     <Hst.Variant title="Default Card">
-        <Card {buttonLink} {buttonText}>
-            <div slot="header">
-                {header}
-            </div>
-            <div slot="text">
+        <Card>
+            <div>
                 {text}
             </div>
         </Card>
     </Hst.Variant>
     <Hst.Variant title="With Nav Button">
         <Card buttonLink="https://google.com">
-            <div slot="header">
-                {header}
-            </div>
-            <div slot="text">
+            <div>
                 {text}
             </div>
         </Card>
--- clients/web/src/lib/components/atoms/Card/Card.svelte
+++ clients/web/src/lib/components/atoms/Card/Card.svelte
@@ -1,45 +1,19 @@
 <script lang="ts">
     export let buttonLink: string;
-    export let buttonText: string = "Read more";
+    export let buttonText = "Read more";
 </script>
 
-<a
-    href="nolink"
-    class="block p-6 max-w-sms 
-    bg-primary-800 
-    rounded-lg border 
-    border-primary-400 
-    shadow-md 
-    hover:bg-primary-400"
+<div
+    class="block p-6 max-w-sms
+    bg-gray-800
+    rounded-lg border
+    border-gray-400
+    shadow-md
+    text-gray-100"
 >
-    <slot name="header">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary-50">"This is a Sprocket Card 2.0"</h5>
-    </slot>
-    <slot name="text">
-        <p class="font-normal text-primary-100">
+    <slot>
+        <p class="font-normal text-gray-100">
             "Here we'll have text to make it look like we know what we're talking about."
         </p>
     </slot>
-    {#if buttonLink}
-        <a
-            href={buttonLink}
-            class="inline-flex items-center py-2 px-3 text-sm
-    font-medium text-center text-white bg-secondary-700 rounded-lg hover:bg-secondary-800
-    focus:ring-4 focus:outline-none focus:ring-blue-300"
-        >
-            {buttonText}
-            <svg
-                aria-hidden="true"
-                class="ml-2 -mr-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-                ><path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                /></svg
-            >
-        </a>
-    {/if}
-</a>
+</div>
