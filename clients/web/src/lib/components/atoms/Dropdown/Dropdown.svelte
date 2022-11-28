d724dcf2e3cb0e8c09fef4db5f94e0c096947444
Brian <bmdonald1998@gmail.com>
2022-11-28 00:39:45 -0600

    feat: add transparent state

--- clients/web/src/lib/components/atoms/Dropdown/Dropdown.svelte
+++ clients/web/src/lib/components/atoms/Dropdown/Dropdown.svelte
@@ -2,10 +2,12 @@
     import {clickOutside} from "$lib/actions/clickOutside.action";
     import {slide} from "svelte/transition";
     let open: boolean = false;
+
+    export let transparent: boolean = false;
 </script>
 
 <div class="relative" use:clickOutside={{callback: () => (open = false)}}>
-    <button type="button" on:click={() => (open = !open)}> <slot name="handle">Handle</slot> </button>
+    <button type="button" class:transparent on:click={() => (open = !open)}> <slot name="handle">Handle</slot> </button>
 
     {#if open}
         <div class="z-10 w-44 rounded divide-y divide-gray-100 shadow bg-gray-600 absolute top-[110%]" transition:slide>
@@ -18,5 +20,9 @@
     button {
         @apply text-white focus:ring-2 focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center 
                 bg-info-600 hover:bg-info-700 focus:ring-info-500;
+
+        &.transparent {
+            @apply bg-transparent hover:bg-transparent p-0;
+        }
     }
 </style>
