c43c5cc983981a7384e0d1c18a07eecba570624a
Brian D <contact@noimbrian.com>
2022-12-05 16:30:03 -0600

    feat: more tooltip things

--- clients/web/src/lib/components/actions/Tooltip/Tooltip.story.svelte
+++ clients/web/src/lib/components/actions/Tooltip/Tooltip.story.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
     import type {Hst as _Hst} from "@histoire/plugin-svelte";
-    import { tooltip } from "./useTooltip.action";
+    import type {TooltipPosition} from "./types";
+    import {tooltip} from "./useTooltip.action";
     export let Hst: _Hst;
+
+    let position: TooltipPosition = "top";
+    $: console.log(position);
 </script>
 
 <Hst.Story title="Atoms/Tooltip">
+    <svelte:fragment slot="controls">
+        <span class="ml-8">Position:</span>
+        <select bind:value={position} class="bg-transparent mx-4 my-4">
+            <option value="top">Top</option>
+            <option value="bottom">Bottom</option>
+            <option value="left">Left</option>
+            <option value="right">Right</option>
+        </select>
+    </svelte:fragment>
     <Hst.Variant title="Default">
-        <button use:tooltip={{content: "Test", position: "top"}}>Hello!</button>
+        <div class="w-screen h-screen flex justify-center items-center">
+            {#key position}
+            <button
+                class="border border-primary rounded-lg px-2 py-1 text-gray-200"
+                use:tooltip={{content: "Test", position: position}}>{position}</button
+            >
+            {/key}
+        </div>
     </Hst.Variant>
 </Hst.Story>
