a6918a749ca0635948645f681c7d0800ebd948d7
Brian D <contact@noimbrian.com>
2022-12-05 16:30:03 -0600

    feat: more tooltip things

--- clients/web/src/lib/components/actions/Tooltip/useTooltip.action.ts
+++ clients/web/src/lib/components/actions/Tooltip/useTooltip.action.ts
@@ -4,17 +4,35 @@ import "./tooltip.postcss"
 export const tooltip = (node: HTMLElement, { content, position = "top" }: TooltipParams) => {
     const tooltip = document.createElement("div")
     tooltip.classList.add("tooltip")
-
+    console.log({position})
+    
     function updatePosition() {
+        const bbox = node.getBoundingClientRect();
         // Position with javascript.
+        tooltip.style.top = ""
+        tooltip.style.left = ""
+        tooltip.style.right = ""
+        tooltip.style.bottom = ""
         switch (position) {
             case "top":
+                tooltip.style.bottom = `${bbox.top + bbox.height + 4}px`
+                tooltip.style.left = `${bbox.left + (bbox.width / 2)}px`
+                tooltip.style.transform = "translateX(-50%)"
                 break;
             case "bottom":
+                tooltip.style.top = `${bbox.top + bbox.height + 4}px`
+                tooltip.style.left = `${bbox.left + (bbox.width / 2)}px`
+                tooltip.style.transform = "translateX(-50%)"
                 break;
             case "left":
+                tooltip.style.top = `${bbox.top + (bbox.height / 2)}px`
+                tooltip.style.right = `${bbox.left + bbox.width + 4}px`
+                tooltip.style.transform = "translateY(-50%)"
                 break;
             case "right":
+                tooltip.style.top = `${bbox.top + (bbox.height / 2)}px`
+                tooltip.style.left = `${bbox.left + bbox.width + 4}px`
+                tooltip.style.transform = "translateY(-50%)"
                 break;
         }
     }
@@ -23,12 +41,12 @@ export const tooltip = (node: HTMLElement, { content, position = "top" }: Toolti
     function renderTooltip() {
         // Render tooltip
         updatePosition()
-        node.parentElement?.insertBefore(tooltip, node)
+        document.body.appendChild(tooltip)
     }
 
     function removeTooltip() {
         // Remove tooltip
-        node.parentElement?.removeChild(tooltip)
+        document.body.removeChild(tooltip)
     }
     node.addEventListener("mouseenter", renderTooltip)
     node.addEventListener("mouseleave", removeTooltip)
@@ -40,3 +58,4 @@ export const tooltip = (node: HTMLElement, { content, position = "top" }: Toolti
         }
     }
 }
+
