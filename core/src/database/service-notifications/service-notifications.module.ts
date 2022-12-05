29146b5b049928abf74ba4ecbf649813eb3e5101
Brian D <contact@noimbrian.com>
2022-12-05 16:45:10 -0600

    chore: remove useless concept

--- core/src/database/service-notifications/service-notifications.module.ts
+++
@@ -1,8 +0,0 @@
-import {Module} from "@nestjs/common";
-
-import {GlobalEntitySubscriberService} from "./global-entity-subscriber/global-entity-subscriber.service";
-
-@Module({
-    providers: [GlobalEntitySubscriberService],
-})
-export class ServiceNotificationsModule {}
