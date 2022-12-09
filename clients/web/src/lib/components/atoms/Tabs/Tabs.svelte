5efc4fde0ac574bcf776e538fdc5bcb978b5f98f
Brian D <contact@noimbrian.com>
2022-12-09 15:33:48 -0600

    chore: aria

--- clients/web/src/lib/components/atoms/Tabs/Tabs.svelte
+++ clients/web/src/lib/components/atoms/Tabs/Tabs.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type {TabItems} from "./types";
     import {Icon} from "@steeze-ui/svelte-icon";
+    import {nanoid} from "nanoid";
 
     type T = $$Generic<TabItems>;
     interface $$Props {
@@ -9,18 +10,40 @@
     }
 
     export let tabs: TabItems;
-    export let activeKey: keyof TabItems;
+    export let activeKey: string;
+
+    const id = nanoid();
+
+    function keydown(e: KeyboardEvent) {
+        const keys = Object.keys(tabs);
+        const tabIdx = keys.indexOf(activeKey);
+
+        switch (e.key) {
+            case "ArrowLeft":
+                if (tabIdx > 0) activeKey = keys[tabIdx - 1];
+                break;
+            case "ArrowRight":
+                if (tabIdx < keys.length - 1) activeKey = keys[tabIdx + 1];
+                break;
+        }
+    }
 </script>
 
 <div class="text-sm font-medium text-center border-b text-gray-400 border-gray-700">
-    <ul class="flex flex-wrap -mb-px">
+    <ul class="flex flex-wrap -mb-px" role="tablist">
         {#each Object.keys(tabs) as key}
             <li class="mr-2">
                 <button
+                    on:keydown={keydown}
                     type="button"
-                    class="inline-block p-2 rounded-t-lg border-2 border-transparent hover:text-gray-600 hover:border-b-gray-300 hover:text-gray-300 flex items-center gap-2"
+                    class="inline-block p-2 rounded-t-lg border-2 border-transparent hover:text-gray-600 hover:text-gray-300 flex items-center gap-2 outline-none"
                     class:active={key === activeKey}
                     on:click={() => (activeKey = key)}
+
+                    role="tab"
+                    id="{id}-but"
+                    aria-selected={`${key === activeKey ? "true" : "false"}`}
+                    aria-controls={id}
                 >
                     {#if tabs[key].icon}
                         <Icon class="h-4 w-4" src={tabs[key].icon} />
@@ -31,13 +54,22 @@
         {/each}
     </ul>
 </div>
-<div class="relative pt-4">
-    {#each Object.keys(tabs) as key}
+
+{#each Object.keys(tabs) as key}
+    <div
+        class="relative pt-4"
+        class:visible={key === activeKey}
+        class:hidden={key !== activeKey}
+
+        {id}
+        aria-labelledby="{id}-but"
+        role="tabpanel"
+    >
         {#if key === activeKey}
             <svelte:component this={tabs[activeKey].component} {...tabs[activeKey].props} />
         {/if}
-    {/each}
-</div>
+    </div>
+{/each}
 
 <style lang="postcss">
     button.active {
