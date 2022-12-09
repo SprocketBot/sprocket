f3b71a206ac0c16cc351211f85716da7a47b12cd
Brian D <contact@noimbrian.com>
2022-12-09 15:01:07 -0600

    Apply suggestions from code review
    Co-authored-by: Zach Stence <zachstence@gmail.com>

--- clients/web/src/lib/components/atoms/Tabs/types.ts
+++ clients/web/src/lib/components/atoms/Tabs/types.ts
@@ -1,18 +1,10 @@
 import type {SvelteComponent, ComponentProps} from "svelte";
 import type {IconSource} from "@steeze-ui/svelte-icon/types";
 
-// export type TabItem = {
-//   component: SvelteComponent,
-//   key: string,
-//   props: ComponentProps<SvelteComponent>
-// }
-
 export type TabItem<T extends SvelteComponent = SvelteComponent> = {
     component: T;
     props: ComponentProps<T>;
     icon?: IconSource;
 };
 
-export type TabItems = {
-    [key: string]: TabItem;
-};
+export type TabItems = Record<string, TabItem>;
