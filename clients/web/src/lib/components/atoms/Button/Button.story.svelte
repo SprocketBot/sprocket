eb5c1bb061b0a6a04b124256f83023bbcd91e2d8
Jake Bailey <asaxplayinghorse@gmail.com>
2022-12-10 21:04:09 +0000

    feat: add button component

---
+++ clients/web/src/lib/components/atoms/Button/Button.story.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type {Hst as _Hst} from "@histoire/plugin-svelte";
+    import Button from "./Button.svelte";
+
+    export let Hst: _Hst;
+    let label:string = "HAI";
+</script>
+
+<Hst.Story title="Atoms/Button">
+    <svelte:fragment slot="controls">
+        <Hst.Text title="Label" bind:value={label} />
+    </svelte:fragment>
+    <Hst.Variant title="Default">
+        <Button {label} />
+    </Hst.Variant>
+</Hst.Story>
