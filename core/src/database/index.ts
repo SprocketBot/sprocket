ec311260c363bd3c90264ef9eb36685a06552ad3
Bryan Pikaard <bryan@pikaard.com>
2022-12-13 17:46:29 +0000

    chore: remove database module

--- core/src/database/index.ts
+++
@@ -1,7 +0,0 @@
-import type {ModelLifecycleFields} from "./base-model";
-
-export * from "./base-model";
-export * from "./database.module";
-
-export type TypeOrmFields = "hasId" | "save" | "remove" | "softRemove" | "recover" | "reload";
-export type IrrelevantFields = TypeOrmFields | ModelLifecycleFields;
