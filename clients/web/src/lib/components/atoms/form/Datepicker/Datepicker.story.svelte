629ce1045fc741785c4d8527bc02c30447f27e9b
Zachary Stence <zachstence@gmail.com>
2022-12-14 17:56:05 -0600

    feat: skeleton Datepicker component

---
+++ clients/web/src/lib/components/atoms/form/Datepicker/Datepicker.story.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type {Hst as _Hst} from "@histoire/plugin-svelte";
+    import Datepicker from "./Datepicker.svelte";
+
+    export let Hst: _Hst;
+</script>
+
+<Hst.Story title="Atoms/Form/Datepicker">
+    <Hst.Variant title="Default" >
+        <Datepicker />
+    </Hst.Variant>
+</Hst.Story>
