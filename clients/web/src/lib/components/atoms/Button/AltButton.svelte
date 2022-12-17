d689dcc0b76e1362308bea88d286dca00f68c003
Jake Bailey <asaxplayinghorse@gmail.com>
2022-12-17 18:29:21 +0000

    feat: style buttons

---
+++ clients/web/src/lib/components/atoms/Button/AltButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let label:string;
+</script>
+
+<button type="button" class="text-white bg-secondary-700 hover:bg-primary-800 
+focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm 
+px-5 py-2.5 mr-2 mb-2 focus:outline-none">Alternative</button>
--- clients/web/src/lib/components/atoms/Button/index.ts
+++ clients/web/src/lib/components/atoms/Button/index.ts
@@ -1,1 +1,2 @@
 export {default as Button} from "./Button.svelte";
+export {default as AltButton} from "./AltButton.svelte";
