6734615c4408e031db2ad5dfa3452751912f994c
Bryan Pikaard <bryan@pikaard.com>
2023-01-21 02:29:03 +0000

    feat: scrim resolver

--- core/src/organization/graphql/organization.object.ts
+++ core/src/organization/graphql/organization.object.ts
@@ -1,6 +1,8 @@
 import {Field, ObjectType} from "@nestjs/graphql";
 
 import {BaseObject} from "../../types/base-object";
+import type {Organization} from "../database/organization.entity";
+import type {OrganizationProfile} from "../database/organization-profile.entity";
 
 @ObjectType()
 export class OrganizationObject extends BaseObject {
@@ -21,4 +23,19 @@ export class OrganizationObject extends BaseObject {
 
     @Field(() => String, {nullable: true})
     logoUrl?: string;
+}
+
+export function organizationObjectFromEntity(entity: Organization, profile: OrganizationProfile): OrganizationObject {
+    return {
+        id: entity.id,
+        createdAt: entity.createdAt,
+        updatedAt: entity.updatedAt,
+        deletedAt: entity.deletedAt,
+        name: profile.name,
+        description: profile.description,
+        websiteUrl: profile.websiteUrl,
+        primaryColor: profile.primaryColor,
+        secondaryColor: profile.secondaryColor,
+        logoUrl: profile.logoUrl,
+    };
 }
