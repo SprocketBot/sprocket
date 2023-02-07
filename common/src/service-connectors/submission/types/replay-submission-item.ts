bbc0705295b4ce50229e97f7de5f580e3a4c29d7
Bryan Pikaard <bryan@pikaard.com>
2023-02-07 02:26:29 +0000

    chore: remove old files

--- common/src/service-connectors/submission/types/replay-submission-item.ts
+++
@@ -1,9 +0,0 @@
-import type {ProgressMessage, Task} from "../../../celery";
-
-export interface ReplaySubmissionItem {
-    taskId: string;
-    originalFilename: string;
-    inputPath: string;
-    outputPath?: string;
-    progress?: ProgressMessage<Task.ParseReplay>;
-}
