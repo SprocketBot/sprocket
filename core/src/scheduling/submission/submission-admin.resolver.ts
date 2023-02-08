32928d41f401ec2a423e49ed6225d339174de12f
Bryan Pikaard <bryan@pikaard.com>
2023-02-08 00:14:30 +0000

    feat: reset submission perms

--- core/src/scheduling/submission/submission-admin.resolver.ts
+++ core/src/scheduling/submission/submission-admin.resolver.ts
@@ -1,23 +1,50 @@
 import {UseGuards} from "@nestjs/common";
 import {Args, Mutation, Resolver} from "@nestjs/graphql";
+import {ReplaySubmissionType} from "@sprocketbot/common";
+import {GraphQLError} from "graphql";
 
 import {AuthenticatedUser} from "../../authentication/decorators";
 import {GraphQLJwtAuthGuard} from "../../authentication/guards";
 import {JwtAuthPayload} from "../../authentication/types";
-import {Actions} from "../../authorization/decorators";
+import {AuthorizationService} from "../../authorization/authorization.service";
+import {MatchRepository} from "../database/match.repository";
+import {ScrimService} from "../scrim/scrim.service";
 import {SubmissionService} from "./submission.service";
 
 @Resolver()
 @UseGuards(GraphQLJwtAuthGuard)
 export class SubmissionAdminResolver {
-    constructor(private readonly submissonService: SubmissionService) {}
+    constructor(
+        private readonly submissonService: SubmissionService,
+        private readonly scrimService: ScrimService,
+        private readonly matchRepository: MatchRepository,
+        private readonly authorizationService: AuthorizationService,
+    ) {}
 
     @Mutation(() => Boolean)
-    @Actions("ResetOrganizationSubmissions")
     async resetSubmission(
         @AuthenticatedUser() user: JwtAuthPayload,
         @Args("submissionId") submissionId: string,
     ): Promise<boolean> {
+        const submission = await this.submissonService.getSubmissionById(submissionId);
+
+        let organizationId: number;
+        if (submission.type === ReplaySubmissionType.MATCH) {
+            const match = await this.matchRepository.findById(submission.matchId, {
+                relations: {
+                    skillGroup: true,
+                },
+            });
+            organizationId = match.skillGroup.organizationId;
+        } else {
+            const scrim = await this.scrimService.getScrimById(submission.scrimId);
+            if (!scrim) throw new Error("Scrim not found");
+            organizationId = scrim.organizationId;
+        }
+
+        if (!(await this.authorizationService.can(user.userId, organizationId, "ResetSubmission")))
+            throw new GraphQLError("Unauthorized");
+
         return this.submissonService.resetSubmission(submissionId, user.userId, true);
     }
 }
