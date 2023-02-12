e11b08daaabcd6afd08970fac9e331ca5eb72409
Brian D <contact@noimbrian.com>
2023-02-11 23:33:29 -0600

    feat: basic layout component

---
+++ clients/web/src/lib/components/layouts/Dashboard/Dashboard.story.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type {Hst as _Hst} from "@histoire/plugin-svelte";
+    import Dashboard from "./Dashboard.svelte";
+    export let Hst: _Hst;
+</script>
+
+<Hst.Story title="Layouts/Dashboard" layout={{type: "single"}}>
+    <Hst.Variant title="Default">
+        <Dashboard>
+            Page Content
+        </Dashboard>
+    </Hst.Variant>
+</Hst.Story>
