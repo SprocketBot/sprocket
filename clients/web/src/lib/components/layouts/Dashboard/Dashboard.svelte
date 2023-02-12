8c7ffb72b5bcc639de6ceaa3bf5210fbefec1cec
Brian D <contact@noimbrian.com>
2023-02-12 00:27:15 -0600

    chore: clean up imports; add some TODOs

--- clients/web/src/lib/components/layouts/Dashboard/Dashboard.svelte
+++ clients/web/src/lib/components/layouts/Dashboard/Dashboard.svelte
@@ -2,11 +2,12 @@
     import {Icon} from "@steeze-ui/svelte-icon";
     import {Bars3} from "@steeze-ui/heroicons";
     import {SmallScreenQuery, XSmallScreenQuery} from "../../../stores";
-    import Drawer from "../../atoms/Drawer/Drawer.svelte";
-    import type {SidebarWidth} from "../../molecules";
-    import Navbar from "../../molecules/Navbar/Navbar.svelte";
-    import SidebarItem from "../../molecules/Sidebar/atoms/SidebarItem.svelte";
-    import Sidebar from "../../molecules/Sidebar/Sidebar.svelte";
+    import {Drawer} from "../../atoms/Drawer";
+    import type {SidebarWidth} from "../../molecules/Sidebar";
+    import {Navbar} from "../../molecules/Navbar";
+    import {Sidebar, SidebarItem} from "../../molecules/Sidebar";
+    import {setContext} from "svelte/types/runtime/internal/lifecycle";
+    import {BREADCRUMB_CONTEXT_KEY, type BreadcrumbContext} from "../../atoms/Breadcrumb";
 
     let sidebarExpanded = false;
     let sidebarType: SidebarWidth = "full";
@@ -20,15 +21,21 @@
             sidebarType = "md";
         }
     }
+    // TODO: Pass breadcrumb context
+    setContext<BreadcrumbContext>(BREADCRUMB_CONTEXT_KEY, []);
 </script>
 
 <main>
     <Navbar class="col-span-2">
         <div slot="contentCenter" class="flex w-full items-center justify-center">
-            <button on:click={() => (sidebarExpanded = true)}><Icon class="w-6 text-gray-50" src={Bars3} /></button>
+            {#if $XSmallScreenQuery}
+                <button on:click={() => (sidebarExpanded = true)}><Icon class="w-6 text-gray-50" src={Bars3} /></button>
+            {/if}
         </div>
     </Navbar>
 
+    <!-- TODO: See if there is a way to do this without redeclaring the sidebar -->
+    <!-- TODO: See if there is a good way to dynamically add to the navigation; Hard coding that is a pain -->
     {#if $XSmallScreenQuery}
         <Drawer bind:open={sidebarExpanded}>
             <Sidebar withHeader={$XSmallScreenQuery} width={sidebarType}>
