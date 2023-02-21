4a9751127a568401ae6c9b674972f1062093de97
Brian D <contact@noimbrian.com>
2023-02-20 22:43:09 -0600

    chore: delete web

--- clients/web/src/lib/components/__debug__/StoreChild.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import {pendingScrims} from "$lib/api";
-</script>
-
-{#each $pendingScrims?.data.pendingScrims ?? [] as scrim}
-    <pre>{JSON.stringify(scrim, null, 2)}</pre>
-{/each}
