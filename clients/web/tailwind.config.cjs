29de0624a65327eddc6b6face57f68dc6e330cac
Brian D <contact@noimbrian.com>
2023-02-21 00:11:43 -0600

    chore: export colors as css vars

--- clients/web/tailwind.config.cjs
+++ clients/web/tailwind.config.cjs
@@ -205,7 +205,27 @@ const config = {
         },
     },
 
-    plugins: [],
+    plugins: [
+        // Expose colors as css variables
+        function({ addBase, theme }) {
+            function extractColorVars(colorObj, colorGroup = '') {
+              return Object.keys(colorObj).reduce((vars, colorKey) => {
+                const value = colorObj[colorKey];
+      
+                const newVars =
+                  typeof value === 'string'
+                    ? { [`--color${colorGroup}-${colorKey}`]: value }
+                    : extractColorVars(value, `-${colorKey}`);
+      
+                return { ...vars, ...newVars };
+              }, {});
+            }
+      
+            addBase({
+              ':root': extractColorVars(theme('colors')),
+            });
+          },
+    ],
 };
 
 module.exports = config;
