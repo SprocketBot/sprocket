789174759c17d6b04ad7bccfcf6676dc88999b5a
Brian D <contact@noimbrian.com>
2023-02-21 00:11:35 -0600

    feat: add centercard layout

--- clients/web/src/lib/components/index.ts
+++ clients/web/src/lib/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./abstract";
 export * from "./actions";
 export * from "./atoms";
+export * from "./layouts";
 export * from "./molecules";
---
+++ clients/web/src/lib/components/layouts/CenterCard/CenterCard.story.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type {Hst as _Hst} from "@histoire/plugin-svelte";
+    import CenterCard from "./CenterCard.svelte";
+    export let Hst: _Hst;
+</script>
+
+<Hst.Story title="Layouts/CenterCard" layout={{type: "single"}}>
+    <Hst.Variant title="Default">
+        <CenterCard>Page Content</CenterCard>
+    </Hst.Variant>
+</Hst.Story>
