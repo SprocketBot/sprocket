543f3320cd57c758376956f53248cf92e0b40eca
Brian D <contact@noimbrian.com>
2023-02-21 00:11:15 -0600

    chore: redo button variants

--- clients/web/src/lib/components/atoms/Button/Button.svelte
+++ clients/web/src/lib/components/atoms/Button/Button.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
+    import type { ButtonVariant } from "./types";
+
+
+    export let variant: ButtonVariant = "default"
     export let type = "primary";
+    let _class: string = ""
+
+    export {_class as class}
+
+    // TODO: Needs Sizes and outlines
 </script>
 
-{#if type === "secondary"}
-    <button
-        type="button"
-        class="text-white bg-secondary-700 hover:bg-primary-800 
-focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm 
-px-5 py-2.5 mr-2 mb-2 focus:outline-none"
-        on:click
-    >
-        <slot />
-    </button>
-{:else}
-    <button
-        type="button"
-        class="text-white bg-primary-700 hover:bg-secondary-800 
-focus:ring-4 focus:ring-secondary-300 font-medium rounded-lg text-sm 
-px-5 py-2.5 mr-2 mb-2 focus:outline-none"
-        on:click
-    >
-        <slot />
-    </button>
-{/if}
+<button class="{_class} button-{variant} focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none flex items-center justify-between">
+    <slot/>
+</button>
+
+<style lang="postcss">
+    .button-default { @apply text-white bg-info-600 hover:bg-info-700 focus:ring-info-800; }
+    .button-alt { @apply border focus:z-10 focus:ring-gray-700 bg-gray-800 text-gray-100 border-gray-600 hover:text-white hover:bg-gray-800; }
+    .button-dark { @apply text-white bg-gray-800 hover:bg-gray-700 focus:ring-gray-700 border-gray-700; }
+    .button-light { @apply  border bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700; }
+    .button-success { @apply text-white  bg-success-600 hover:bg-success-700 focus:ring-success-800; }
+    .button-danger { @apply text-white  bg-danger-600 hover:bg-danger-700 focus:ring-danger-900; }
+    .button-warning { @apply text-white bg-warning-400 hover:bg-warning-500 focus:ring-warning-900; }
+</style>
