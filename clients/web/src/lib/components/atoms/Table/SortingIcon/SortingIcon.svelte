c43e6f9e5e1aaaed2ee03a6615c2a848fddb1233
Zachary Stence <zachstence@gmail.com>
2023-03-03 11:46:29 -0600

    refactor: create SortingIcon

---
+++ clients/web/src/lib/components/atoms/Table/SortingIcon/SortingIcon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { SortDirection } from "@tanstack/svelte-table";
+    import { Icon } from "@steeze-ui/svelte-icon";
+    import { ChevronUp, ChevronDown } from "@steeze-ui/heroicons";
+
+    export let direction: SortDirection | false | undefined = undefined;
+</script>
+
+<span class="w-8 h-8 flex flex-col items-center justify-center cursor-pointer">
+    <Icon src={ChevronUp} theme="mini" class="w-4 h-4 {direction === "asc" ? "text-gray-50" : "text-gray-300"}" />
+    <Icon src={ChevronDown} theme="mini" class="w-4 h-4 {direction === "desc" ? "text-gray-50" : "text-gray-300"}" />
+</span>
