8f6e973ce7b51a13ef345a26976838c47ec23972
Brian D <contact@noimbrian.com>
2023-03-05 02:59:38 -0600

    feat: make auth cookies helpers accept server events

--- clients/web/src/lib/api/auth-cookies.ts
+++ clients/web/src/lib/api/auth-cookies.ts
@@ -1,22 +1,33 @@
 import {setCookie, removeCookie, getCookie} from "typescript-cookie";
+import type { RequestEvent } from '@sveltejs/kit';
 
 const ACCESS_COOKIE_KEY = "sprocket-access-token";
 const REFRESH_COOKIE_KEY = "sprocket-refresh-token";
-export const updateAuthCookies = ({access, refresh}: {access: string; refresh: string}) => {
+export const updateAuthCookies = ({access, refresh}: {access: string; refresh: string}, event?: RequestEvent) => {
     // TODO: Should these cookies expire when the tokens themselves do?
     //       That may create a pretty easy mechanism for checking if the tokens themselves have expired.
 
-    setCookie(ACCESS_COOKIE_KEY, access, {path: "/"});
-    setCookie(REFRESH_COOKIE_KEY, refresh, {path: "/"});
+    if (event) {
+        event.cookies.set(ACCESS_COOKIE_KEY, access, {path: "/"})
+        event.cookies.set(REFRESH_COOKIE_KEY, refresh, {path: "/"});
+    } else {
+        setCookie(ACCESS_COOKIE_KEY, access, {path: "/"});
+        setCookie(REFRESH_COOKIE_KEY, refresh, {path: "/"});
+    }
 };
-export const clearAuthCookies = () => {
-    removeCookie(ACCESS_COOKIE_KEY, {path: "/"});
-    removeCookie(REFRESH_COOKIE_KEY, {path: "/"});
+export const clearAuthCookies = (event?: RequestEvent) => {
+    if (event) {
+        event.cookies.delete(ACCESS_COOKIE_KEY)
+        event.cookies.delete(REFRESH_COOKIE_KEY)
+    } else {
+        removeCookie(ACCESS_COOKIE_KEY, {path: "/"});
+        removeCookie(REFRESH_COOKIE_KEY, {path: "/"});
+    }
 };
 
-export const getAuthCookies = () => {
+export const getAuthCookies = (event?: RequestEvent) => {
     return {
-        access: getCookie(ACCESS_COOKIE_KEY),
-        refresh: getCookie(REFRESH_COOKIE_KEY),
+        access: event ? event.cookies.get(ACCESS_COOKIE_KEY) : getCookie(ACCESS_COOKIE_KEY),
+        refresh: event ? event.cookies.get(REFRESH_COOKIE_KEY) : getCookie(REFRESH_COOKIE_KEY),
     };
 };
