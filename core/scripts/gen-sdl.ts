58b03a7f855e4950b966f0fd6b6ed6a022b68cb1
Brian D <contact@noimbrian.com>
2023-03-05 23:17:29 -0600

    feat: add ability to disable config (useful for CI)

--- core/scripts/gen-sdl.ts
+++ core/scripts/gen-sdl.ts
@@ -9,22 +9,25 @@ async function generateSchema(): Promise<void> {
     const app = await NestFactory.create(GraphQLSchemaBuilderModule);
     await app.init();
 
-
     // This is needed because GraphQLSchemaFactory expects an array of this type
     // eslint-disable-next-line @typescript-eslint/ban-types
     const resolvers: Function[] = []
 
-
     const resolverPaths = await promisify(glob)("./**/*.resolver.ts", { realpath: true })
     await Promise.all(resolverPaths.map(async resolverPath => {
-        const resolverModule = await import(resolverPath)
-        for (const [exportKey, exportValue] of Object.entries(resolverModule)) {
-            if (exportKey.toLowerCase().endsWith("resolver") && typeof exportValue === "function") {
-                resolvers.push(exportValue)
+        try {
+            const resolverModule = await import(resolverPath)
+            for (const [exportKey, exportValue] of Object.entries(resolverModule)) {
+                if (exportKey.toLowerCase().endsWith("resolver") && typeof exportValue === "function") {
+                    resolvers.push(exportValue)
+                }
             }
+        } catch (e) {
+            console.error(`Failed to import ${resolverPath}`)
+            throw e
         }
-    }))
 
+    }))
 
     const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
     const schema = await gqlSchemaFactory.create(resolvers);
