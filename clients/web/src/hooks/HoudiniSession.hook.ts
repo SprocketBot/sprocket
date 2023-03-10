0a0235305499e4e086e151667657998136fd3a73
Brian D <contact@noimbrian.com>
2023-03-09 22:31:14 -0600

    chore: session tweaks

--- clients/web/src/hooks/HoudiniSession.hook.ts
+++ clients/web/src/hooks/HoudiniSession.hook.ts
@@ -2,7 +2,6 @@ import {setSession} from "$houdini";
 import type {Handle} from "@sveltejs/kit";
 
 export const HoudiniSessionHook: Handle = async ({event, resolve}) => {
-    console.debug("Setting Houdini Session");
     // Get session information
     const access = event.cookies.get("sprocket-access-token");
     const refresh = event.cookies.get("sprocket-refresh-token");
@@ -10,5 +9,5 @@ export const HoudiniSessionHook: Handle = async ({event, resolve}) => {
     // Set the session
     setSession(event, {access, refresh});
     // pass the event onto the default handle
-    return await resolve(event);
+    return resolve(event);
 };
