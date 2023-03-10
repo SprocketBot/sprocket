b4f7683875bb0192d5cae6b67d3514947dd500d0
Brian D <contact@noimbrian.com>
2023-03-09 22:35:19 -0600

    feat: sidebar improvements

--- clients/web/src/lib/components/molecules/Sidebar/atoms/index.ts
+++ clients/web/src/lib/components/molecules/Sidebar/atoms/index.ts
@@ -1,2 +1,3 @@
 export {default as SidebarDivider} from "./SidebarDivider.svelte";
 export {default as SidebarItem} from "./SidebarItem.svelte";
+export {default as SidebarGroup} from "./SidebarGroup.svelte";
