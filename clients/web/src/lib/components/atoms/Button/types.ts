ff349eb6b54e113120bcc528dbfb068195002ca5
Brian D <contact@noimbrian.com>
2023-03-09 22:30:25 -0600

    feat: add button outline variants

--- clients/web/src/lib/components/atoms/Button/types.ts
+++ clients/web/src/lib/components/atoms/Button/types.ts
@@ -1,6 +1,6 @@
 import type {Action} from "svelte/action";
 
-export type ButtonVariant = "default" | "alt" | "dark" | "light" | "success" | "danger" | "warning";
+export type ButtonVariant = "default" | "alt" | "success" | "danger" | "warning";
 export type ButtonSize = "default" | "xs" | "sm" | "lg" | "xl";
 // export type ProgressBarSize = "sm" | "md" | "lg" | "xl";
 // export type ProgressLocation = "hidden" | "inside" | "outside";
