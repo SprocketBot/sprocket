8d38c08e1aa7a73c7f41e6c6cf1f404a7f216ae3
Brian D <contact@noimbrian.com>
2023-03-09 22:35:19 -0600

    feat: sidebar improvements

--- clients/web/src/lib/components/molecules/Sidebar/atoms/SidebarGroup.svelte
+++ clients/web/src/lib/components/molecules/Sidebar/atoms/SidebarGroup.svelte
@@ -3,7 +3,7 @@
     import {Icon} from "@steeze-ui/svelte-icon";
     import {ChevronUp} from "@steeze-ui/heroicons";
     import type {IconSource} from "@steeze-ui/svelte-icon/types";
-    import SidebarItem from "./atoms/SidebarItem.svelte";
+    import SidebarItem from "./SidebarItem.svelte";
 
     export let icon: IconSource | undefined;
     export let label: string;
@@ -12,10 +12,12 @@
 </script>
 
 <SidebarItem {label} {icon} on:click={() => (collapsed = !collapsed)}>
-    <span class="transition duration-500" class:-rotate-180={collapsed}><Icon src={ChevronUp} class="h-5 w-5" /></span>
+    <span class="transition-transform duration-500" class:-rotate-180={collapsed}
+        ><Icon src={ChevronUp} class="h-5 w-5" /></span
+    >
     <div slot="siblings" class="contents">
         {#if !collapsed}
-            <div transition:slide>
+            <div transition:slide class="pl-6">
                 <slot />
             </div>
         {/if}
