b6feb70d518852bcbaf1bb00bf9cd9b1291b93fc
Brian D <contact@noimbrian.com>
2023-03-09 22:31:51 -0600

    chore: care less about houdini session

--- clients/web/src/routes/+layout.svelte
+++ clients/web/src/routes/+layout.svelte
@@ -1,21 +1,16 @@
-<script>
+<script lang="ts">
     import "@fontsource/montserrat";
     import "../app.postcss";
-    import {getSession} from "$houdini";
     import {getAuthCookies} from "$lib/api/auth-cookies";
     import {ToastContainer} from "$lib/components";
     import {SetJwtContext} from "$lib/context";
 
-    let session;
-
-    getSession().then(r => (session = r));
-
     SetJwtContext({
         get access() {
-            return session?.access ?? getAuthCookies().access;
+            return getAuthCookies().access;
         },
         get refresh() {
-            return session?.refresh ?? getAuthCookies().refresh;
+            return getAuthCookies().refresh;
         },
     });
 </script>
