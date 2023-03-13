ef8efd9291194b268995ff09a6b2b4676a3fbea6
Dan Aimone <30274286+danaimone@users.noreply.github.com>
2023-03-13 11:20:54 -0700

    bugfix: remove just utilmodule and not the entire import...

--- microservices/submission-service/src/replay-validation/replay-validation.module.ts
+++ microservices/submission-service/src/replay-validation/replay-validation.module.ts
@@ -1,17 +1,12 @@
 import {forwardRef, Module} from "@nestjs/common";
+import {CoreModule, MatchmakingModule, MinioModule} from "@sprocketbot/common";
 
 import {ReplaySubmissionModule} from "../replay-submission/replay-submission.module";
 import {ReplayValidationController} from "./replay-validation.controller";
 import {ReplayValidationService} from "./replay-validation.service";
 
 @Module({
-    imports: [
-        MatchmakingModule,
-        CoreModule,
-        MinioModule,
-        forwardRef(() => ReplaySubmissionModule),
-        UtilModule,
-    ],
+    imports: [MatchmakingModule, CoreModule, MinioModule, forwardRef(() => ReplaySubmissionModule), UtilModule],
     providers: [ReplayValidationService],
     exports: [ReplayValidationService],
     controllers: [ReplayValidationController],
