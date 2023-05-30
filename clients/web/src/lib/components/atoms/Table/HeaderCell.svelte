dd8d0d302bc3603d3aef9ae1953bc7b16440c081
Brian D <contact@noimbrian.com>
2023-05-29 22:25:43 -0500

    style: format

--- clients/web/src/lib/components/atoms/Table/HeaderCell.svelte
+++ clients/web/src/lib/components/atoms/Table/HeaderCell.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import { flexRender , type Header } from "@tanstack/svelte-table";
+    import {flexRender, type Header} from "@tanstack/svelte-table";
 
-    import { hoverable } from "$lib/actions";
+    import {hoverable} from "$lib/actions";
 
-    import { SortingIcon } from "./SortingIcon";
+    import {SortingIcon} from "./SortingIcon";
 
     export let header: Header<any, unknown>;
     export let colspan = 1;
@@ -23,7 +23,7 @@
             on:keypress={onSort}
             class="w-full h-full flex items-center justify-center gap-2 p-4 text-sm font-bold text-white uppercase"
             class:cursor-pointer={sortable}
-            use:hoverable={{ onHoverChange: v => hovered = v }}
+            use:hoverable={{onHoverChange: v => (hovered = v)}}
         >
             <svelte:component this={content} />
 
