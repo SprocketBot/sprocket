781e278d89807b8f3e938b774725f343baf4a97e
Jake Bailey <asaxplayinghorse@gmail.com>
2023-08-03 22:09:26 +0000

    feat: create league and observe pages

---
+++ clients/web/src/routes/(app-pages)/app/scrims/observe/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import {OpenScrimsContext} from "$lib/api";
+    import {Button} from "$lib/components";
+
+    const openScrims = OpenScrimsContext();
+</script>
+
+{#each $openScrims ?? [] as scrim}
+    {scrim.id} | {scrim.status}
+{:else}
+    "Hi, there are no currently observable scrims available."
+{/each}
