d0215e2a660a6d2bd8c7ecbe324b4463c7584fa9
Jake Bailey <asaxplayinghorse@gmail.com>
2023-08-03 22:09:26 +0000

    feat: create league and observe pages

---
+++ clients/web/src/routes/(app-pages)/app/league/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import {UserProfileStore} from "$houdini";
+    import {onMount} from "svelte";
+
+    const profileStore = new UserProfileStore();
+
+    onMount(() => {
+        profileStore.fetch();
+    });
+</script>
+
+"This is where we'll put the league match submissions page."
