acfd9e680929dca876ea4a21cf795a49143930ce
Jake Bailey <asaxplayinghorse@gmail.com>
2023-08-25 06:34:48 +0000

    feat: *rainbows*

--- clients/web/src/lib/components/molecules/FranchiseVsCard/FranchiseVsCard.svelte
+++ clients/web/src/lib/components/molecules/FranchiseVsCard/FranchiseVsCard.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type {LeagueScheduleFranchise} from "../../../api";
+	import type {LeagueScheduleFranchise} from "$lib/api";
 	import Color from "color";
 	export let profile: LeagueScheduleFranchise;
 
@@ -8,14 +8,34 @@
 	let applicableFontVar = "";
 	const primary = new Color(profile.primaryColor);
 	const secondary = new Color(profile.secondaryColor);
-	$: applicableFontVar = primary.luminosity() > secondary.luminosity() ? "text-context-primary" : "text-context-secondary";
-
+	$: applicableFontVar = primary.luminosity() > secondary.luminosity() ? profile.primaryColor : profile.secondaryColor;
+	
 </script>
 
 
 <div class="flex-1 flex-col items-center gap-2" style="--primary-color: {profile.primaryColor}; --secondary-color: {profile.secondaryColor}">
-	<div class="aspect-square rounded-full w-16 h-16 p-2 mx-auto bg-gradient-to-br from-context-primary to-context-secondary flex justify-center items-center">
+	<div class="aspect-square rounded-full w-16 h-16 p-2 mx-auto
+	bg-gradient-to-br from-context-primary to-context-secondary flex
+	justify-center items-center">
 		<img class="max-w-full max-h-full"  src={profile.photo?.url} alt={profile.title}/>
 	</div>
-	<span class="block text-xl { applicableFontVar } font-bold text-center">{profile.title}</span>
+	<span class="block text-xl font-bold text-center" style="color:
+	{applicableFontVar}">{profile.title}</span>
 </div>
+
+<style lang="postcss">
+	span {
+		position: relative;
+	}
+
+	span::before {
+		content: "";
+		position: absolute;
+		top: 100%;
+		width: 100%;
+		left: 0;
+		height: 5px;
+		border-radius: 2px;
+		background: linear-gradient(111.3deg, var(--primary-color), var(--secondary-color));
+	}
+</style>
