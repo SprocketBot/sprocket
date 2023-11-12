e5fb481825946e191413047b7e6197361afcc6d9
Jake Bailey <asaxplayinghorse@gmail.com>
2023-11-12 14:27:06 -0800

    feat: finish member object

--- core/src/organization/graphql/member.object.ts
+++ core/src/organization/graphql/member.object.ts
@@ -2,8 +2,6 @@ import {Field, Int,ObjectType} from "@nestjs/graphql";
 
 import { PlayerObject } from "../../franchise/graphql/player.object";
 import {BaseObject} from "../../types/base-object";
-import type { Member } from "../database/member.entity";
-import type { MemberProfile } from "../database/member-profile.entity";
 import { MemberPlatformAccountObject } from "./member-platform-account.object";
 import { MemberProfileObject } from "./member-profile.object";
 import { MemberRestrictionObject } from "./member-restriction.object";
@@ -24,19 +22,4 @@ export class MemberObject extends BaseObject {
 
     @Field(() => Int)
     restrictions: MemberRestrictionObject;
-
-    @Field(() => Int)
-    organizationStaffSeats: OrganizationStaffSeat[];
 }
-
-// export function memberObjectFromEntity(entity: Member, profile: MemberProfile): MemberObject {
-    // return {
-        // id: entity.id,
-        // createdAt: entity.createdAt,
-        // updatedAt: entity.updatedAt,
-        // deletedAt: entity.deletedAt,
-        // players: entity.players,
-
-        // profile: profile,
-    // }
-// }
