d3553426a4e7435b02a4addc65535817a1e2f120
Brian D <contact@noimbrian.com>
2024-06-10 21:54:40 -0500

    chore: point to core

--- clients/web/houdini.config.js
+++ clients/web/houdini.config.js
@@ -3,9 +3,18 @@ const config = {
 	watchSchema: {
 		url: (env) => {
 			// Use URL to remove the port if it matches the protocol (e.g. 443 doesn't need to be specified if it's https)
-			const rawUrl = new URL(
-				`${env.PUBLIC_API_SECURE.toLowerCase() === 'true' ? 'https' : 'http'}://${env.PUBLIC_API_URL}:${env.PUBLIC_API_PORT ?? 443}`
-			).toString();
+			const {
+				PUBLIC_API_URL,
+				PUBLIC_API_PORT,
+				INTERNAL_API_URL,
+				INTERNAL_API_PORT,
+				PUBLIC_API_SECURE
+			} = env;
+			const rawUrl = INTERNAL_API_URL
+				? new URL(`http://${INTERNAL_API_URL}:${INTERNAL_API_PORT ?? 3000}`).toString()
+				: new URL(
+						`${PUBLIC_API_SECURE.toLowerCase() === 'true' ? 'https' : 'http'}://${PUBLIC_API_URL}:${PUBLIC_API_PORT ?? 443}`
+					).toString();
 			// Remove the trailing '/' to make it easier to work with
 			const apiUrl = rawUrl.endsWith('/') ? rawUrl.substring(0, rawUrl.length - 1) : rawUrl;
 			console.log(`Using Houdini (config) API @ ${apiUrl}/graphql`);
@@ -24,7 +33,7 @@ const config = {
 			/** @param {Date} v */
 			marshal: (v) => v.toISOString(),
 			/** @param {string} v */
-			unmarshal: (v) => new Date(v)			
+			unmarshal: (v) => new Date(v)
 		}
 	},
 	features: {
--- clients/web/src/lib/constants.ts
+++ clients/web/src/lib/constants.ts
@@ -1,9 +1,14 @@
+import { browser } from '$app/environment';
 import { env } from '$env/dynamic/public';
+import { env as privEnv } from '$env/dynamic/private';
 const { PUBLIC_API_URL, PUBLIC_API_SECURE, PUBLIC_API_PORT } = env;
+const { INTERNAL_API_URL, INTERNAL_API_PORT } = privEnv;
 
 // Use URL to remove the port if it matches the protocol (e.g. 443 doesn't need to be specified if it's https)
-const rawUrl = new URL(
+const rawUrl = browser
+	? new URL(
 	`${PUBLIC_API_SECURE.toLowerCase() === 'true' ? 'https' : 'http'}://${PUBLIC_API_URL}:${PUBLIC_API_PORT}`
-).toString();
+	).toString()
+	: new URL(`http://${INTERNAL_API_URL ?? PUBLIC_API_URL}:${INTERNAL_API_PORT ?? PUBLIC_API_PORT}`).toString();
 // Remove the trailing '/' to make it easier to work with
 export const apiUrl = rawUrl.endsWith('/') ? rawUrl.substring(0, rawUrl.length - 1) : rawUrl;
