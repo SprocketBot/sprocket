335f88cb6aed34df2390de3f5e13017ca2be1294
Jake Bailey <asaxplayinghorse@gmail.com>
2024-07-09 17:39:07 -0700

    feat: tie it all together by creating the fixtures.

--- core/src/scheduling/schedule-fixture/schedule-fixture.service.ts
+++ core/src/scheduling/schedule-fixture/schedule-fixture.service.ts
@@ -2,12 +2,18 @@ import {Injectable} from "@nestjs/common";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 
-import {ScheduleFixture} from "../../database";
+import {GameSkillGroup, Match, MatchParent, ScheduleFixture, ScheduleGroup} from "../../database";
+import { FranchiseService } from "../../franchise/franchise";
+import { MatchService } from "../match";
 
 @Injectable()
 export class ScheduleFixtureService {
-    constructor(@InjectRepository(ScheduleFixture)
-    private readonly scheduleFixtureRepo: Repository<ScheduleFixture>) {}
+    constructor(
+        @InjectRepository(ScheduleFixture)
+        private readonly scheduleFixtureRepo: Repository<ScheduleFixture>,
+        private readonly franchiseService: FranchiseService,
+        private readonly matchService: MatchService,
+    ) {}
 
     async getFixturesForGroup(groupId: number): Promise<ScheduleFixture[]> {
         return this.scheduleFixtureRepo.find({
@@ -29,5 +35,44 @@ export class ScheduleFixtureService {
             },
             relations: ["awayFranchise", "awayFranchise.profile", "homeFranchise", "homeFranchise.profile"],
         });
+    }
+    
+    async createScheduleFixture(schedule_group: ScheduleGroup, home_name: string, away_name: string, skill_groups: GameSkillGroup[]): Promise<ScheduleFixture> {
+
+        // Get both franchises involved
+        const home = await this.franchiseService.getFranchiseByName(home_name);
+        const away = await this.franchiseService.getFranchiseByName(away_name);
+
+        // Create the fixture
+        let fixture = this.scheduleFixtureRepo.create();
+        fixture.scheduleGroup = schedule_group;
+        fixture.homeFranchise = home;
+        fixture.awayFranchise = away;
+        fixture.homeFranchiseId = home.id;
+        fixture.awayFranchiseId = away.id;
+        
+        // Create the match parents
+        let matches: Match[] = [];
+        let mps: MatchParent[] = [];
+
+        for (const sg of skill_groups) {
+            const [m, mp] = await this.matchService.createMatchWithMatchParent(sg);
+            matches.push(m);
+            mps.push(mp);
+        }
+
+        // Put it all back together in the DB and return
+        fixture = this.scheduleFixtureRepo.merge(
+            fixture,
+            {
+                scheduleGroup: schedule_group,
+                homeFranchise: home,
+                awayFranchise: away,
+                homeFranchiseId: home.id,
+                awayFranchiseId: away.id,
+                matchParents: mps,
+                matches: matches,
+            });
+        return fixture;
     }
 }
