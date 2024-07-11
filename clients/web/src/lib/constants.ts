99fb253758b0f7ad1cc90e3dabfb2e4471f41449
brian <contact@noimbrian.com>
2024-07-11 13:26:30 -0500

    fix: try to fix logout button

--- clients/web/src/lib/constants.ts
+++ clients/web/src/lib/constants.ts
@@ -25,3 +25,10 @@ if (typeof window === "undefined") {
 // Use URL to remove the port if it matches the protocol (e.g. 443 doesn't need to be specified if it's https)
 // Remove the trailing '/' to make it easier to work with
 export const apiUrl = rawUrl.endsWith('/') ? rawUrl.substring(0, rawUrl.length - 1) : rawUrl;
+
+
+const rawPublicUrl = new URL(
+	`${PUBLIC_API_SECURE.toLowerCase() === 'true' ? 'https' : 'http'}://${PUBLIC_API_URL}:${PUBLIC_API_PORT ?? 443}`
+).toString()
+
+export const publicApiUrl = rawPublicUrl.endsWith('/') ? rawPublicUrl.substring(0, rawPublicUrl.length - 1) : rawPublicUrl;
--- clients/web/src/routes/(auth)/logout/+page.ts
+++ clients/web/src/routes/(auth)/logout/+page.ts
@@ -1,6 +1,6 @@
 import { redirect } from '@sveltejs/kit';
-import { apiUrl } from '../../../lib/constants';
+import { publicApiUrl } from '../../../lib/constants';
 
 export const load = () => {
-	throw redirect(301, `${apiUrl}/auth/logout`);
+	throw redirect(301, `${publicApiUrl}/auth/logout`);
 };
