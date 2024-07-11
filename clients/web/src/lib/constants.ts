01dcf8ecc960e41c9a9f70f991d76a23a6e05288
Jake Bailey <asaxplayinghorse@gmail.com>
2024-07-11 10:38:51 -0700

    fix: have web use a different api url depending on when its in the browser or server

--- clients/web/src/lib/constants.ts
+++ clients/web/src/lib/constants.ts
@@ -4,9 +4,23 @@ const { PUBLIC_API_URL, PUBLIC_API_SECURE, PUBLIC_API_PORT } = env;
 if (!PUBLIC_API_SECURE || !PUBLIC_API_URL || !PUBLIC_API_PORT) 
 	throw new Error("Missing required environment variables")
 
+let rawUrl
+if (typeof window === "undefined") {
+    const { PRIVATE_API_URL, PRIVATE_API_SECURE, PRIVATE_API_PORT } = process.env;
+    if (!PRIVATE_API_SECURE || !PRIVATE_API_URL || !PRIVATE_API_PORT)
+        throw new Error("Missing required environment variables")
+
+    // we are in server land
+    rawUrl = new URL(
+        `${PRIVATE_API_SECURE.toLowerCase() === 'true' ? 'https' : 'http'}://${PRIVATE_API_URL}:${PRIVATE_API_PORT ?? 443}`
+    ).toString();
+} else {
+    // we are in browser land
+    rawUrl = new URL(
+        `${PUBLIC_API_SECURE.toLowerCase() === 'true' ? 'https' : 'http'}://${PUBLIC_API_URL}:${PUBLIC_API_PORT ?? 443}`
+    ).toString();
+}
+
 // Use URL to remove the port if it matches the protocol (e.g. 443 doesn't need to be specified if it's https)
-const rawUrl = new URL(
-	`${PUBLIC_API_SECURE.toLowerCase() === 'true' ? 'https' : 'http'}://${PUBLIC_API_URL}:${PUBLIC_API_PORT}`
-).toString();
 // Remove the trailing '/' to make it easier to work with
 export const apiUrl = rawUrl.endsWith('/') ? rawUrl.substring(0, rawUrl.length - 1) : rawUrl;
