c6bffcaf6fb5d4e15eecac6bff20c2be3033fbdf
Jake Bailey <asaxplayinghorse@gmail.com>
2024-12-14 15:35:59 -0800

    feat: create role entity, resource constants, and update model

---
+++ core/src/db/role/role.entity.ts
@@ -0,0 +1,10 @@
+import { BaseEntity } from '../base.entity';
+import { UserEntity } from '../user/user.entity';
+import { ManyToOne } from 'typeorm';
+import { Entity } from 'typeorm';
+
+@Entity('round', { schema: 'sprocket' })
+export class RoleEntity extends BaseEntity {
+  @ManyToOne(() => UserEntity)
+  user: Promise<UserEntity>;
+}
