2685add0723b2eba278756663a580d1e56e92676
Jake Bailey <asaxplayinghorse@gmail.com>
2024-12-19 16:24:36 -0800

    feat: Add new mutation to core to handle LFS scrims

---
+++ core/src/scrim/types/CreateLFSScrimInput.ts
@@ -0,0 +1,27 @@
+import {
+    Field, InputType, Int,
+} from "@nestjs/graphql";
+
+import type {ScrimPlayer} from "./ScrimPlayer";
+import {ScrimSettingsInput} from "./ScrimSettings";
+
+@InputType()
+export class CreateLFSScrimInput {
+    @Field(() => Int)
+    gameModeId: number;
+
+    @Field()
+    settings: ScrimSettingsInput;
+
+    @Field({nullable: true})
+    createGroup: boolean;
+
+    @Field(() => Int)
+    leaveAfter: number;
+    
+    @Field(() => Int)
+    numRounds: number;
+
+    @Field()
+    players: ScrimPlayer[];
+}
