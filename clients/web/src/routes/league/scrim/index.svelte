3358f1c660ad724587d476d93c99b753f49996e0
Jake Bailey <asaxplayinghorse@gmail.com>
2024-12-29 12:56:20 -0800

    feat: flesh out LFS index page

--- clients/web/src/routes/league/scrim/index.svelte
+++ clients/web/src/routes/league/scrim/index.svelte
@@ -1,47 +1,35 @@
 <script lang="ts">
 	import {
-	    DashboardLayout, DashboardCard, Spinner, LeagueScheduleGroup,
+	    DashboardLayout, DashboardCard, Spinner
 	} from "$lib/components";
 	import {
-	    LeagueScheduleStore, type LeagueScheduleSeason, type LeagueScheduleWeek,
+		LFSScrimsStore, type LFSScrim
 	} from "$lib/api";
 
-	const store = new LeagueScheduleStore();
+	const store = new LFSScrimsStore();
 	let fetching = true;
-	let schedule: LeagueScheduleSeason | undefined;
-	let scheduleGroups: LeagueScheduleWeek[] = [];
-	let currentWeek: LeagueScheduleWeek | undefined;
+	let scrims: LFSScrim[] | undefined = [];
 
 	$: {
-	    // @ts-expect-error `fetching` exists on the query store but isn't defined in the type
+		// @ts-expect-error `fetching` exists on the query store but isn't defined in the type
 	    fetching = $store.fetching;
-
-	    schedule = $store.data?.seasons[0];
-	    scheduleGroups = schedule ? schedule?.childGroups.sort((a, b) => Date.parse(a.start) - Date.parse(b.start)) : [];
-
-		const nextWeekIndex = scheduleGroups.findIndex(sg => Date.parse(sg.start) > Date.now());
-		const currentWeekIndex = nextWeekIndex >= 1 ? nextWeekIndex - 1 : -1
-		currentWeek = currentWeekIndex ? scheduleGroups[currentWeekIndex] : undefined
+		scrims = $store.data?.LFSScrims;
 	}
 </script>
 
 <DashboardLayout>
-	<DashboardCard class="col-span-8 row-span-3" title="League Play Schedule">
+	<DashboardCard class="col-span-8 row-span-3" title="LFS (Team) Scrims">
 		{#if fetching}
 			<div class="h-full w-full flex items-center justify-center">
 				<Spinner class="h-16 w-full"/>
 			</div>
-		{:else if schedule}
-			<h2 class="text-2xl text-accent font-bold mb-8">{schedule.game.title} | {schedule.description}</h2>
-
-			<div class="flex flex-col gap-10">
-				{#if currentWeek}
-					<LeagueScheduleGroup scheduleGroup={currentWeek} isCurrentWeek />
-				{/if}
-
-				{#each scheduleGroups as week (week?.id)}
-					<LeagueScheduleGroup scheduleGroup={week} />
-				{/each}
+		{:else if scrims}
+			{#each scrims as scrim (scrim.id)}
+				<h2 class="text-2xl text-accent font-bold mb-8">{scrim.id}</h2>
+			{/each}
+		{:else}
+			<div class="h-full w-full flex items-center justify-center">
+				No scrims found
 			</div>
 		{/if}
 	</DashboardCard>
