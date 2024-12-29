8ac3e10732dfcfc02ed918be1df634f510aefaa5
Jake Bailey <asaxplayinghorse@gmail.com>
2024-12-29 12:55:51 -0800

    feat: add LFS to nav

--- clients/web/src/lib/stores/navigation.store.ts
+++ clients/web/src/lib/stores/navigation.store.ts
@@ -2,6 +2,7 @@ import {writable} from "svelte/store";
 
 export const SCRIM_NAV_ITEM = {target: "/scrims", label: "Play"};
 export const LEAGUE_NAV_ITEM = {target: "/league", label: "League Play"};
+export const LFS_NAV_ITEM = {target: "/league/scrim", label: "LFS Scrims"};
 export const ADMIN_NAV_ITEM = {target: "/admin", label: "Admin"};
 
 export interface NavigationItem {
@@ -9,4 +10,4 @@ export interface NavigationItem {
     target: string;
 }
 
-export const navigationStore = writable<NavigationItem[]>([SCRIM_NAV_ITEM, LEAGUE_NAV_ITEM]);
+export const navigationStore = writable<NavigationItem[]>([SCRIM_NAV_ITEM, LEAGUE_NAV_ITEM, LFS_NAV_ITEM]);
