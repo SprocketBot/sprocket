e88e5ad3457c7f36cf1387f2beafb32dadd1d10f
Jake Bailey <asaxplayinghorse@gmail.com>
2025-01-02 13:31:55 -0800

    feat: After upload, we now properly show replay parse status.

---
+++ clients/web/src/routes/league/scrim/[submissionId].svelte
@@ -0,0 +1,31 @@
+<script context="module" lang="ts">
+  export const load = ({ params }: unknown): unknown => ({
+    props: {
+      submissionId: params.submissionId,
+    },
+  });
+</script>
+
+<script lang="ts">
+  import type { SubmissionStoreValue } from "$lib/api";
+  import { SubmissionStore } from "$lib/api";
+  import { SubmissionView } from "$lib/components";
+  import DashboardLayout from "../../../lib/components/layouts/DashboardLayout.svelte";
+  import UploadReplaysModal from "../../../lib/components/organisms/scrims/modals/UploadReplaysModal.svelte";
+
+  export let submissionId: string;
+  let submissionStore: SubmissionStore | undefined;
+  $: if (submissionId) {
+    submissionStore = new SubmissionStore(submissionId);
+  }
+  let submission: SubmissionStoreValue["submission"] | undefined;
+  $: if ($submissionStore) submission = $submissionStore?.data?.submission;
+</script>
+
+<DashboardLayout>
+  {#if submission}
+    <SubmissionView {submission} />
+  {:else}
+    <UploadReplaysModal {submissionId} />
+  {/if}
+</DashboardLayout>
--- clients/web/src/routes/league/scrim/index.svelte
+++ clients/web/src/routes/league/scrim/index.svelte
@@ -1,56 +1,66 @@
 <script lang="ts">
-	import {
-	    CreateLFSScrimModal, DashboardLayout, DashboardCard, ScrimTable, Spinner, UploadReplaysModal
-	} from "$lib/components";
-	import {
-		LFSScrimsStore, type CurrentScrim
-	} from "$lib/api";
+  import {
+    CreateLFSScrimModal,
+    DashboardLayout,
+    DashboardCard,
+    ScrimTable,
+    Spinner,
+    UploadReplaysModal,
+  } from "$lib/components";
+  import { LFSScrimsStore, type CurrentScrim } from "$lib/api";
 
-	const store = new LFSScrimsStore();
-	let fetching = true;
-	let uploading = false;
-	let creating = false;
-	let submissionId: string = "";
-	let scrims: CurrentScrim[] | undefined = [];
+  const store = new LFSScrimsStore();
+  let fetching = true;
+  let uploading = false;
+  let creating = false;
+  let submissionId: string = "";
+  let scrims: CurrentScrim[] | undefined = [];
 
-	$: {
-		// @ts-expect-error `fetching` exists on the query store but isn't defined in the type
-	    fetching = $store.fetching;
-		scrims = $store.data?.LFSScrims;
-	}
+  $: {
+    // @ts-expect-error `fetching` exists on the query store but isn't defined in the type
+    fetching = $store.fetching;
+    scrims = $store.data?.LFSScrims;
+  }
 
-	const openUploadModal = (scrim: CurrentScrim) => {
-		console.log(JSON.stringify(scrim));
-        submissionId = scrim.submissionId ?? "";
-        uploading = true;
-    };
-	
-	const openCreateScrimModal = () => {
-		creating = true;
-	}
+  const openUploadModal = (scrim: CurrentScrim) => {
+    console.log(JSON.stringify(scrim));
+    submissionId = scrim.submissionId ?? "";
+    window.location.href = `/league/scrim/${submissionId}`;
+  };
+
+  const openCreateScrimModal = () => {
+    creating = true;
+  };
 </script>
 
 <DashboardLayout>
-	<DashboardCard class="col-span-8 row-span-3" title="LFS (Team) Scrims">
-		<div class="flex flex-col md:flex-row justify-between mb-4">
-			<h2>Available Scrims</h2>
-			<button class="btn btn-primary w-full md:w-auto" on:click={openCreateScrimModal}>
-				Create LFS Scrim
-			</button>
-		</div>
-		{#if fetching}
-			<div class="h-full w-full flex items-center justify-center">
-				<Spinner class="h-16 w-full"/>
-			</div>
-		{:else if scrims}
-			<ScrimTable scrims={scrims} lfs={true} joinScrim={(scrim) => openUploadModal(scrim)}/>
-		{:else}
-			<div class="h-full w-full flex items-center justify-center">
-				No scrims found
-			</div>
-		{/if}
-	</DashboardCard>
+  <DashboardCard class="col-span-8 row-span-3" title="LFS (Team) Scrims">
+    <div class="flex flex-col md:flex-row justify-between mb-4">
+      <h2>Available Scrims</h2>
+      <button
+        class="btn btn-primary w-full md:w-auto"
+        on:click={openCreateScrimModal}
+      >
+        Create LFS Scrim
+      </button>
+    </div>
+    {#if fetching}
+      <div class="h-full w-full flex items-center justify-center">
+        <Spinner class="h-16 w-full" />
+      </div>
+    {:else if scrims}
+      <ScrimTable
+        {scrims}
+        lfs={true}
+        joinScrim={(scrim) => openUploadModal(scrim)}
+      />
+    {:else}
+      <div class="h-full w-full flex items-center justify-center">
+        No scrims found
+      </div>
+    {/if}
+  </DashboardCard>
 </DashboardLayout>
 
-<UploadReplaysModal bind:visible={uploading} submissionId={submissionId}/>
+<UploadReplaysModal bind:visible={uploading} {submissionId} />
 <CreateLFSScrimModal bind:visible={creating} />
