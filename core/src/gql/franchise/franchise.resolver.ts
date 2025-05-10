c55265ce319c6736dda4fa3c8a79fd4ede6899a9
Jake Bailey <asaxplayinghorse@gmail.com>
2025-05-09 20:46:04 -0700

    Add franchise to the GQL API

--- core/src/gql/franchise/franchise.resolver.ts
+++ core/src/gql/franchise/franchise.resolver.ts
@@ -2,7 +2,7 @@ import { ResolveField, Resolver } from '@nestjs/graphql';
 import { FranchiseObject } from './franchise.object';
 
 @Resolver(() => FranchiseObject)
-export class UserAuthAccountResolver {
+export class FranchiseResolver {
   @ResolveField(() => FranchiseObject)
   async getFranchise(id: number): Promise<FranchiseObject> {
     throw new Error('Not yet implemented');
