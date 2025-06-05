993e3ee0e3a37d840b2b91d51bcdff6f52cdf0bd
Matthieu <matthieu.trudel@gmail.com>
2025-06-04 22:27:49 -0400

    missed one small change

--- core/src/db/game_mode/game_mode.entity.ts
+++ core/src/db/game_mode/game_mode.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, ManyToOne, Unique } from 'typeorm';
 import { GameEntity } from '../game/game.entity';
 
 @Entity('game_mode', { schema: 'sprocket' })
-@Unique('game-name-unq', ['gameId', 'name'])
+@Unique('game-name-unq', ['game', 'name'])
 export class GameModeEntity extends BaseEntity {
 	@ManyToOne(() => GameEntity, (ge) => ge.gameModes)
 	game?: GameEntity;
