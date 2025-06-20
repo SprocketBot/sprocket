888fbb6591cf03617bd363c44edd735d3b75d169
Matthieu <matthieu.trudel@gmail.com>
2025-06-19 22:17:23 -0400

    removing baseobject fields in club.object

--- core/src/gql/club/club.object.ts
+++ core/src/gql/club/club.object.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { ObjectType } from '@nestjs/graphql';
 import { TeamObject } from '../team/team.object';
 import { FranchiseObject } from '../franchise/franchise.object';
 import { GameObject } from '../game/game.object';
@@ -6,15 +6,6 @@ import { BaseObject } from '../base.object';
 
 @ObjectType('Club')
 export class ClubObject extends BaseObject {
-	@Field()
-	id: string;
-
-	@Field()
-	createdAt: Date;
-
-	@Field()
-	updateAt: Date;
-
 	// Field is implicit because of ResolveField
 	franchise: FranchiseObject;
 
