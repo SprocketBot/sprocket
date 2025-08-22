752384eeadcd7fe79377f9d04413e182a745aca8
Jake Bailey <asaxplayinghorse@gmail.com>
2025-08-22 15:18:06 -0700

    fix: Config changes fixed previous type errors

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,48 +1,47 @@
-import {ValidationPipe} from "@nestjs/common";
-import {NestFactory} from "@nestjs/core";
-import {Transport} from "@nestjs/microservices";
-import {AllExceptionsFilter, config} from "@sprocketbot/common";
-import {writeFile} from "fs/promises";
-import {SpelunkerModule} from "nestjs-spelunker";
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { Transport } from "@nestjs/microservices";
+import { AllExceptionsFilter, config } from "@sprocketbot/common";
+import { writeFile } from "fs/promises";
+import { SpelunkerModule } from "nestjs-spelunker";
 
-import {AppModule} from "./app.module";
+import { AppModule } from "./app.module";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function writeDepGraph(app: Awaited<ReturnType<typeof NestFactory.create>>): Promise<void> {
-    // Teo generate graph call this in main, then start the app
-    const tree = SpelunkerModule.explore(app);
-    const root = SpelunkerModule.graph(tree);
-    const edges = SpelunkerModule.findGraphEdges(root);
-    const mermaidEdges = edges.map(({from, to}) => `${from.module.name}-->${to.module.name}`);
-    const fileContent = `\`\`\`mermaid\nflowchart\n\t${mermaidEdges.join("\n\t")}\n\`\`\``;
-    await writeFile(`${__dirname}/../DepTree.md`, fileContent);
+  // Teo generate graph call this in main, then start the app
+  const tree = SpelunkerModule.explore(app);
+  const root = SpelunkerModule.graph(tree);
+  const edges = SpelunkerModule.findGraphEdges(root);
+  const mermaidEdges = edges.map(({ from, to }) => `${from.module.name}-->${to.module.name}`);
+  const fileContent = `\`\`\`mermaid\nflowchart\n\t${mermaidEdges.join("\n\t")}\n\`\`\``;
+  await writeFile(`${__dirname}/../DepTree.md`, fileContent);
 }
 
 async function bootstrap(): Promise<void> {
-    // @ts-expect-error I have no idea why this one is broken but I promise it's ok.
-    const app = await NestFactory.create(AppModule, {
-        logger: config.logger.levels,
-    });
+  const app = await NestFactory.create(AppModule, {
+    logger: config.logger.levels,
+  });
 
-    app.connectMicroservice({
-        transport: Transport.RMQ,
-        options: {
-            urls: [config.transport.url],
-            queue: config.transport.core_queue,
-            queueOptions: {
-                durable: true,
-            },
-            heartbeat: 120,
-        },
-    });
-    app.enableCors();
-    app.useGlobalPipes(new ValidationPipe());
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [config.transport.url],
+      queue: config.transport.core_queue,
+      queueOptions: {
+        durable: true,
+      },
+      heartbeat: 120,
+    },
+  });
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
-    app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalFilters(new AllExceptionsFilter());
 
-    const port = 3001;
-    await app.startAllMicroservices();
-    await app.listen(port);
+  const port = 3001;
+  await app.startAllMicroservices();
+  await app.listen(port);
 }
 
 // eslint-disable-next-line no-console
