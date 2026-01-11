2d699a0115f1e1806b2c76fe622ccc289032173e
Jake Bailey <asaxplayinghorse@gmail.com>
2026-01-11 11:03:00 -0800

    data: Add migration for previous changes.

---
+++ core/migrations/1768157507366-Automigration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Automigration1768157507366 implements MigrationInterface {
+  name = 'Automigration1768157507366'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "sprocket"."scrim_meta" ADD "isCompetitive" boolean NOT NULL DEFAULT true`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "sprocket"."scrim_meta" DROP COLUMN "isCompetitive"`);
+  }
+
+}
