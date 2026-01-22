fe8214307a4b32b8b661d97e626edfb729433c84
Jake Bailey <asaxplayinghorse@gmail.com>
2026-01-21 21:02:31 -0800

    cleanup: Remove unused scripts
    And cleanup the check users script.

--- check_users.ts
+++ check_users.ts
@@ -7,7 +7,7 @@ config();
 const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST || "localhost",
-  port: parseInt(process.env.DB_PORT) || 5432,
+  port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
   database: process.env.DB_DATABASE || "sprocket",
