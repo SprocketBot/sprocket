bab3855dc388ba8b03e772afa26db95e2a83bfae
Jake Bailey <asaxplayinghorse@gmail.com>
2026-01-28 15:06:12 -0800

    fix: Admin Replay Uploads

--- core/src/mledb/mledb-player/mledb-player.guard.ts
+++ core/src/mledb/mledb-player/mledb-player.guard.ts
@@ -4,6 +4,7 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { GraphQLError } from 'graphql';
 
 import type { UserPayload } from '../../identity';
+import { MLE_OrganizationTeam } from '../../database/mledb';
 import { MledbPlayerService } from './mledb-player.service';
 
 @Injectable()
@@ -14,6 +15,18 @@ export class FormerPlayerScrimGuard implements CanActivate {
     const ctx = GqlExecutionContext.create(context);
     const payload = ctx.getContext().req.user as UserPayload;
     const mlePlayer = await this.mledbPlayerService.getMlePlayerBySprocketUser(payload.userId);
+
+    const orgs = await this.mledbPlayerService.getPlayerOrgs(mlePlayer);
+    if (
+      orgs.some(
+        o =>
+          o.orgTeam === MLE_OrganizationTeam.MLEDB_ADMIN ||
+          o.orgTeam === MLE_OrganizationTeam.LEAGUE_OPERATIONS,
+      )
+    ) {
+      return true;
+    }
+
     if (mlePlayer.teamName === 'FP') throw new GraphQLError('User is a former player in MLE');
 
     return true;
